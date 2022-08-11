e90ba60af86de03e9dddc7e602416a1318e7ac01
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2022-08-11 16:43:37 +0800

    Fix SEO of site

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,6 @@ module.exports = {
     ],
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,16 @@
-import React from "react"
 import PropTypes from "prop-types"
-import styled from "styled-components"
+import React from "react"
 import { isMobile } from "react-device-detect"
-import Helmet from "react-helmet"
+import styled from "styled-components"
 
-import { StaticQuery, graphql } from "gatsby"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
+import { graphql, StaticQuery } from "gatsby"
 
 import "../../content/assets/stylesheets/style.scss"
 
-import Header from "./layouts/header"
 import Footer from "./layouts/footer"
+import Header from "./layouts/header"
 
 library.add(fab)
 
@@ -47,22 +46,9 @@ const Layout = ({ children }) => {
           }
         }
       `}
-      render={data => (
+      render={(data) => (
         <React.Fragment>
-          <Helmet
-            title={"title"}
-            meta={[
-              { name: "description", content: "Full Stack Web Developer" },
-              {
-                name: "keywords",
-                content:
-                  "full, stack, web, developer, philippines, davao, city, guy, romelle, magayano",
-              },
-            ]}
-          ></Helmet>
-          <Header
-            menuLinks={data.SiteMenuLinks.siteMetadata.menuLinks}
-          />
+          <Header menuLinks={data.SiteMenuLinks.siteMetadata.menuLinks} />
           <MainDiv className={`relative mb-32`}>
             <div className={`container mx-auto`}>
               <div className={`row mb-40`}>{children}</div>
