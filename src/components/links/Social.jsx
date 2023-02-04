3b64f1bb06e7c1633680b25eec5ccd41d872d803
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-04 23:13:56 +0800

    Update homepage

--- src/components/links/Social.jsx
+++ src/components/links/Social.jsx
@@ -1,18 +1,16 @@
-import Link from 'next/link'
-
 /**
- * @description Render the social link component
+ * @description Render the social link
  * @param {Object} props
- * @returns {JSX} Social link component
+ * @returns {JSX} Social link
  */
 const SocialLink = (props) => {
-	const { icon: Icon } = props
+	const { icon: Icon, url, ariaLabel } = props;
 
 	return (
-		<Link className="group -m-1 p-1" {...props}>
-      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
-    </Link>
-	)
-}
+		<a className="group -m-1 p-1" href={url || "#"} aria-label={ariaLabel} target="_blank">
+			<Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+		</a>
+	);
+};
 
-export default SocialLink
+export default SocialLink;
