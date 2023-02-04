acde544872f903099a34ccc41d71e1f894959cba
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-04 14:48:20 +0800

    Add SVG icons

---
+++ src/components/icons/svg/ArrowLeft.jsx
@@ -0,0 +1,19 @@
+/**
+ * @description Render the arrow left icon
+ * @param {Object} props
+ * @returns {JSX} Arrow left icon
+ */
+const ArrowLeftSvgIcon = (props) => {
+	return (
+		<svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+      <path
+        d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+	)
+}
+
+export default ArrowLeftSvgIcon
