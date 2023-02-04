b3c8d081d6ef8efff620df0be4d68189b3b77551
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-04 14:44:41 +0800

    Update `next` and `eslint` config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
+import rehypePrism from "@mapbox/rehype-prism";
+import nextMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx'],
-  reactStrictMode: true,
-  experimental: {
-    scrollRestoration: true,
-  },
-}
+	pageExtensions: ["js", "jsx", "mdx"],
+	reactStrictMode: true,
+	experimental: {
+		scrollRestoration: true
+	}
+};
 
 const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [remarkGfm],
+		rehypePlugins: [rehypePrism]
+	}
+});
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
