7950c6d3b9d8037e7fa7178782ab422307374130
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-07 01:59:45 +0800

    Update home page

---
+++ src/data/workExperiences.js
@@ -0,0 +1,80 @@
+import logoEpicDesignLabs from "@/images/logos/epic-design-labs.png";
+import logoInfosoftStudio from "@/images/logos/infosoft-studio.png";
+import logoMakeDevelopment from "@/images/logos/make-development.jpeg";
+import logoMultiplyMii from "@/images/logos/multiplymii.jpeg";
+import logoWesternPestControl from "@/images/logos/western-pest-control.jpeg";
+
+const workExperiencesData = [
+	{
+		company: "Epic Design Labs",
+		title: "Full Stack Developer",
+		logo: logoEpicDesignLabs,
+		start: "Apr. 2020",
+		end: "Jan. 2023"
+	},
+	{
+		company: "MultiplyMii",
+		title: "Full Stack Developer",
+		logo: logoMultiplyMii,
+		start: "Apr. 2020",
+		end: "Sept. 2021"
+	},
+	{
+		company: "MAKE Interactive BV",
+		title: "Web Developer",
+		logo: logoMakeDevelopment,
+		start: "Apr. 2019",
+		end: "Feb. 2020"
+	},
+	{
+		company: "Orth Enterprises, LLC",
+		title: "Web Developer",
+		logo: null,
+		start: "Apr. 2019",
+		end: "Aug. 2019"
+	},
+	{
+		company: "Outsourcing Wizards",
+		title: "Web Developer",
+		logo: null,
+		start: "Jan. 2019",
+		end: "Mar. 2019"
+	},
+	{
+		company: "Infosoft Studio",
+		title: "Web Developer",
+		logo: logoInfosoftStudio,
+		start: "May. 2017",
+		end: "Sept. 2018"
+	},
+	{
+		company: "MiniClean Business Solutions",
+		title: "Web Developer",
+		logo: null,
+		start: "Nov. 2014",
+		end: "Apr. 2015"
+	},
+	{
+		company: "Western Pest Control",
+		title: "Webmaster/Web Developer",
+		logo: logoWesternPestControl,
+		start: "May 2014",
+		end: "Oct. 2014"
+	},
+	{
+		company: "Freelance",
+		title: "Web Developer",
+		logo: null,
+		start: "May 2014",
+		end: "Oct. 2014"
+	},
+	{
+		company: "Freelance",
+		title: "Frontend Developer",
+		logo: null,
+		start: "Sept. 2013",
+		end: "Oct. 2013"
+	}
+];
+
+export default workExperiencesData;
