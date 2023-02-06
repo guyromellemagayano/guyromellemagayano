da36e006038f5541a0594edf97f27df57ca8c9be
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-07 01:59:45 +0800

    Update home page

---
+++ src/data/socialLinks.js
@@ -0,0 +1,27 @@
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from "@/components/SocialIcons";
+import MailAltSvgIcon from "@/components/icons/svg/MailAlt";
+
+const socialLinksData = [
+	{
+		url: "https://twitter.com/mguyromelle",
+		ariaLabel: "Follow on Twitter",
+		icon: TwitterIcon
+	},
+	{
+		url: "https://github.com/guyromellemagayano",
+		ariaLabel: "Follow on GitHub",
+		icon: GitHubIcon
+	},
+	{
+		url: "https://www.linkedin.com/in/mguyromelle/",
+		ariaLabel: "Follow on LinkedIn",
+		icon: LinkedInIcon
+	},
+	{
+		url: "mailto:hello@guyromellemagayano.dev",
+		ariaLabel: "hello@guyromellemagayano.dev",
+		icon: MailAltSvgIcon
+	}
+];
+
+export default socialLinksData;
