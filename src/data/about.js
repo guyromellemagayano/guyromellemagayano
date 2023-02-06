eb7bf6483d4731ea43874fb8036c7b86f9bb5c96
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-07 02:00:21 +0800

    Update About page

---
+++ src/data/about.js
@@ -0,0 +1,23 @@
+const AboutData = () => {
+	const meta = {
+		title: "About - Guy Romelle Magayano",
+		description:
+			"I’m Guy, a full stack developer and an open-source enthusiast based in Davao City, Philippines. I love building things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences while also ensuring minimal code and maximum efficiency.",
+		keywords:
+			"guy romelle magayano, full stack developer, davao, philippines, custom web application, custom web development, devops, seo, amazon, google, javascript, typescript, html, css, react, react native, wordpress, tailwind, jamstack, gatsby, nextjs, astro"
+	};
+
+	const hero = {
+		heading: "I’m Guy Romelle Magayano and I build things for the web.",
+		description: [
+			"I’ve loved making things for as long as I can remember, and wrote my first program when I was 6 years old, just two weeks after my mom brought home the brand new Macintosh LC 550 that I taught myself to type on.",
+			"The only thing I loved more than computers as a kid was space. When I was 8, I climbed the 40-foot oak tree at the back of our yard while wearing my older sister’s motorcycle helmet, counted down from three, and jumped — hoping the tree was tall enough that with just a bit of momentum I’d be able to get to orbit.",
+			"I spent the next few summers indoors working on a rocket design, while I recovered from the multiple surgeries it took to fix my badly broken legs. It took nine iterations, but when I was 15 I sent my dad’s Blackberry into orbit and was able to transmit a photo back down to our family computer from space.",
+			"Today, I’m the founder of Planetaria, where we’re working on civilian space suits and manned shuttle kits you can assemble at home so that the next generation of kids really can make it to orbit — from the comfort of their own backyards."
+		]
+	};
+
+	return { meta, hero };
+};
+
+export default AboutData;
