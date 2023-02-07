00dd8166ff041636e14e1d9fb4ea326bafd153d5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-08 07:33:19 +0800

    WIP Update skills pages

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -9,7 +9,7 @@ function Article({ article }) {
 		<article className="md:grid md:grid-cols-4 md:items-baseline">
 			<Card className="md:col-span-3">
 				<Card.Title href={`/articles/${article.slug}`}>{article.title}</Card.Title>
-				<Card.Eyebrow as="time" dateTime={article.date} className="md:hidden" decorate>
+				<Card.Eyebrow as="time" dateTime={article.date} decorate>
 					{formatDate(article.date)}
 				</Card.Eyebrow>
 				<Card.Description>{article.description}</Card.Description>
