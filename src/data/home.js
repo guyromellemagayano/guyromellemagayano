ac0f79d6fa73bf55789902de5513c9458a4b02ef
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-08 07:34:06 +0800

    Update SEO data of home page

--- src/data/home.js
+++ src/data/home.js
@@ -7,8 +7,7 @@ import image3 from "@/images/photos/workplace.jpg";
 const HomeData = () => {
 	const meta = {
 		title: "Guy Romelle Magayano - Full Stack Developer, open-source enthusiast, and a minimalist",
-		description:
-			"I’m Guy, a full stack developer and an open-source enthusiast based in Davao City, Philippines. I love building things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences while also ensuring minimal code and maximum efficiency.",
+		description: "Full stack developer, open-source enthusiast, and a minimalist.",
 		keywords:
 			"guy romelle magayano, full stack developer, davao, philippines, custom web application, custom web development, devops, seo, amazon, google, javascript, typescript, html, css, react, react native, wordpress, tailwind, jamstack, gatsby, nextjs, astro"
 	};
