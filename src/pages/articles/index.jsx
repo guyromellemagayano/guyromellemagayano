0f38848f7845e571184fe24b3aa2a99318edad2c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-08 17:15:27 +0800

    Refactor articles page

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,49 +1,38 @@
-import Card from "@/components/Card";
+import { ArticlesList, ArticlesListCards } from "@/components/List";
 import { SimpleLayout } from "@/components/SimpleLayout";
-import { formatDate } from "@/lib/formatDate";
+import ArticlesData from "@/data/articles";
 import { getAllArticles } from "@/lib/getAllArticles";
 import Head from "next/head";
 
-function Article({ article }) {
-	return (
-		<article className="md:grid md:grid-cols-4 md:items-baseline">
-			<Card.Eyebrow as="time" dateTime={article.date} className="mt-1 hidden md:block">
-				{formatDate(article.date)}
-			</Card.Eyebrow>
-			<Card className="md:col-span-3">
-				<Card.Title href={`/articles/${article.slug}`}>{article.title}</Card.Title>
-				<Card.Description>{article.description}</Card.Description>
-				<Card.Cta>Read article</Card.Cta>
-			</Card>
-		</article>
-	);
-}
+/**
+ * @description Render the articles page
+ * @param {Object} props
+ * @returns Articles page
+ */
+const Articles = (props) => {
+	const { articles } = props;
 
-export default function ArticlesIndex({ articles }) {
+	// Destructure the data from ArticlesData function
+	const { meta, hero } = ArticlesData();
+
 	return (
 		<>
 			<Head>
-				<title>Articles - Spencer Sharp</title>
-				<meta
-					name="description"
-					content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-				/>
+				<title>{meta.title}</title>
+				<meta name="description" content={meta.description} />
+				<meta name="keywords" content={meta.keywords} />
 			</Head>
-			<SimpleLayout
-				title="Writing on software design, company building, and the aerospace industry."
-				intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
-			>
-				<div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-					<div className="flex max-w-3xl flex-col space-y-16">
-						{articles.map((article) => (
-							<Article key={article.slug} article={article} />
-						))}
-					</div>
-				</div>
+
+			<SimpleLayout id="hero" title={hero.heading} intro={hero.description}>
+				<ArticlesList>
+					{articles.map((article) => (
+						<ArticlesListCards key={article.slug} article={article} />
+					))}
+				</ArticlesList>
 			</SimpleLayout>
 		</>
 	);
-}
+};
 
 export async function getStaticProps() {
 	return {
@@ -52,3 +41,5 @@ export async function getStaticProps() {
 		}
 	};
 }
+
+export default Articles;
