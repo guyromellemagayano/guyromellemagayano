a4d6febc15250b14583df8c8c49a02a133228b20
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-08 17:15:27 +0800

    Refactor articles page

---
+++ src/data/articles.js
@@ -0,0 +1,18 @@
+const ArticlesData = () => {
+	const meta = {
+		title: "Articles - Guy Romelle Magayano",
+		description: "",
+		keywords: ""
+	};
+
+	const hero = {
+		heading: "Writing on web and mobile app development, JAMstack, and e-commerce.",
+		description: [
+			"All of my learnings, experiences, and thoughts on programming, leadership, trends, and more, collected in chronological order."
+		]
+	};
+
+	return { meta, hero };
+};
+
+export default ArticlesData;
