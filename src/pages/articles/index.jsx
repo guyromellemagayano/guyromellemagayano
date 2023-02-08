a4b5a1b6133a929586c6d464e5d8fc3b0e4e9c56
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-08 16:00:01 +0800

    WIP Fix article pages and its subpages

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -7,17 +7,14 @@ import Head from "next/head";
 function Article({ article }) {
 	return (
 		<article className="md:grid md:grid-cols-4 md:items-baseline">
-			<Card className="md:col-span-3">
-				<Card.Title href={`/articles/${article.slug}`}>{article.title}</Card.Title>
-				<Card.Eyebrow as="time" dateTime={article.date} decorate>
-					{formatDate(article.date)}
-				</Card.Eyebrow>
-				<Card.Description>{article.description}</Card.Description>
-				<Card.Cta>Read article</Card.Cta>
-			</Card>
 			<Card.Eyebrow as="time" dateTime={article.date} className="mt-1 hidden md:block">
 				{formatDate(article.date)}
 			</Card.Eyebrow>
+			<Card className="md:col-span-3">
+				<Card.Title href={`/articles/${article.slug}`}>{article.title}</Card.Title>
+				<Card.Description>{article.description}</Card.Description>
+				<Card.Cta>Read article</Card.Cta>
+			</Card>
 		</article>
 	);
 }
