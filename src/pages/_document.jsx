481e7dfd4f006a57d3e728e9deef6e02c7817fa8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-09 17:38:18 +0800

    Format updates on ` _document` page

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,4 +1,4 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document";
 
 const modeScript = `
   let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
@@ -33,28 +33,20 @@ const modeScript = `
     disableTransitionsTemporarily()
     updateMode()
   }
-`
+`;
 
 export default function Document() {
-  return (
-    <Html className="h-full antialiased" lang="en">
-      <Head>
-        <script dangerouslySetInnerHTML={{ __html: modeScript }} />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.xml`}
-        />
-        <link
-          rel="alternate"
-          type="application/feed+json"
-          href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
-        />
-      </Head>
-      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html className="h-full antialiased" lang="en">
+			<Head>
+				<script dangerouslySetInnerHTML={{ __html: modeScript }} />
+				<link rel="alternate" type="application/rss+xml" href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.xml`} />
+				<link rel="alternate" type="application/feed+json" href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`} />
+			</Head>
+			<body className="flex h-full flex-col bg-zinc-50 dark:bg-black">
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
