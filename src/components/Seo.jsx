96ac352ce479d5feb1be2425e6eaa082ff5bb0ff
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-11 07:22:53 +0800

    Defer loading of GA script

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -20,9 +20,9 @@ const Seo = (props) => {
 			{/* Global site tag (gtag.js) - Google Analytics */}
 			<Script
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-				strategy="afterInteractive"
+				strategy="defer"
 			/>
-			<Script id="google-analytics" strategy="afterInteractive">
+			<Script id="google-analytics" strategy="defer">
 				{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
