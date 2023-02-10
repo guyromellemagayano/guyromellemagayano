6b7165afce9222281f62e988549bd15645721a92
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-10 10:41:26 +0800

    Update `about`, `articles`, and `skills` data

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,7 +1,7 @@
 const ArticlesData = () => {
 	const meta = {
 		title: "Articles - Guy Romelle Magayano",
-		description: "Writing on web and mobile app development, JAMstack, and e-commerce.",
+		description: "Writing on web and mobile app development, JAMstack, e-commerce, and a lot more.",
 		keywords:
 			"writing, web development, mobile app development, jamstack, e-commerce, seo, amazon, google, javascript, typescript, html, css, react, react native, wordpress, tailwind, jamstack, gatsby, nextjs, astro"
 	};
