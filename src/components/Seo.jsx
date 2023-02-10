9bd935e5fb80995614dec2c38d478bd996ee0e62
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-11 07:16:42 +0800

    Setup Google Analytics on site

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 /**
  * @description Render the SEO component
@@ -9,11 +10,28 @@ const Seo = (props) => {
 	const { meta } = props;
 
 	return (
-		<Head>
-			<title>{meta?.title || ""}</title>
-			<meta name="description" content={meta?.description || ""} />
-			<meta name="keywords" content={meta?.keywords || ""} />
-		</Head>
+		<>
+			<Head>
+				<title>{meta?.title || ""}</title>
+				<meta name="description" content={meta?.description || ""} />
+				<meta name="keywords" content={meta?.keywords || ""} />
+			</Head>
+
+			{/* Global site tag (gtag.js) - Google Analytics */}
+			<Script
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+				strategy="afterInteractive"
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+        `}
+			</Script>
+		</>
 	);
 };
 
