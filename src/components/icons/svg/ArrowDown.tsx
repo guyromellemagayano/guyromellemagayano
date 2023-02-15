e3bbf068d3401c463d186da5c2e8adbd89dd56ba
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-15 16:35:53 +0800

    Update SVG icon components for TypeScript

---
+++ src/components/icons/svg/ArrowDown.tsx
@@ -0,0 +1,17 @@
+import { SVGProps } from "react";
+
+// Arrow down SVG icon
+const ArrowDownSvgIcon = (props: SVGProps<SVGSVGElement>) => {
+	return (
+		<svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+			<path
+				d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+};
+
+export default ArrowDownSvgIcon;
