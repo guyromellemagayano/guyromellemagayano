af0674a27fde86ed09c7e20ae06a3623b1053206
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-15 10:38:07 +0800

    Refactor footer component

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,8 +1,12 @@
 import Container from "@/components/Container";
+import FooterData from "@/data/footer";
 import NavLink from "../NavLink";
 
 // Footer component
 const Footer = () => {
+	// Destructure the data from the FooterData function
+	const { menu, copyright } = FooterData();
+
 	return (
 		<footer className="mt-32">
 			<Container.Outer>
@@ -10,13 +14,12 @@ const Footer = () => {
 					<Container.Inner>
 						<div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
 							<div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-								<NavLink href="/about">About</NavLink>
-								<NavLink href="/projects">Projects</NavLink>
-								<NavLink href="/skills">Skills</NavLink>
-								<NavLink href="/uses">Uses</NavLink>
+								{menu.map((item) => (
+									<NavLink href={item.link}>{item.title}</NavLink>
+								))}
 							</div>
 							<p className="text-sm text-zinc-400 dark:text-zinc-500">
-								&copy; {new Date().getFullYear()} Guy Romelle Magayano. All rights reserved.
+								&copy; {copyright.year} {copyright.text}
 							</p>
 						</div>
 					</Container.Inner>
