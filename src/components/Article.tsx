8af7735804a111fd15b5be559a930b124e33c162
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-24 20:43:58 +0800

    Fix UI issues

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,19 +1,19 @@
-import { IArticleProps } from "@/interfaces/components";
-import { formatDate } from "@/lib/formatDate";
-import Card from "./Card";
+import { IArticleProps } from '@/interfaces/components'
+import { formatDate } from '@/lib/formatDate'
+import Card from './Card'
 
 // Article component
 const Article = ({ slug, meta: { title, date, description } }: IArticleProps): JSX.Element => {
-	return (
-		<Card as="article">
-			<Card.Title href={`/articles/${slug}`}>{title}</Card.Title>
-			<Card.Eyebrow as="time" dateTime={date} decorate>
-				{formatDate(date)}
-			</Card.Eyebrow>
-			<Card.Description>{description}</Card.Description>
-			<Card.Cta>Read article</Card.Cta>
-		</Card>
-	);
-};
+  return (
+    <Card as="article">
+      <Card.Title href={`/articles/${slug}`}>{title}</Card.Title>
+      <Card.Eyebrow as="time" dateTime={date} className="text-zinc-400 dark:text-zinc-500" decorate>
+        {formatDate(date)}
+      </Card.Eyebrow>
+      <Card.Description>{description}</Card.Description>
+      <Card.Cta>Read article</Card.Cta>
+    </Card>
+  )
+}
 
-export default Article;
+export default Article
