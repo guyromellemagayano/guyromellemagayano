a5673f51af2af918d5adb1c081bcf06831b19b57
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-27 17:37:47 +0800

    Update SEO keywords

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,135 +1,113 @@
-import logoEpicDesignLabs from "@/images/logos/epic-design-labs.png";
-import logoInfosoftStudio from "@/images/logos/infosoft-studio.png";
-import logoMakeDevelopment from "@/images/logos/make-development.jpeg";
-import logoMultiplyMii from "@/images/logos/multiplymii.jpeg";
-import logoWesternPestControl from "@/images/logos/western-pest-control.jpeg";
-import image4 from "@/images/photos/food.jpg";
-import image5 from "@/images/photos/photography.jpg";
-import image2 from "@/images/photos/speaker.jpg";
-import image1 from "@/images/photos/travel.jpg";
-import image3 from "@/images/photos/workplace.jpg";
-import { IHomeData } from "@/interfaces/data";
+import logoEpicDesignLabs from '@/images/logos/epic-design-labs.png'
+import logoInfosoftStudio from '@/images/logos/infosoft-studio.png'
+import logoMakeDevelopment from '@/images/logos/make-development.jpeg'
+import logoMultiplyMii from '@/images/logos/multiplymii.jpeg'
+import image4 from '@/images/photos/food.jpg'
+import image5 from '@/images/photos/photography.jpg'
+import image2 from '@/images/photos/speaker.jpg'
+import image1 from '@/images/photos/travel.jpg'
+import image3 from '@/images/photos/workplace.jpg'
+import { IHomeData } from '@/interfaces/data'
 
 const HomeData = (): IHomeData => {
-	const meta = {
-		title: "Guy Romelle Magayano - Full Stack Developer, open-source enthusiast, and a minimalist",
-		description: "Full stack developer, open-source enthusiast, and a minimalist.",
-		keywords:
-			"guy romelle magayano, full stack developer, davao, philippines, custom web application, custom web development, devops, seo, amazon, google, javascript, typescript, html, css, react, react native, wordpress, tailwind, jamstack, gatsby, nextjs, astro"
-	};
+  const meta = {
+    title: 'Guy Romelle Magayano - Full Stack Developer, open-source enthusiast, and a minimalist',
+    description: 'Full stack developer, open-source enthusiast, and a minimalist.',
+    keywords:
+      'guy romelle magayano, full stack developer, davao, philippines, custom web application, custom web development, devops, seo, amazon, google, javascript, typescript, html, css, react, react native, wordpress, tailwind, jamstack, gatsby, nextjs',
+  }
 
-	const hero = {
-		heading: "Full stack developer, open-source enthusiast, and a minimalist.",
-		description: [
-			"I’m Guy, a full stack developer and an open-source enthusiast based in Davao City, Philippines. I love building things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences while also ensuring minimal code and maximum efficiency."
-		]
-	};
+  const hero = {
+    heading: 'Full stack developer, open-source enthusiast, and a minimalist.',
+    description: [
+      'I’m Guy, a full stack developer and an open-source enthusiast based in Davao City, Philippines. I love building things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences while also ensuring minimal code and maximum efficiency.',
+    ],
+  }
 
-	const slidePhotos = [
-		{
-			alt: "",
-			src: image1
-		},
-		{
-			alt: "",
-			src: image2
-		},
-		{
-			alt: "",
-			src: image3
-		},
-		{
-			alt: "",
-			src: image4
-		},
-		{
-			alt: "",
-			src: image5
-		}
-	];
+  const slidePhotos = [
+    {
+      alt: '',
+      src: image1,
+    },
+    {
+      alt: '',
+      src: image2,
+    },
+    {
+      alt: '',
+      src: image3,
+    },
+    {
+      alt: '',
+      src: image4,
+    },
+    {
+      alt: '',
+      src: image5,
+    },
+  ]
 
-	const cvFile = "/docs/cv.pdf";
+  const cvFile = '/docs/cv.pdf'
 
-	const workExperiences = [
-		{
-			company: "Epic Design Labs",
-			title: "Full Stack Developer",
-			logo: logoEpicDesignLabs,
-			start: "Apr. 2020",
-			end: "Feb. 2023"
-		},
-		{
-			company: "MultiplyMii",
-			title: "Full Stack Developer",
-			logo: logoMultiplyMii,
-			start: "Apr. 2020",
-			end: "Sept. 2021"
-		},
-		{
-			company: "MAKE Interactive BV",
-			title: "Web Developer",
-			logo: logoMakeDevelopment,
-			start: "Apr. 2019",
-			end: "Feb. 2020"
-		},
-		{
-			company: "Orth Enterprises, LLC",
-			title: "Web Developer",
-			logo: null,
-			start: "Apr. 2019",
-			end: "Aug. 2019"
-		},
-		{
-			company: "Outsourcing Wizards",
-			title: "Web Developer",
-			logo: null,
-			start: "Jan. 2019",
-			end: "Mar. 2019"
-		},
-		{
-			company: "Infosoft Studio",
-			title: "Web Developer",
-			logo: logoInfosoftStudio,
-			start: "May. 2017",
-			end: "Sept. 2018"
-		},
-		{
-			company: "MiniClean Business Solutions",
-			title: "Web Developer",
-			logo: null,
-			start: "Nov. 2014",
-			end: "Apr. 2015"
-		},
-		{
-			company: "Western Pest Control",
-			title: "Webmaster",
-			logo: logoWesternPestControl,
-			start: "May 2014",
-			end: "Oct. 2014"
-		},
-		{
-			company: "Freelance",
-			title: "Web Developer",
-			logo: null,
-			start: "May 2014",
-			end: "Oct. 2014"
-		},
-		{
-			company: "Freelance",
-			title: "Frontend Developer",
-			logo: null,
-			start: "Sept. 2013",
-			end: "Oct. 2013"
-		}
-	];
+  const workExperiences = [
+    {
+      company: 'Epic Design Labs',
+      title: 'Full Stack Developer',
+      logo: logoEpicDesignLabs,
+      start: 'Apr. 2020',
+      end: 'Feb. 2023',
+    },
+    {
+      company: 'MultiplyMii',
+      title: 'Full Stack Developer',
+      logo: logoMultiplyMii,
+      start: 'Apr. 2020',
+      end: 'Sept. 2021',
+    },
+    {
+      company: 'MAKE Interactive BV',
+      title: 'Web Developer',
+      logo: logoMakeDevelopment,
+      start: 'Apr. 2019',
+      end: 'Feb. 2020',
+    },
+    {
+      company: 'Orth Enterprises, LLC',
+      title: 'Web Developer',
+      logo: null,
+      start: 'Apr. 2019',
+      end: 'Aug. 2019',
+    },
+    {
+      company: 'Outsourcing Wizards',
+      title: 'Web Developer',
+      logo: null,
+      start: 'Jan. 2019',
+      end: 'Mar. 2019',
+    },
+    {
+      company: 'Infosoft Studio',
+      title: 'Web Developer',
+      logo: logoInfosoftStudio,
+      start: 'May. 2017',
+      end: 'Sept. 2018',
+    },
+    {
+      company: 'MiniClean Business Solutions',
+      title: 'Web Developer',
+      logo: null,
+      start: 'Nov. 2014',
+      end: 'Apr. 2015',
+    },
+  ]
 
-	return {
-		meta,
-		hero,
-		slidePhotos,
-		cvFile,
-		workExperiences
-	};
-};
+  return {
+    meta,
+    hero,
+    slidePhotos,
+    cvFile,
+    workExperiences,
+  }
+}
 
-export default HomeData;
+export default HomeData
