bd19ebbcf4f142eabd02bf0166028b3ab9e26a57
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-27 17:35:32 +0800

    Update site navigation links

--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -11,6 +11,9 @@ const MobileNavigation = (rest: any): JSX.Element => {
   // Destructure the data from the FooterData function
   const { menu } = NavigationData()
 
+  // Page filter
+  const pageFilter = ['Skills', 'Work', 'Projects', 'About']
+
   return (
     <Popover {...rest}>
       <Popover.Button className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
@@ -50,11 +53,13 @@ const MobileNavigation = (rest: any): JSX.Element => {
             </div>
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                {menu.map((item) => (
-                  <MobileNavItem key={item.link} href={item.link}>
-                    {item.title}
-                  </MobileNavItem>
-                ))}
+                {menu
+                  .filter((item) => pageFilter.includes(item.title))
+                  .map((item) => (
+                    <MobileNavItem key={item.link} href={item.link}>
+                      {item.title}
+                    </MobileNavItem>
+                  ))}
               </ul>
             </nav>
           </Popover.Panel>
