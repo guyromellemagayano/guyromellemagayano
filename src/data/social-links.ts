bc46b98e2ab433f5e7b324fe27d5cd66b0d50ba9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-02-27 17:37:47 +0800

    Update SEO keywords

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -1,28 +1,28 @@
-import MailSvgIcon from "@/components/icons/svg/Mail";
-import { GitHubSvgIcon, LinkedInSvgIcon, TwitterSvgIcon } from "@/components/SocialIcons";
-import { ISocialLinksData } from "@/interfaces/data";
+import MailSvgIcon from '@/components/icons/svg/Mail'
+import { GitHubSvgIcon, LinkedInSvgIcon, TwitterSvgIcon } from '@/components/SocialIcons'
+import { ISocialLinksData } from '@/interfaces/data'
 
 const socialLinksData = [
-	{
-		url: "https://twitter.com/mguyromelle",
-		ariaLabel: "Follow on Twitter",
-		icon: TwitterSvgIcon
-	},
-	{
-		url: "https://github.com/guyromellemagayano",
-		ariaLabel: "Follow on GitHub",
-		icon: GitHubSvgIcon
-	},
-	{
-		url: "https://www.linkedin.com/in/mguyromelle/",
-		ariaLabel: "Follow on LinkedIn",
-		icon: LinkedInSvgIcon
-	},
-	{
-		url: "mailto:hello@guyromellemagayano.dev",
-		ariaLabel: "hello@guyromellemagayano.dev",
-		icon: MailSvgIcon
-	}
-] as ISocialLinksData[];
+  {
+    url: 'https://twitter.com/mguyromelle',
+    ariaLabel: 'Follow on Twitter',
+    icon: TwitterSvgIcon,
+  },
+  {
+    url: 'https://github.com/guyromellemagayano',
+    ariaLabel: 'Follow on GitHub',
+    icon: GitHubSvgIcon,
+  },
+  {
+    url: 'https://www.linkedin.com/in/mguyromelle/',
+    ariaLabel: 'Follow on LinkedIn',
+    icon: LinkedInSvgIcon,
+  },
+  {
+    url: 'mailto:aspiredtechie2010@gmail.com',
+    ariaLabel: 'aspiredtechie2010@gmail.com',
+    icon: MailSvgIcon,
+  },
+] as ISocialLinksData[]
 
-export default socialLinksData;
+export default socialLinksData
