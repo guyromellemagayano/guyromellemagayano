514ab497d52a96abfcb8ce21ba72c35f1ac30c2f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-03-25 08:52:47 +0800

    Add custom 404 page

--- src/components/SimpleLayout.tsx
+++ src/components/SimpleLayout.tsx
@@ -1,17 +1,17 @@
-import Container from "@/components/Container";
-import { ISimpleLayoutProps } from "@/interfaces/components";
+import Container from '@/components/Container'
+import { ISimpleLayoutProps } from '@/interfaces/components'
 
 // Simple layout component
 const SimpleLayout = ({ title, intro, children }: ISimpleLayoutProps): JSX.Element => {
-	return (
-		<Container className="mt-16 sm:mt-32">
-			<header className="max-w-2xl">
-				<h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">{title}</h1>
-				<p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">{intro}</p>
-			</header>
-			<div className="mt-16 sm:mt-20">{children}</div>
-		</Container>
-	);
-};
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <header className="max-w-2xl">
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">{title}</h1>
+        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">{intro}</p>
+      </header>
+      <div className="mt-16 sm:mt-20">{children}</div>
+    </Container>
+  )
+}
 
-export default SimpleLayout;
+export default SimpleLayout
