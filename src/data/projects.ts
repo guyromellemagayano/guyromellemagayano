d12cda66c28549079cd12dab26529f812d0e558a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-04 05:41:58 +0800

    Update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,5 @@
 import logoDjango from '@/images/logos/django.png'
 // import logoGithub from '@/images/logos/github.png'
-import logoWordpress from '@/images/logos/wordpress.png'
 import { IProjectsData } from '@/interfaces/data'
 
 const ProjectsData = (): IProjectsData => {
@@ -28,16 +27,6 @@ const ProjectsData = (): IProjectsData => {
       },
       tech: ['NextJS', 'Django', 'PostgreSQL', 'Terraform', 'Docker', 'AWS', 'Traefik', 'Go', 'LocalStack'],
       logo: logoDjango,
-    },
-    {
-      name: 'Big Leap Partnerships',
-      description: 'Taking your business from where it is to where you want it to be',
-      link: {
-        url: 'https://github.com/guyromellemagayano/big-leap-partnerships',
-        text: 'github.com',
-      },
-      tech: ['WordPress', 'PHP', 'Timber', 'MySQL', 'Gulp', 'jQuery', 'Bootstrap'],
-      logo: logoWordpress,
     },
   ]
 
