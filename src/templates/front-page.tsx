eaf8ec48bf5db0bd81052fa9092eb42b51a04c07
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-08 18:24:07 +0800

    WIP Update front-page

--- src/templates/front-page.tsx
+++ src/templates/front-page.tsx
@@ -1,16 +1,94 @@
-import { GetHomePageQuery } from '@/__generated__/graphql'
+import { Container, PhotosLayout, ResumeLayout, Seo, SocialLink } from '@/components'
+import HomeData from '@/data/home'
+import socialLinksData from '@/data/social-links'
+import { ISeoMetaCommonProps } from '@/interfaces'
 import { FaustTemplate } from '@faustwp/core'
-import React from 'react'
+import { gql } from '../__generated__'
+import { GetHomePageQuery } from '../__generated__/graphql'
 
-// Front page template
-const Component: FaustTemplate<GetHomePageQuery> = (props): React.ReactNode => {
-    console.log(props?.data?.generalSettings)
+const Component: FaustTemplate<GetHomePageQuery> = (props) => {
+    // TODO: Add loading state for previews
+
+    console.log(props)
+
+    const meta: ISeoMetaCommonProps = {
+        title: props.data?.generalSettings?.title || 'Untitled',
+        description: props.data?.generalSettings?.description || 'No description',
+        keywords: '',
+    }
+
+    // Destructure the data from the HomeData function
+    const { hero, slidePhotos, cvFile, workExperiences } = HomeData()
+
+    // Update the work experience data
+    const updatedResumeData = {
+        work: workExperiences,
+        file: cvFile,
+    }
 
     return (
         <>
-            <h1>Hello</h1>
+            <Seo meta={meta} />
+
+            <Container id="hero" className="mt-9">
+                <div className="max-w-2xl">
+                    <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                        {hero.heading}
+                    </h1>
+                    <p className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
+                        {hero.description.map((paragraph, index) => {
+                            return (
+                                <span key={index} className="space-y-7">
+                                    {paragraph}
+                                </span>
+                            )
+                        })}
+                    </p>
+                    <div className="mt-6 flex gap-6">
+                        {socialLinksData.map((link) => {
+                            return <SocialLink key={link.url} {...link} />
+                        })}
+                    </div>
+                </div>
+            </Container>
+
+            <PhotosLayout data={slidePhotos} />
+
+            <Container className="mt-24 md:mt-28">
+                <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+                    <div className="flex flex-col gap-16">{/* TODO: Create articles container here */}</div>
+                    <div className="space-y-10 lg:pl-16 xl:pl-24">
+                        {/* <NewsletterLayout /> */}
+                        <ResumeLayout data={updatedResumeData} />
+                    </div>
+                </div>
+            </Container>
         </>
     )
 }
+
+Component.query = gql(`
+  query GetHomePage {
+    generalSettings {
+      title
+      description
+    }
+    primaryMenuItems: menuItems(where: { location: PRIMARY }) {
+      nodes {
+        id
+        uri
+        path
+        label
+        parentId
+        cssClasses
+        menu {
+          node {
+            name
+          }
+        }
+      }
+    }
+  }
+`)
 
 export default Component
