8dcd6cc30dc9c2c6c62a8ea82c79bb83b15a729c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-08 17:33:19 +0800

    Code formatting fixes on `next` config

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,40 @@
-import { getWpHostname, withFaust } from "@faustwp/core";
-import { withSentryConfig } from "@sentry/nextjs";
+import { getWpHostname, withFaust } from '@faustwp/core'
+import { withSentryConfig } from '@sentry/nextjs'
 
 /** @type {import('next').NextConfig} */
 const faustConfig = {
-	reactStrictMode: true,
-	pageExtensions: ["js", "jsx", "ts", "tsx"],
-	sassOptions: {
-		includePaths: ['node_modules'],
-	},
-	images: {
-		domains: [getWpHostname()],
-	},
-	experimental: {
-		scrollRestoration: true
-	},
-	sentry: {
-		hideSourceMaps: true
-	},
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
-};
+    reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    sassOptions: {
+        includePaths: ['node_modules'],
+    },
+    images: {
+        domains: [getWpHostname()],
+    },
+    experimental: {
+        scrollRestoration: true,
+    },
+    sentry: {
+        hideSourceMaps: true,
+    },
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
+}
 
 const sentryConfig = {
-	disableLogger: true,
-	hideSourceMaps: true,
-	org: "guy-romelle-magayano",
-	org: "guy-romelle-magayano",
-	project: "guyromellemagayano",
-	project: "guyromellemagayano",
-	silent: true,
-	silent: true,
-	transpileClientSDK: true,
-	tunnelRoute: "/monitoring",
-	widenClientFileUpload: true,
-};
+    disableLogger: true,
+    hideSourceMaps: true,
+    org: 'guy-romelle-magayano',
+    org: 'guy-romelle-magayano',
+    project: 'guyromellemagayano',
+    project: 'guyromellemagayano',
+    silent: true,
+    silent: true,
+    transpileClientSDK: true,
+    tunnelRoute: '/monitoring',
+    widenClientFileUpload: true,
+}
 
-export default withSentryConfig(withFaust(faustConfig), sentryConfig);
+export default withFaust(faustConfig, withSentryConfig(sentryConfig))
