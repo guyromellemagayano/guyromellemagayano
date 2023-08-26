33c488e2f4b115b04c1237f91fa1b2438ecdea1f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-26 12:13:41 +0800

    Update site setup

---
+++ sentry.client.config.ts
@@ -0,0 +1,28 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+const SENTRY_DSN = process.env.SENTRY_DSN
+
+Sentry.init({
+    dsn: SENTRY_DSN,
+    // Replay may only be enabled for the client-side
+    integrations: [new Sentry.Replay()],
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 0.25,
+    environment: process.env.SENTRY_ENVIRONMENT,
+    enabled: process.env.NODE_ENV === 'production',
+
+    // ...
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+})
