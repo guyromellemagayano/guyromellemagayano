6bac96aabf81136e96ea82d2706589208fb4c2ab
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-30 10:52:15 +0800

    Update code format of `app` page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,38 @@
-import "../../faust.config"
-import 'focus-visible'
-import '@/styles/tailwind.css'
 import Footer from '@/components/layouts/Footer'
 import Header from '@/components/layouts/Header'
 import usePrevious from '@/hooks/usePrevious'
+import '@/styles/tailwind.css'
+import { FaustProvider } from '@faustwp/core'
 import { Analytics } from '@vercel/analytics/react'
-import { FaustProvider } from "@faustwp/core";
+import 'focus-visible'
 import { AppProps } from 'next/app'
+import '../../faust.config'
 
 const App = ({ Component, pageProps, router }: AppProps): React.ReactNode => {
-  const previousPathname = usePrevious(router.pathname)
+    const previousPathname = usePrevious(router.pathname)
 
-  // Add the previousPathname to pageProps
-  const extendedPageProps = { ...pageProps, previousPathname }
+    // Add the previousPathname to pageProps
+    const extendedPageProps = { ...pageProps, previousPathname }
 
-  return (
-    <FaustProvider pageProps={pageProps}>
-      <div className="fixed inset-0 flex justify-center sm:px-8 bg-white dark:bg-zinc-900">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full" />
-        </div>
-      </div>
+    return (
+        <FaustProvider pageProps={pageProps}>
+            <div className="fixed inset-0 flex justify-center sm:px-8 bg-white dark:bg-zinc-900">
+                <div className="flex w-full max-w-7xl lg:px-8">
+                    <div className="w-full" />
+                </div>
+            </div>
 
-      <div className="relative">
-        <Header />
-        <main>
-          <Component {...extendedPageProps} key={router.asPath} />
-        </main>
-        <Footer />
-      </div>
+            <div className="relative">
+                <Header />
+                <main>
+                    <Component {...extendedPageProps} key={router.asPath} />
+                </main>
+                <Footer />
+            </div>
 
-      <Analytics />
-    </FaustProvider>
-  )
+            <Analytics />
+        </FaustProvider>
+    )
 }
 
 export default App
