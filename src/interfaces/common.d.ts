6cfa5bfb81226b26f57dc3867aa3c02c076f9f0f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-30 12:04:28 +0800

    Update `common` and `components` interfaces

--- src/interfaces/common.d.ts
+++ src/interfaces/common.d.ts
@@ -1,11 +1,11 @@
 // SEO meta common props
 export interface ISeoMetaCommonProps {
     title: string
-    description: string | null
+    description?: string
 }
 
 // Hero component common props
 export interface IHeroCommonProps {
     heading: string
-    description: string
+    description?: string[] | string
 }
