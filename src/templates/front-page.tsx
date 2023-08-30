7f7ab4347d4c8a3f228764e08b822eaba8901748
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-30 11:07:20 +0800

    Fix code format of front page

--- src/templates/front-page.tsx
+++ src/templates/front-page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { FaustTemplate } from '@faustwp/core'
 import { gql } from '../__generated__'
 import { GetHomePageQuery } from '../__generated__/graphql'
-import { FaustTemplate } from '@faustwp/core'
 // import Article from '@/components/Article'
 import Container from '@/components/Container'
 import Seo from '@/components/Seo'
@@ -17,54 +17,54 @@ const Component: FaustTemplate<GetHomePageQuery> = (props): React.ReactNode => {
     const { meta, hero, slidePhotos, cvFile, workExperiences } = HomeData()
 
     // Update the work experience data
-  const updatedResumeData = {
-    work: workExperiences,
-    file: cvFile,
-  }
+    const updatedResumeData = {
+        work: workExperiences,
+        file: cvFile,
+    }
 
-  console.log(props)
+    console.log(props)
 
-  return (
-    <>
-      <Seo meta={meta} />
+    return (
+        <>
+            <Seo meta={meta} />
 
-      <Container id="hero" className="mt-9">
-        <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            {hero.heading}
-          </h1>
-          <p className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
-            {hero.description.map((paragraph, index) => {
-              return (
-                <span key={index} className="space-y-7">
-                  {paragraph}
-                </span>
-              )
-            })}
-          </p>
-          <div className="mt-6 flex gap-6">
-            {socialLinksData.map((link) => {
-              return <SocialLink key={link.url} {...link} />
-            })}
-          </div>
-        </div>
-      </Container>
+            <Container id="hero" className="mt-9">
+                <div className="max-w-2xl">
+                    <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                        {hero.heading}
+                    </h1>
+                    <p className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
+                        {hero.description.map((paragraph, index) => {
+                            return (
+                                <span key={index} className="space-y-7">
+                                    {paragraph}
+                                </span>
+                            )
+                        })}
+                    </p>
+                    <div className="mt-6 flex gap-6">
+                        {socialLinksData.map((link) => {
+                            return <SocialLink key={link.url} {...link} />
+                        })}
+                    </div>
+                </div>
+            </Container>
 
-      <Photos data={slidePhotos} />
+            <Photos data={slidePhotos} />
 
-      <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            {/* {articles?.map((article) => <Article key={article.slug} {...article} />)} */}
-          </div>
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-            {/* <Newsletter /> */}
-            <Resume data={updatedResumeData} />
-          </div>
-        </div>
-      </Container>
-    </>
-  )
+            <Container className="mt-24 md:mt-28">
+                <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+                    <div className="flex flex-col gap-16">
+                        {/* {articles?.map((article) => <Article key={article.slug} {...article} />)} */}
+                    </div>
+                    <div className="space-y-10 lg:pl-16 xl:pl-24">
+                        {/* <Newsletter /> */}
+                        <Resume data={updatedResumeData} />
+                    </div>
+                </div>
+            </Container>
+        </>
+    )
 }
 
 Component.query = gql(`
