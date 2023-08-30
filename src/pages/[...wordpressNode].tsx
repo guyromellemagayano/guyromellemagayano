17e0148a052c51065017c4501fe0ddd988e4e941
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-30 10:52:55 +0800

    Fix `getStaticProps` and `getStaticPaths` of some pages

--- src/pages/[...wordpressNode].tsx
+++ src/pages/[...wordpressNode].tsx
@@ -1,18 +1,39 @@
-import { getWordPressProps, WordPressTemplate } from "@faustwp/core";
-import { GetStaticPaths, GetStaticProps } from "next";
-import { WordPressTemplateProps } from "../types";
+import { getWordPressProps, WordPressTemplate } from '@faustwp/core'
+import {
+    GetStaticPaths,
+    GetStaticPathsResult,
+    GetStaticProps,
+    GetStaticPropsResult,
+} from 'next'
+import { WordPressTemplateProps } from '../types'
 
-export default function Page(props: WordPressTemplateProps) {
-  return <WordPressTemplate {...props} />;
+const Page = (props: WordPressTemplateProps): React.ReactNode => {
+    return <WordPressTemplate {...props} />
 }
 
-export const getStaticProps: GetStaticProps = (ctx) => {
-  return getWordPressProps({ ctx });
-};
+export default Page
 
-export const getStaticPaths: GetStaticPaths = () => {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
-};
+export const getStaticProps: GetStaticProps = async (
+    ctx
+): Promise<GetStaticPropsResult<WordPressTemplateProps>> => {
+    const result = await getWordPressProps({ ctx })
+    if (
+        typeof result === 'object' &&
+        'props' in result &&
+        '__SEED_NODE__' in result.props &&
+        '__TEMPLATE_QUERY_DATA__' in result.props
+    ) {
+        return result as GetStaticPropsResult<WordPressTemplateProps>
+    }
+
+    console.log('Unexpected result:', result)
+    throw new Error('Incompatible type returned by getWordPressProps')
+}
+
+export const getStaticPaths: GetStaticPaths =
+    async (): Promise<GetStaticPathsResult> => {
+        return {
+            paths: [],
+            fallback: 'blocking',
+        }
+    }
