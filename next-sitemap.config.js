b2a6e81e9d3a6299da3e768da5111bf9be1588c3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-30 09:45:36 +0800

    Update `next` sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.guyromellemagayano.dev',
+  siteUrl: process.env.SITE_URL || 'https://guyromellemagayano.dev',
   basePath: '/',
   sitemapBaseFileName: 'sitemap',
   generateRobotsTxt: true,
