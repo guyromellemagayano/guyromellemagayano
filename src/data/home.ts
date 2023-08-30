f5edc33d99c7d93013af7c323558c83760d44fe9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-08-30 11:47:05 +0800

    Update `home` data

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,3 +1,4 @@
+import { GetHomePageQuery } from '@/__generated__/graphql'
 import logoEpicDesignLabs from '@/images/logos/epic-design-labs.png'
 import logoInfosoftStudio from '@/images/logos/infosoft-studio.png'
 import logoMakeDevelopment from '@/images/logos/make-development.jpeg'
@@ -9,106 +10,109 @@ import image2 from '@/images/photos/speaker.jpg'
 import image1 from '@/images/photos/travel.jpg'
 import image3 from '@/images/photos/workplace.jpg'
 import { IHomeData } from '@/interfaces/data'
+import { FaustTemplateProps } from '@faustwp/core'
 
-const HomeData = (): IHomeData => {
-  const meta = {
-    title: 'Guy Romelle Magayano - Full Stack Developer, open-source enthusiast, and a minimalist',
-    description: 'Full stack developer, open-source enthusiast, and a minimalist.',
-    keywords:
-      'guy romelle magayano, full stack developer, davao, philippines, custom web application, custom web development, devops, seo, amazon, google, javascript, typescript, html, css, react, react native, wordpress, tailwind, jamstack, gatsby, nextjs',
-  }
+const HomeData = (props: FaustTemplateProps<GetHomePageQuery>): IHomeData => {
+    const { data } = props
 
-  const hero = {
-    heading: 'Full stack developer, open-source enthusiast, and a minimalist.',
-    description: [
-      'I’m Guy, a full stack developer and an open-source enthusiast based in Davao City, Philippines. I love building things that live on the internet, whether that be websites, applications, or anything in between. My goal is to always build products that provide pixel-perfect, performant experiences while also ensuring minimal code and maximum efficiency.',
-    ],
-  }
+    const meta = {
+        title:
+            data?.generalSettings?.title ||
+            'Default Title - Guy Romelle Magayano',
+        description:
+            data?.generalSettings?.description ||
+            'Default Description - Guy Romelle Magayano',
+    }
 
-  const slidePhotos = [
-    {
-      alt: '',
-      src: image1,
-    },
-    {
-      alt: '',
-      src: image2,
-    },
-    {
-      alt: '',
-      src: image3,
-    },
-    {
-      alt: '',
-      src: image4,
-    },
-    {
-      alt: '',
-      src: image5,
-    },
-  ]
+    const hero = {
+        heading: data?.page?.title || 'Default Heading',
+        description: data?.page?.content || 'Default Description',
+    }
 
-  const cvFile = '/docs/cv.pdf'
+    const slidePhotos = [
+        {
+            alt: '',
+            src: image1,
+        },
+        {
+            alt: '',
+            src: image2,
+        },
+        {
+            alt: '',
+            src: image3,
+        },
+        {
+            alt: '',
+            src: image4,
+        },
+        {
+            alt: '',
+            src: image5,
+        },
+    ]
 
-  const workExperiences = [
-    {
-      company: 'X-Team',
-      title: 'Senior Full Stack + WordPress Developer',
-      logo: logoXTeam,
-      start: 'Apr. 2023',
-      end: 'Present',
-    },
-    {
-      company: 'Epic Design Labs',
-      title: 'Senior Full Stack Developer',
-      logo: logoEpicDesignLabs,
-      start: 'Apr. 2020',
-      end: 'Feb. 2023',
-    },
-    {
-      company: 'MultiplyMii',
-      title: 'Senior Full Stack Developer',
-      logo: logoMultiplyMii,
-      start: 'Apr. 2020',
-      end: 'Sept. 2021',
-    },
-    {
-      company: 'MAKE Interactive BV',
-      title: 'Full Stack Developer',
-      logo: logoMakeDevelopment,
-      start: 'Apr. 2019',
-      end: 'Feb. 2020',
-    },
-    {
-      company: 'Orth Enterprises, LLC',
-      title: 'Full Stack Developer',
-      logo: null,
-      start: 'Apr. 2019',
-      end: 'Aug. 2019',
-    },
-    {
-      company: 'Outsourcing Wizards',
-      title: 'Full Stack Developer',
-      logo: null,
-      start: 'Jan. 2019',
-      end: 'Mar. 2019',
-    },
-    {
-      company: 'Infosoft Studio',
-      title: 'Full Stack Developer',
-      logo: logoInfosoftStudio,
-      start: 'Nov. 2014',
-      end: 'Sept. 2018',
-    },
-  ]
+    const cvFile = '/docs/cv.pdf'
 
-  return {
-    meta,
-    hero,
-    slidePhotos,
-    cvFile,
-    workExperiences,
-  }
+    const workExperiences = [
+        {
+            company: 'X-Team',
+            title: 'Senior Full Stack + WordPress Developer',
+            logo: logoXTeam,
+            start: 'Apr. 2023',
+            end: 'Present',
+        },
+        {
+            company: 'Epic Design Labs',
+            title: 'Senior Full Stack Developer',
+            logo: logoEpicDesignLabs,
+            start: 'Apr. 2020',
+            end: 'Feb. 2023',
+        },
+        {
+            company: 'MultiplyMii',
+            title: 'Senior Full Stack Developer',
+            logo: logoMultiplyMii,
+            start: 'Apr. 2020',
+            end: 'Sept. 2021',
+        },
+        {
+            company: 'MAKE Interactive BV',
+            title: 'Full Stack Developer',
+            logo: logoMakeDevelopment,
+            start: 'Apr. 2019',
+            end: 'Feb. 2020',
+        },
+        {
+            company: 'Orth Enterprises, LLC',
+            title: 'Full Stack Developer',
+            logo: null,
+            start: 'Apr. 2019',
+            end: 'Aug. 2019',
+        },
+        {
+            company: 'Outsourcing Wizards',
+            title: 'Full Stack Developer',
+            logo: null,
+            start: 'Jan. 2019',
+            end: 'Mar. 2019',
+        },
+        {
+            company: 'Infosoft Studio',
+            title: 'Full Stack Developer',
+            logo: logoInfosoftStudio,
+            start: 'Nov. 2014',
+            end: 'Sept. 2018',
+        },
+    ]
+
+    return {
+        meta,
+        hero,
+        slidePhotos,
+        cvFile,
+        workExperiences,
+    }
 }
 
 export default HomeData
