ea7326bb541c5dd40592deee5f850fc214d1cd5a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-11-19 15:08:29 +0800

    fix: 🐛 clean up unused code

--- src/components/Prose.tsx
+++
@@ -1,9 +0,0 @@
-import { IProseProps } from '@/interfaces/components'
-import clsx from 'clsx'
-
-// Prose component
-const Prose = ({ children, className }: IProseProps): React.ReactNode => {
-  return <div className={clsx('prose dark:prose-invert', className)}>{children}</div>
-}
-
-export default Prose
