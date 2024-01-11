1e6b7767c3855abac64999fdd708f2ef7acb0810
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-01-11 08:58:05 +0800

    fix: 🐛 remove WordPress REST API integrations

--- apps/portfolio/configs/env.ts
+++ apps/portfolio/configs/env.ts
@@ -1,5 +1,1 @@
-export const NEXT_CMS_WORDPRESS_URL: any =
-  process.env.NODE_ENV === 'production'
-    ? process.env.nextCmsWordPressUrl
-    : 'http://guy-romelle-magayano.lndo.site/graphql'
 export const LAZY_LOADING_DURATION: number = 1000
