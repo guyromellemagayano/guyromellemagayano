b7f545ae69e7b560fda1f0706a906407cd05a8f0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-01-19 08:25:26 +0800

    fix: 🐛 consolidate `lib` and `util` functions

--- apps/portfolio/utils/helpers.ts
+++ apps/portfolio/utils/helpers.ts
@@ -1,3 +1,16 @@
+/**
+ * Converts an array of strings into a URL slug.
+ * @param array - The array of strings to be converted.
+ * @returns The URL slug generated from the array.
+ */
+export const arrayToUrlSlug = (array: string[]): string => {
+  const slugArray = array.map(item =>
+    item.toLowerCase().replace(/[\s\W-]+/g, '/')
+  )
+
+  return slugArray.join('-')
+}
+
 export type TClampProps = {
   number: number
   min: number
