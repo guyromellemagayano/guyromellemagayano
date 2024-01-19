9b02bc4089f87d82ff8e53464f1e4b200c1d2571
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-01-19 08:31:28 +0800

    fix: 🐛 update portfolio app types

--- apps/portfolio/types/common.d.ts
+++ apps/portfolio/types/common.d.ts
@@ -1,4 +1,4 @@
-import { THeroCommonProps, TSeoMetaCommonProps } from './common'
+import { ReactNode } from "react"
 
 // Base type for SEO metadata
 export type TSeoMetaCommonProps<T = object> = T & {
@@ -26,3 +26,29 @@ export type TGenericPageData<T = object> = TDataProps<T>
 export type TBaseCommonAppComponentProps<T = object> = T & {
   [key: string]: any
 }
+
+// Base type for props that include children
+export type TWithChildren<T = object> = T & {
+  children?: ReactNode
+}
+
+// Base type for props that include class name
+export type TWithClassName<T = object> = T & {
+  className?: string
+}
+
+// Base type for props that can have an ID and className
+export type TWithIDAndClass<T = object> = T &
+  TWithChildren<T> &
+  TWithClassName<T> & {
+    id?: string
+  }
+
+// Props for generic components that represent a block structure
+export type TBlockProps<T = object> = T &
+  TWithIDAndClass<T> & {
+    as?: keyof JSX.IntrinsicElements
+  }
+
+// Props for generic containers with optional children, id, and className
+export type TContainerProps<T = object> = T & TWithIDAndClass<T>
