3d23108c8d05091f2924ea238be17deefce1dee0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 07:39:38 +0800

    fix: 🐛 update About component markup

--- apps/portfolio/components/app/About.tsx
+++ apps/portfolio/components/app/About.tsx
@@ -1,47 +1,62 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import Container from '@/components/Container'
 import ContentLayout from '@/components/layouts/Content'
 import ImageLayout from '@/components/layouts/Image'
 import SocialLinksList from '@/components/list/SocialLinks'
 
-import type { TAboutData } from '@/data/about'
 import SocialLinksData from '@/data/social-links'
 
-import imagePortrait from '@/images/portrait.jpg'
+import type { TCommonComponentProps, TCommonPageData } from '@/types/common'
 
-type TAboutApp = (data: TAboutData) => ReactNode
+import imagePortrait from '@/images/portrait.jpg'
+import { isArrayType, isEmpty } from '@/utils/checkTypes'
+
+export type TAboutDataProps = {
+  data: TCommonPageData
+}
+
+export type TAboutAppProps = TCommonComponentProps & TAboutDataProps
 
 /**
  * Renders the about page.
+ * @param id The about page id.
  * @param data The about page data.
+ * @param rest The about page props.
  * @returns The about page component.
  */
-const AboutApp: TAboutApp = data => {
-  const heading = data?.hero?.heading || '',
-    description = data?.hero?.description || []
+const AboutApp: FC<TAboutAppProps> = ({ id, data, ...rest }) => {
+  const customId = useId()
 
   return (
-    <Container id="hero" className="mt-16 sm:mt-32">
+    <Container id={id || customId} {...rest}>
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:pl-20">
           <div className="max-w-xs px-2.5 lg:max-w-none">
-            <ImageLayout
-              src={imagePortrait}
-              alt=""
-              sizes="(min-width: 1024px) 32rem, 20rem"
-              className="aspect-square rotate-3 rounded-2xl object-cover"
-              priority
-            />
+            {!isEmpty(imagePortrait) && (
+              <ImageLayout
+                src={imagePortrait}
+                alt=""
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-square rotate-3 rounded-2xl object-cover"
+                priority
+              />
+            )}
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
-          <ContentLayout title={heading} intro={description} layout="aside" />
+          <ContentLayout
+            title={data?.hero?.heading || ''}
+            intro={data?.hero?.description || []}
+            layout="aside"
+          />
         </div>
         <div className="lg:pl-20">
-          <SocialLinksList data={SocialLinksData} />
+          {isArrayType(SocialLinksData) && !isEmpty(SocialLinksData) && (
+            <SocialLinksList data={SocialLinksData} />
+          )}
         </div>
       </div>
     </Container>
