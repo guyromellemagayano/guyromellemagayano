9f6f44358b693b192fbb885bd181edd9b798b08e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 07:43:46 +0800

    fix: 🐛 update Projects component markup

--- apps/portfolio/components/app/Projects.tsx
+++ apps/portfolio/components/app/Projects.tsx
@@ -1,28 +1,36 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import ContentLayout from '@/components/layouts/Content'
 import ProjectsList from '@/components/list/Projects'
 
 import { TProjectsData } from '@/data/projects'
 
-type TProjectsApp = (data: TProjectsData) => ReactNode
+import type { TCommonComponentProps } from '@/types/common'
+
+export type TProjectsAppDataProps = {
+  data: TProjectsData
+}
+
+export type TProjectsAppProps = TCommonComponentProps & TProjectsAppDataProps
 
 /**
  * Renders the projects page.
+ * @param id The projects page id.
+ * @param data The projects page data.
+ * @param rest The projects page props
  * @returns The projects page component.
  */
-const ProjectsApp: TProjectsApp = data => {
-  const heading = data?.hero?.heading || '',
-    description = data?.hero?.description || []
+const ProjectsApp: FC<TProjectsAppProps> = ({ id, data, ...rest }) => {
+  const customId = useId()
 
   return (
     <ContentLayout
-      id="hero"
-      title={heading}
-      intro={description}
-      className="sm:px-8 mt-16 sm:mt-32"
+      id={id || customId}
+      title={data?.hero?.heading || ''}
+      intro={data?.hero?.description || []}
+      {...rest}
     >
       <ProjectsList>
         {/* {projects?.map(project => (
