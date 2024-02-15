44183e9401aac34ade8a1b248081be7f56243b39
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 07:48:00 +0800

    fix: 🐛 update Skills page markup

--- apps/portfolio/app/skills/page.tsx
+++ apps/portfolio/app/skills/page.tsx
@@ -1,3 +1,5 @@
+import { FC } from 'react'
+
 import { Metadata } from 'next'
 
 import SkillsApp from '@/components/app/Skills'
@@ -16,8 +18,8 @@ export const metadata: Metadata = {
  * Renders the about page.
  * @returns The about page component.
  */
-const Page = async (): Promise<JSX.Element> => {
-  return <SkillsApp {...data} />
+const Page: FC = async () => {
+  return <SkillsApp className="mt-16 sm:mt-32" data={data} />
 }
 
 export default Page
