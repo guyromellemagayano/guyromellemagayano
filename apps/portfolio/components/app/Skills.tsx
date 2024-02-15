477478f33c0ccff01fb981853e10e42f1439c476
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 07:48:20 +0800

    fix: 🐛 update Skills component markup

--- apps/portfolio/components/app/Skills.tsx
+++ apps/portfolio/components/app/Skills.tsx
@@ -1,46 +1,51 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import ContentLayout from '@/components/layouts/Content'
 import SkillsList from '@/components/list/Skills'
 import SkillsCardsList from '@/components/list/SkillsCards'
 
-import { TSkillsData } from '@/data/skills'
+import { TSkillsData, TSkillsDataProps, TSkillsItemsProps } from '@/data/skills'
 
-type TSkillsApp = (data: TSkillsData) => ReactNode
+import type { TCommonComponentProps } from '@/types/common'
+import { isArrayType, isEmpty } from '@/utils/checkTypes'
+
+export type TSkillsAppDataProps = {
+  data: TSkillsData
+}
+
+export type TSkillsAppProps = TCommonComponentProps & TSkillsAppDataProps
 
 /**
  * Renders the skills page.
+ * @param id The skills page id.
+ * @param data The skills page data.
+ * @param rest The skills page props.
  * @returns The skills page component.
  */
-const SkillsApp: TSkillsApp = data => {
-  const heading = data?.hero?.heading || '',
-    description = data?.hero?.description || [],
-    skills = data?.skills || []
+const SkillsApp: FC<TSkillsAppProps> = ({ id, data, ...rest }) => {
+  const customId = useId()
 
   return (
     <ContentLayout
-      id="hero"
-      title={heading}
-      intro={description}
-      className="mt-16 sm:mt-32"
+      id={id || customId}
+      title={data?.hero?.heading || ''}
+      intro={data?.hero?.description || []}
+      {...rest}
     >
       <div className="grid gap-y-12">
-        {skills?.map(skill => {
-          const name = skill?.name || ''
-          const items = skill?.items || []
-
-          return (
-            <SkillsList key={name} title={name}>
-              {items?.map(rest => {
-                const title = rest?.title || ''
-
-                return <SkillsCardsList key={title} {...rest} />
-              })}
+        {isArrayType(data?.skills) &&
+          !isEmpty(data?.skills) &&
+          data.skills.map((skill: TSkillsDataProps, index) => (
+            <SkillsList key={index} title={skill?.name || ''}>
+              {isArrayType(skill?.items) &&
+                !isEmpty(skill?.items) &&
+                skill.items.map((rest: TSkillsItemsProps, index) => (
+                  <SkillsCardsList key={index} {...rest} />
+                ))}
             </SkillsList>
-          )
-        })}
+          ))}
       </div>
     </ContentLayout>
   )
