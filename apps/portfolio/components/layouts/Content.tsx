32bebcff3969305d5826211b1dd650154ac4be0d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 13:12:21 +0800

    fix: 🐛 update `Content` component types, props, docs

--- apps/portfolio/components/layouts/Content.tsx
+++ apps/portfolio/components/layouts/Content.tsx
@@ -1,123 +1,41 @@
 'use client'
 
-import { Key, ReactNode } from 'react'
+import { ElementType, FC, useId } from 'react'
 
 import { clsx } from 'clsx'
 import { usePathname } from 'next/navigation'
 
 import Container from '@/components/Container'
 
-import { isArrayType } from '@/utils/checkTypes'
+import { isArrayType, isEmpty, isStringType } from '@/utils/checkTypes'
 
-import type { TWithClassName, TWithIDAndClass } from '@/types/common'
+import type {
+  TCommonComponentReturnType,
+  TContainerProps
+} from '@/types/common'
 
-export type TContentLayoutProps<T = object> = T &
-  TWithClassName<T> &
-  TWithIDAndClass<T> & {
-    as?: React.ElementType
-    title?: string | null
-    intro?: string[] | string | null
-    layout?: 'simple' | 'aside'
-  }
-
-/**
- * Rendersthe simple layout content.
- * @param props - The props object.
- * @param props.id - The id of the layout.
- * @param props.title - The title of the layout.
- * @param props.intro - The intro of the layout.
- * @param props.children - The children of the layout.
- * @returns The simple layout content component.
- */
-const ContentSimple = (props: TContentLayoutProps): ReactNode => {
-  const pathname = usePathname()
-
-  return (
-    <Container id="hero">
-      <div className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          {props?.title || ''}
-        </h1>
-        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          {isArrayType(props?.intro) && props?.intro?.length > 0
-            ? props.intro.map(
-                (paragraph: string, index: Key | null | undefined) => {
-                  return (
-                    <span key={index} className="space-y-7">
-                      {paragraph}
-                    </span>
-                  )
-                }
-              )
-            : props.intro}
-        </p>
-      </div>
-      <div
-        className={clsx(
-          props?.className || '',
-          pathname !== '/' ? 'mt-16 sm:mt-20' : ''
-        )}
-      >
-        {props?.children || ''}
-      </div>
-    </Container>
-  )
+export type TContentLayoutProps = TContainerProps & {
+  as?: ElementType
+  title?: string
+  intro?: string[] | string
+  layout?: 'simple' | 'aside'
 }
 
 /**
- * Rendersthe aside layout content.
- * @param {Object} props - The props object.
- * @param {String} props.id - The id of the layout.
- * @param {String} props.title - The title of the layout.
- * @param {String} props.intro - The intro of the layout.
- * @param {ReactNode} props.children - The children of the layout.
- * @returns The aside layout content component.
- */
-const ContentAside = (props: TContentLayoutProps): JSX.Element => {
-  return (
-    <>
-      <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-        {props?.title || ''}
-      </h1>
-
-      {isArrayType(props?.intro) && props?.intro?.length > 0
-        ? props.intro.map(
-            (paragraph: string, index: Key | null | undefined) => {
-              return (
-                <p
-                  key={index}
-                  className="mt-6 text-base text-zinc-600 dark:text-zinc-400"
-                >
-                  {paragraph}
-                </p>
-              )
-            }
-          )
-        : props.intro}
-      <div className={clsx('mt-16 sm:mt-20', props?.className || '')}>
-        {props?.children || ''}
-      </div>
-    </>
-  )
-}
-
-/**
- * Rendersthe content layout.
- * @param {Object} props - The props object.
- * @param {String} props.as - The HTML tag or React component to render as the layout.
- * @param {String} props.className - The class name of the layout.
- * @param {String} props.id - The id of the layout.
- * @param {ReactNode} props.children - The children of the layout.
+ * Renders the content layout.
+ * @param as - The component type.
+ * @param children - The children of the content.
+ * @param layout - The layout of the content.
+ * @param rest - The rest of the props of the content.
  * @returns The content layout component.
  */
-const ContentLayout = ({
+const ContentLayout: FC<TContentLayoutProps> = ({
   as: Component = 'div',
-  className,
   children,
   layout = 'simple',
   ...rest
-}: TContentLayoutProps): JSX.Element => {
-  let render: JSX.Element | null = null
+}) => {
+  let render: TCommonComponentReturnType = null
 
   if (layout === 'aside') {
     render = <ContentAside {...rest}>{children}</ContentAside>
@@ -125,7 +43,100 @@ const ContentLayout = ({
     render = <ContentSimple {...rest}>{children}</ContentSimple>
   }
 
-  return <Component className={className}>{render}</Component>
+  return <Component>{render}</Component>
+}
+
+/**
+ * Renders the simple layout content.
+ * @param title - The title of the content.
+ * @param intro - The intro of the content.
+ * @param id - The id of the content.
+ * @param className - The class name of the content.
+ * @param children - The children of the content.
+ * @param rest - The rest of the props of the content.
+ * @returns The simple layout content component.
+ */
+const ContentSimple: FC<TContentLayoutProps> = ({
+  title,
+  intro,
+  id,
+  className,
+  children,
+  ...rest
+}) => {
+  const pathname = usePathname(),
+    customId = useId()
+
+  return (
+    <Container id={id || customId} {...rest}>
+      <div className="max-w-2xl">
+        {isStringType(title) && title?.length > 0 && (
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            {title}
+          </h1>
+        )}
+
+        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          {isArrayType(intro) && intro?.length > 0
+            ? intro.map((paragraph: string, index) => {
+                return (
+                  <span key={index} className="space-y-7">
+                    {paragraph}
+                  </span>
+                )
+              })
+            : intro}
+        </p>
+      </div>
+      <div className={clsx(pathname !== '/' && 'mt-16 sm:mt-20', className)}>
+        {children}
+      </div>
+    </Container>
+  )
+}
+
+/**
+ * Renders the aside layout content.
+ * @param title - The title of the content.
+ * @param intro - The intro of the content.
+ * @param id - The id of the content.
+ * @param className - The class name of the content.
+ * @param children - The children of the content.
+ * @param rest - The rest of the props of the content.
+ * @returns The aside layout content component.
+ */
+const ContentAside: FC<TContentLayoutProps> = ({
+  title,
+  intro,
+  id,
+  className,
+  children,
+  ...rest
+}) => {
+  const customId = useId()
+
+  return (
+    <aside id={id || customId} {...rest}>
+      {isStringType(title) && !isEmpty(title) && (
+        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+          {title}
+        </h1>
+      )}
+
+      <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+        {isArrayType(intro) && !isEmpty(intro)
+          ? intro.map((paragraph: string, index) => {
+              return (
+                <span key={index} className="space-y-7">
+                  {paragraph}
+                </span>
+              )
+            })
+          : intro}
+      </p>
+      <div className={clsx('mt-16 sm:mt-20', className)}>{children}</div>
+    </aside>
+  )
 }
 
 export default ContentLayout
