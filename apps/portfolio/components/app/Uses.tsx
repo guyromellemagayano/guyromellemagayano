13c9eb739095b5080f3a6dfee1282bfab65743f7
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 08:04:06 +0800

    fix: 🐛 update Uses component markup

--- apps/portfolio/components/app/Uses.tsx
+++ apps/portfolio/components/app/Uses.tsx
@@ -1,52 +1,53 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import ContentLayout from '@/components/layouts/Content'
 import ToolsList from '@/components/list/Tools'
 import ToolsListCards from '@/components/list/ToolsCards'
+
 import type { TUsesData } from '@/data/uses'
 
-type TUsesApp = (data: TUsesData) => ReactNode
+import { isArrayType, isEmpty } from '@/utils/checkTypes'
+
+import { TCommonComponentProps } from '@/types/common'
+
+export type TUsesAppDataProps = {
+  data: TUsesData
+}
+
+export type TUsesAppProps = TCommonComponentProps & TUsesAppDataProps
 
 /**
  * Renders the uses page.
  * @returns The uses page component.
  */
-const UsesApp: TUsesApp = data => {
-  const heading = data?.hero?.heading || '',
-    description = data?.hero?.description || [],
-    tools = data?.tools || []
+const UsesApp: FC<TUsesAppProps> = ({ id, data, ...rest }) => {
+  const customId = useId()
 
   return (
     <ContentLayout
-      id="hero"
-      title={heading}
-      intro={description}
-      className="sm:px-8 mt-16 sm:mt-32"
+      id={id || customId}
+      title={data?.hero?.heading || ''}
+      intro={data?.hero?.description || []}
+      {...rest}
     >
       <div className="space-y-20">
-        {tools?.map(tool => {
-          const name = tool?.name || ''
-          const items = tool?.items || []
-
-          return (
-            <ToolsList key={name} title={name}>
-              {items?.map(item => {
-                const title = item?.title || ''
-                const description = item?.description || ''
-
-                return (
+        {isArrayType(data?.tools) &&
+          !isEmpty(data?.tools) &&
+          data.tools.map((tool, index) => (
+            <ToolsList key={index} title={tool?.name || ''}>
+              {isArrayType(tool?.items) &&
+                !isEmpty(tool?.items) &&
+                tool.items.map((item, index2) => (
                   <ToolsListCards
-                    key={title}
-                    title={title}
-                    description={description}
+                    key={index2}
+                    title={item?.title || ''}
+                    description={item?.title || ''}
                   />
-                )
-              })}
+                ))}
             </ToolsList>
-          )
-        })}
+          ))}
       </div>
     </ContentLayout>
   )
