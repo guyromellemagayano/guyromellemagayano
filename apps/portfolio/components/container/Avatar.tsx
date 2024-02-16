5ad4a8e7f7ca359354aaec27c08d0e3afd383c09
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-16 12:47:52 +0800

    fix: 🐛 update `AvatarContainer` component issues

--- apps/portfolio/components/container/Avatar.tsx
+++ apps/portfolio/components/container/Avatar.tsx
@@ -1,31 +1,30 @@
 'use client'
 
-import { CSSProperties, ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import clsx from 'clsx'
 
-import type { TWithChildren, TWithClassName } from '@/types/common'
-
-export type TAvatarContainerProps<T = object> = T &
-  TWithClassName<T> &
-  TWithChildren<T> & { style?: CSSProperties }
-
-type TAvatarContainer = (props: TAvatarContainerProps) => ReactNode
+import type { TContainerProps } from '@/types/common'
 
 /**
- * Rendersavatar container component
- * @param className - Extend CSS classes to override default styling.
+ * Renders avatar container component
+ * @param id - The id of the component.
+ * @param className - The class name of the component.
  * @param children - The children to render.
  * @param rest - Other props to pass down to the component.
  * @returns The avatar container component.
  */
-const AvatarContainer: TAvatarContainer = ({
+const AvatarContainer: FC<TContainerProps> = ({
+  id,
   className,
   children,
   ...rest
 }) => {
+  const customId = useId()
+
   return (
     <div
+      id={id || customId}
       className={clsx(
         'h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10',
         className
