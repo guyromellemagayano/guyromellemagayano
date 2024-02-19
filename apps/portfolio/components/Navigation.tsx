1bbfa316e95fcd2bb24c9b360196258c058b7a87
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 02:09:47 +0800

    fix: 🐛 update `DesktopNavigation` component types, props, docs

--- apps/portfolio/components/Navigation.tsx
+++ apps/portfolio/components/Navigation.tsx
@@ -1,30 +1,39 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC } from 'react'
 
 import NavItem from '@/components/NavItem'
 
 import NavigationData from '@/data/navigation'
+
+import { isArrayType, isEmpty } from '@/utils/checkTypes'
+
+import { TCommonComponentProps } from '@/types/common'
+
+export type TDesktopNavigationProps = TCommonComponentProps
 
 /**
  * Renders the desktop navigation component.
  * @param rest - The props object.
  * @returns The rendered desktop navigation component.
  */
-const DesktopNavigation = (rest: any): ReactNode => {
+const DesktopNavigation: FC<TCommonComponentProps> = ({ ...rest }) => {
   // Destructure the data from the FooterData function
   const { headerNav } = NavigationData()
 
   return (
-    <nav {...rest}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        {headerNav.map(item => (
-          <NavItem key={item.link} href={item.link}>
-            {item.title}
-          </NavItem>
-        ))}
-      </ul>
-    </nav>
+    isArrayType(headerNav) &&
+    !isEmpty(headerNav) && (
+      <nav {...rest}>
+        <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+          {headerNav.map(({ link, title }, index) => (
+            <NavItem key={index} href={link || '#'}>
+              {title || ''}
+            </NavItem>
+          ))}
+        </ul>
+      </nav>
+    )
   )
 }
 
