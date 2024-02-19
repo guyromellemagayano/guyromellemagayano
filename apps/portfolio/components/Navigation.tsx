238843e66fa6eafe05d2c408a4aaf6991d6a4739
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 02:16:50 +0800

    fix: 🐛 additional updates on `DesktopNavigation` component

--- apps/portfolio/components/Navigation.tsx
+++ apps/portfolio/components/Navigation.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { FC } from 'react'
+import { FC, useId } from 'react'
 
 import NavItem from '@/components/NavItem'
 
@@ -17,14 +17,14 @@ export type TDesktopNavigationProps = TCommonComponentProps
  * @param rest - The props object.
  * @returns The rendered desktop navigation component.
  */
-const DesktopNavigation: FC<TCommonComponentProps> = ({ ...rest }) => {
-  // Destructure the data from the FooterData function
-  const { headerNav } = NavigationData()
+const DesktopNavigation: FC<TCommonComponentProps> = ({ id, ...rest }) => {
+  const { headerNav } = NavigationData(),
+    customId = useId()
 
   return (
     isArrayType(headerNav) &&
     !isEmpty(headerNav) && (
-      <nav {...rest}>
+      <nav id={id || customId} {...rest}>
         <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
           {headerNav.map(({ link, title }, index) => (
             <NavItem key={index} href={link || '#'}>
