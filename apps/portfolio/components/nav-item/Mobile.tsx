44167c0f411d8e48f7b678f10854a1aef00d4979
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 02:28:01 +0800

    fix: 🐛 update `MobileNavItem` component types, props, docs

--- apps/portfolio/components/nav-item/Mobile.tsx
+++ apps/portfolio/components/nav-item/Mobile.tsx
@@ -1,21 +1,37 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import { Popover } from '@headlessui/react'
 import Link from 'next/link'
 
 import type { TNavItemProps } from '@/components/NavItem'
 
+export type TMobileNavItemProps = TNavItemProps
+
 /**
  * Renders the mobile nav item component.
  * @param href - The href of the mobile nav item.
+ * @param id - The ID of the mobile nav item.
  * @param children - The children of the mobile nav item.
  * @returns The rendered mobile nav item component.
  */
-const MobileNavItem = ({ href, children }: TNavItemProps): ReactNode => {
+const MobileNavItem: FC<TMobileNavItemProps> = ({
+  href,
+  id,
+  children,
+  ...rest
+}) => {
+  const customId = useId()
+
   return (
-    <Popover.Button as={Link} href={href} className="block py-2">
+    <Popover.Button
+      as={Link}
+      href={href}
+      id={id || customId}
+      className="block py-2"
+      {...rest}
+    >
       {children}
     </Popover.Button>
   )
