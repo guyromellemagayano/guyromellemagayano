7074a71926c105efa727304d286ce97bf056808a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 02:27:27 +0800

    fix: 🐛 update `Loading` app types, props, docs

--- apps/portfolio/app/loading.tsx
+++ apps/portfolio/app/loading.tsx
@@ -1,15 +1,17 @@
+import { FC } from 'react'
+
 /**
  * Render the loading page content.
  * @param page - The page to render.
  * @returns The loading page content component.
  */
-const Loading = (): JSX.Element => {
+const Loading: FC = () => {
   return (
-    <main className="flex min-h-full items-end justify-center px-6 py-24 sm:py-32 lg:px-8">
+    <div className="flex min-h-full items-end justify-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold">Loading...</p>
       </div>
-    </main>
+    </div>
   )
 }
 
