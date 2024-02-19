072620c8a4a8311b8b696bcb1f4f4e637894a3a4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 00:45:01 +0800

    fix: 🐛 update `ArticleCardsList` component types, props, docs

--- apps/portfolio/components/list/ArticleCards.tsx
+++ apps/portfolio/components/list/ArticleCards.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { FC, useId } from 'react'
+
 import type { TArticleProps } from '@/components/Article'
 import Card from '@/components/Card'
 
@@ -7,24 +9,40 @@ import { formatDate } from '@/utils/formatDate'
 
 /**
  * Renders the articles cards list component.
- * @param props - The article props.
+ * @param date - The date of the article.
+ * @param slug - The slug of the article.
+ * @param title - The title of the article.
+ * @param description - The description of the article.
+ * @param id - The additional ID for the component.
+ * @param rest - The rest of the props.
  * @returns The rendered articles cards list component.
  */
-const ArticlesCardsList = (props: TArticleProps): JSX.Element => {
+const ArticlesCardsList: FC<TArticleProps> = ({
+  date,
+  slug,
+  title,
+  description,
+  id,
+  ...rest
+}) => {
+  const customId = useId()
+
   return (
-    <article className="md:grid md:grid-cols-4 md:items-baseline">
+    <article
+      id={id || customId}
+      className="md:grid md:grid-cols-4 md:items-baseline"
+      {...rest}
+    >
       <Card.Eyebrow
         as="time"
         className="mt-1 hidden md:block text-zinc-400 dark:text-zinc-500"
       >
-        {formatDate(props?.date || '')}
+        {formatDate(date || '')}
       </Card.Eyebrow>
 
       <Card className="md:col-span-3">
-        <Card.Title href={`/articles/${props?.slug}`}>
-          {props?.title}
-        </Card.Title>
-        <Card.Description>{props?.description}</Card.Description>
+        <Card.Title href={`/articles/${slug}`}>{title}</Card.Title>
+        <Card.Description>{description}</Card.Description>
         <Card.Cta>Read article</Card.Cta>
       </Card>
     </article>
