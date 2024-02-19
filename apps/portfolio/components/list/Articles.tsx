1a2de27b145d985a30b810e3cb103b5bfb3ff342
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 00:51:35 +0800

    fix: 🐛 update `ArticlesList` component types, props, docs

--- apps/portfolio/components/list/Articles.tsx
+++ apps/portfolio/components/list/Articles.tsx
@@ -1,17 +1,21 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
-import type { TWithChildren } from '@/types/common'
+import type { TContainerProps } from '@/types/common'
+
+export type TArticlesListProps = TContainerProps
 
 /**
  * Renders the articles list component.
  * @param children - The children of the articles list.
  * @returns The rendered articles list component.
  */
-const ArticlesList = ({ children }: TWithChildren): ReactNode => {
+const ArticlesList: FC<TContainerProps> = ({ id, children, ...rest }) => {
+  const customId = useId()
+
   return (
-    <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+    <div id={id || customId} {...rest}>
       <div className="flex max-w-3xl flex-col space-y-16">{children}</div>
     </div>
   )
