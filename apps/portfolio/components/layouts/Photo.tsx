a2c0415761a245f83db465f4b9938a487f5fa9a2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 00:13:37 +0800

    fix: 🐛 update `Photo` component types, props, docs

--- apps/portfolio/components/layouts/Photo.tsx
+++ apps/portfolio/components/layouts/Photo.tsx
@@ -1,55 +1,69 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import clsx from 'clsx'
 import { StaticImport } from 'next/dist/shared/lib/get-img-props'
-import Image from 'next/image'
+
+import ImageLayout from '@/components/layouts/Image'
 
 import RotationsData from '@/data/rotations'
 
-import type { TWithChildren } from '@/types/common'
+import { isArrayType, isEmpty } from '@/utils/checkTypes'
 
-export type TPhotoLayoutData<T = object> = T & {
+import type {
+  TCommonAdditionalProps,
+  TWithClassName,
+  TWithID
+} from '@/types/common'
+
+export type TPhotoLayoutData = TCommonAdditionalProps & {
   alt: string
-  src: StaticImport | string | null
-  [key: string]: any
+  src: StaticImport | string | undefined
 }
 
-export type TPhotoLayoutProps<T = object> = T &
-  TWithChildren<T> & {
-    data?: Array<TPhotoLayoutData> | null
+export type TPhotoLayoutProps = TWithClassName &
+  TWithID &
+  TCommonAdditionalProps & {
+    data?: Array<TPhotoLayoutData>
   }
 
 /**
- * Rendersthe photos component.
+ * Renders the photos component.
  * @param data - The photos data.
+ * @param id - The photos ID.
+ * @param rest - The rest of the photos props.
  * @returns The rendered photos component.
  */
-const PhotoLayout = ({ data }: TPhotoLayoutProps): ReactNode => {
+const PhotoLayout: FC<TPhotoLayoutProps> = ({ data, id, ...rest }) => {
+  const customId = useId()
+
   return (
-    <div className="mt-16 sm:mt-20">
-      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {data?.map((image, index) => (
-          <div
-            key={index}
-            className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
-              RotationsData[index % RotationsData.length]
-            )}
-          >
-            <Image
-              src={image.src || '#'}
-              alt={image.alt || ''}
-              sizes="(min-width: 640px) 18rem, 11rem"
-              className="absolute inset-0 h-full w-full object-cover"
-              unoptimized
-              priority
-            />
-          </div>
-        ))}
+    isArrayType(data) &&
+    !isEmpty(data) && (
+      <div id={id || customId} {...rest}>
+        <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+          {data?.map(({ src, alt }, index) => (
+            <div
+              key={index}
+              className={clsx(
+                'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+                RotationsData[index % RotationsData.length]
+              )}
+            >
+              <ImageLayout
+                src={src}
+                alt={alt}
+                sizes="(min-width: 640px) 18rem, 11rem"
+                className="absolute inset-0 h-full w-full object-cover"
+                unoptimized
+                priority
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    )
   )
 }
 
