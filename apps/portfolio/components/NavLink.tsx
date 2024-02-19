827358fb0bd0fd4e4ed760e8f1f03044597fe094
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 02:19:59 +0800

    fix: 🐛 update `NavLink` component types, props, docs

--- apps/portfolio/components/NavLink.tsx
+++ apps/portfolio/components/NavLink.tsx
@@ -1,25 +1,38 @@
 'use client'
 
-import { ReactNode } from 'react'
+import { FC, useId } from 'react'
 
 import Link from 'next/link'
 
 import type { TNavItemProps } from '@/components/NavItem'
 
+import { isEmpty, isObjectType, isStringType } from '@/utils/checkTypes'
+
+export type TNavLinkProps = TNavItemProps
+
 /**
  * Renders the nav link component.
  * @param href - The URL to link to.
+ * @param id - The ID of the nav link.
  * @param children - The content to render inside the link.
+ * @param rest - The props object.
  * @returns The rendered nav link component.
  */
-const NavLink = ({ href, children }: TNavItemProps): ReactNode => {
+const NavLink: FC<TNavLinkProps> = ({ href, id, children, ...rest }) => {
+  const customId = useId()
+
   return (
-    <Link
-      href={href}
-      className="transition hover:text-amber-500 dark:hover:text-amber-400"
-    >
-      {children}
-    </Link>
+    (isStringType(href) || isObjectType(href)) &&
+    !isEmpty(href) && (
+      <Link
+        href={href}
+        id={id || customId}
+        className="transition hover:text-amber-500 dark:hover:text-amber-400"
+        {...rest}
+      >
+        {children}
+      </Link>
+    )
   )
 }
 
