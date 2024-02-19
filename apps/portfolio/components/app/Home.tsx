88d1103f3817358eb619386a5aa9e16b1f0ef728
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 00:25:23 +0800

    fix: 🐛 update `Resume` component types, props, docs

--- apps/portfolio/components/app/Home.tsx
+++ apps/portfolio/components/app/Home.tsx
@@ -57,6 +57,7 @@ const HomeApp: FC<THomeAppProps> = ({ id, data, ...rest }) => {
             <ResumeLayout
               workExperiences={data?.workExperiences || []}
               cvFile={data?.cvFile || ''}
+              className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
             />
             <NewsletterLayout />
           </div>
