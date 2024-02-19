3383cad5128ad83574390836bdf337273d2709e6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-20 03:35:11 +0800

    fix: 🐛 update common type declarations

--- apps/portfolio/types/common.d.ts
+++ apps/portfolio/types/common.d.ts
@@ -1,66 +1,68 @@
-import { ReactNode } from "react"
+import { CSSProperties, FC, HTMLAttributes, JSX, ReactNode } from 'react'
 
 // Base type for additional props
-export type TCommonAdditionalProps<T = object> = T & {
+export type TCommonAdditionalProps = {
   [key: string]: any
 }
 
 // Base type for SEO metadata
-export type TSeoMetaCommonProps<T = object> = T & {
-  title: string | null
-  description?: string | null
-  keywords?: string | null
+export type TSeoMetaCommonProps = {
+  title: string
+  description?: string
+  keywords?: string
 }
 
 // Base type for hero components
-export type THeroCommonProps<T = object> = T & {
-  heading: string | null
-  description: string[] | null
+export type THeroCommonProps = {
+  heading?: string
+  description?: string[]
 }
 
-// Common data props
-export type TDataProps<T = object> = T & {
-  meta: TSeoMetaCommonProps
-  hero: THeroCommonProps
+export type TCommonPageData = {
+  meta?: TSeoMetaCommonProps
+  hero?: THeroCommonProps
 }
 
 // Data props for pages without specific data requirements
-export type TGenericPageData<T = object> = TDataProps<T>
+export type TCommonComponentProps = TWithID &
+  TWithClassName &
+  TCommonAdditionalProps &
+  TWithStyle
 
-// Base type for components with common app props
-export type TBaseCommonAppComponentProps<T = object> = T & {
-  [key: string]: any
+// Props for generic containers with optional children, id, and className
+export type TContainerProps = TCommonComponentProps & TWithChildren
+
+// Base type for props that include style object
+export type TWithStyle = {
+  style?: CSSProperties
 }
 
 // Base type for props that include children
-export type TWithChildren<T = object> = T & {
+export type TWithChildren = {
   children?: ReactNode
 }
 
 // Base type for props that include class name
-export type TWithClassName<T = object> = T & {
+export type TWithClassName = {
   className?: string
 }
 
-// Base type for props that can have an ID and className
-export type TWithIDAndClass<T = object> = T &
-  TWithChildren<T> &
-  TWithClassName<T> & {
-    id?: string
-  }
+// Base type for props that include an ID
+export type TWithID = {
+  id?: string
+}
 
 // Props for generic components that represent a block structure
-export type TBlockProps<T = object> = T &
-  TWithIDAndClass<T> & {
-    as?: keyof JSX.IntrinsicElements
+export type TBlockProps = HTMLAttributes<HTMLElement> &
+  TContainerProps & {
+    as?: keyof JSX.IntrinsicElements | FC<any>
+    large?: boolean
   }
-
-// Props for generic containers with optional children, id, and className
-export type TContainerProps<T = object> = T & TWithIDAndClass<T>
 
 // Props for common SVG images
 export type TCommonSvgImageProps = (
-  rest: TCommonAdditionalProps & {
-    rest: SVGProps<SVGSVGElement>
-  }
-) => ReactNode
+  rest: SVGProps<SVGSVGElement>
+) => TCommonComponentReturnType
+
+// Props for common component return types
+export type TCommonComponentReturnType = ReactNode | null
