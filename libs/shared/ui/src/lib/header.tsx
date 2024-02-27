addd17b707e29a9048101bef2ea17137fbfe42ed
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-27 10:39:46 +0800

    feat: 🎸 add shared header ui component, update imports

---
+++ libs/shared/ui/src/lib/header.tsx
@@ -0,0 +1,35 @@
+import { ForwardedRef, forwardRef, HTMLAttributes, useId } from 'react'
+
+import styled from '@emotion/styled'
+
+/* eslint-disable-next-line */
+export interface SharedHeaderUiProps extends HTMLAttributes<HTMLElement> {}
+
+export type HeaderRef = HTMLElement
+
+const StyledSharedHeaderUi = styled.header``
+
+/**
+ * Render the shared header component.
+ * @param children - The children of the shared header.
+ * @param rest - The rest of the props of the shared header.
+ * @param ref - Ref forwarding from parent component.
+ * @returns The rendered shared header component.
+ */
+export const SharedHeaderUi = forwardRef<HeaderRef, SharedHeaderUiProps>(
+  ({ children, ...rest }, ref: ForwardedRef<HeaderRef>) => {
+    // Generates a unique ID that can be used for accessibility attributes
+    const customId = useId()
+
+    return (
+      <StyledSharedHeaderUi ref={ref} {...rest} id={rest.id ?? customId}>
+        {children}
+      </StyledSharedHeaderUi>
+    )
+  }
+)
+
+// Set a display name for debugging purposes
+SharedHeaderUi.displayName = 'Header'
+
+export default SharedHeaderUi
