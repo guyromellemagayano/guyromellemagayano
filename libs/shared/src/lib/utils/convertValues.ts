545924c287deb5662236e3caf6ba461862d52ec5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-02-28 18:54:56 +0800

    refactor: 💡 update util files, add `tailwind-merge` package

--- libs/shared/src/lib/utils/convertValues.ts
+++ libs/shared/src/lib/utils/convertValues.ts
@@ -14,85 +14,65 @@ import upperFirst from 'lodash/upperFirst'
 import { isEmpty } from './checkTypes'
 
 // Convert string to lowercase
-export const convertStringToLowercase = (str: string): string => {
-  return !isEmpty(str) ? toLower(str) : str
-}
+export const convertStringToLowercase = (str: string) =>
+  !isEmpty(str) ? toLower(str) : str
 
 // Convert string to uppercase
-export const convertStringToUppercase = (str: string): string => {
-  return !isEmpty(str) ? toUpper(str) : str
-}
+export const convertStringToUppercase = (str: string) =>
+  !isEmpty(str) ? toUpper(str) : str
 
 // Convert string to title case
-export const convertStringToTitleCase = (str: string): string => {
-  return !isEmpty(str) ? startCase(camelCase(str)) : str
-}
+export const convertStringToTitleCase = (str: string) =>
+  !isEmpty(str) ? startCase(camelCase(str)) : str
 
 // Convert string to camel case
-export const convertStringToCamelCase = (str: string): string => {
-  return !isEmpty(str) ? camelCase(str) : str
-}
+export const convertStringToCamelCase = (str: string) =>
+  !isEmpty(str) ? camelCase(str) : str
 
 // Convert string to snake case
-export const convertStringToSnakeCase = (str: string): string => {
-  return !isEmpty(str) ? snakeCase(str) : str
-}
+export const convertStringToSnakeCase = (str: string) =>
+  !isEmpty(str) ? snakeCase(str) : str
 
 // Convert string to kebab case
-export const convertStringToKebabCase = (str: string): string => {
-  return !isEmpty(str) ? kebabCase(str) : str
-}
+export const convertStringToKebabCase = (str: string) =>
+  !isEmpty(str) ? kebabCase(str) : str
 
 // Convert string to constant case
-export const convertStringToConstantCase = (str: string): string => {
-  return !isEmpty(str) ? toUpper(str).replace(/ /g, '_') : str
-}
+export const convertStringToConstantCase = (str: string) =>
+  !isEmpty(str) ? toUpper(str).replace(/ /g, '_') : str
 
 // Convert string to sentence case
-export const convertStringToSentenceCase = (str: string): string => {
-  return !isEmpty(str) ? upperFirst(toLower(str)) : str
-}
+export const convertStringToSentenceCase = (str: string) =>
+  !isEmpty(str) ? upperFirst(toLower(str)) : str
 
 // Convert string to number
-export const convertStringToNumber = (str: string): string | number => {
-  return !isEmpty(str) ? toNumber(str) : str
-}
+export const convertStringToNumber = (str: string) =>
+  !isEmpty(str) ? toNumber(str) : str
 
 // Convert string to boolean
-export const convertStringToBoolean = (str: string): string | boolean => {
-  return !isEmpty(str) ? str === 'true' : str
-}
+export const convertStringToBoolean = (str: string) =>
+  !isEmpty(str) ? str === 'true' : str
 
 // Convert string to array
-export const convertStringToArray = (str: string): string | any[] => {
-  return !isEmpty(str) ? toArray(str) : str
-}
+export const convertStringToArray = (str: string) =>
+  !isEmpty(str) ? toArray(str) : str
 
 // Convert string to object
-export const convertStringToObject = (str: string): string | object => {
-  try {
-    return !isEmpty(str) ? JSON.parse(str) : str
-  } catch {
-    return str
-  }
-}
+export const convertStringToObject = (str: string) =>
+  !isEmpty(str) ? JSON.parse(str) : str
 
 // Convert object to string
-export const convertObjectToString = (obj: object): object | string => {
-  return !isEmpty(obj) ? JSON.stringify(obj) : obj
-}
+export const convertObjectToString = (obj: object) =>
+  !isEmpty(obj) ? JSON.stringify(obj) : obj
 
 // Convert array to string
-export const convertArrayToString = (arr: any[]): string | any[] => {
-  return !isEmpty(arr) ? join(arr, ' ') : arr
-}
+export const convertArrayToString = (arr: any[]) =>
+  !isEmpty(arr) ? join(arr, ' ') : arr
 
 // Convert number to string
-export const convertNumberToString = (num: number): string | number => {
-  return !isEmpty(num) ? toString(num) : num
-}
+export const convertNumberToString = (num: number) =>
+  !isEmpty(num) ? toString(num) : num
 
 // Convert string to HTML
-export const convertStringToHTML = (str: string): string | React.ReactNode => {
-  return !isEmpty(str) ? unescape(str) : str
-}
+export const convertStringToHTML = (str: string) =>
+  !isEmpty(str) ? unescape(str) : str
