110f1f2a8e10ebf5897a0d3144acf16b37c36f13
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-04 17:26:01 +0800

    feat: 🎸 [TEST] update test `sentry` API endpoint

--- apps/portfolio/src/app/page.tsx
+++ apps/portfolio/src/app/page.tsx
@@ -1,9 +1,46 @@
+import * as Sentry from '@sentry/nextjs'
+
+import { SharedButton } from '@guy-romelle-magayano/components/server'
+
 /**
  * Renders the home page.
  * @returns The home page component.
  */
 const Page = async () => {
-  return <h1>Henlo</h1>
+  return (
+    <>
+      <SharedButton
+        onClick={() => {
+          Sentry.startSpan(
+            {
+              name: 'Example Frontend Span',
+              op: 'test'
+            },
+            async () => {
+              const res = await fetch('/api/sentry-example-api')
+              if (!res.ok) {
+                throw new Error('Sentry Example Frontend Error')
+              }
+            }
+          )
+        }}
+      />
+
+      <p>
+        Next, look for the error on the{' '}
+        <a href="https://guy-romelle-magayano.sentry.io/issues/?project=4504692968718336">
+          Issues Page
+        </a>
+        .
+      </p>
+      <p style={{ marginTop: '24px' }}>
+        For more information, see{' '}
+        <a href="https://docs.sentry.io/platforms/javascript/guides/nextjs/">
+          https://docs.sentry.io/platforms/javascript/guides/nextjs/
+        </a>
+      </p>
+    </>
+  )
 }
 
 export default Page
