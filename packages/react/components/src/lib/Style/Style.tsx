e268798df819c771de478c5ba95992a6d6cd4d24
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-19 13:36:21 +0800

    feat: 🎸 add `Style` shared React component

---
+++ packages/react/components/src/lib/Style/Style.tsx
@@ -0,0 +1,18 @@
+import { StyleHTMLAttributes } from 'react'
+
+export type StyleRef = HTMLStyleElement
+export type StyleProps = StyleHTMLAttributes<StyleRef>
+
+/**
+ * Render the style component.
+ * @param children - The children of the style.
+ * @param rest - The rest of the props of the style.
+ * @returns The rendered style component.
+ */
+const Style = ({ children, ...rest }: StyleProps) => {
+  return <style {...rest}>{children}</style>
+}
+
+Style.displayName = 'Style'
+
+export default Style
