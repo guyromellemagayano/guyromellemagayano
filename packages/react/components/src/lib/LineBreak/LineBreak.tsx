6c44ec19ae312be4ad96f5de9831700ae1512176
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-19 12:55:38 +0800

    feat: 🎸 add `LineBreak` shared React component

---
+++ packages/react/components/src/lib/LineBreak/LineBreak.tsx
@@ -0,0 +1,21 @@
+import { customId } from '@guy-romelle-magayano/react-utils/server'
+import { HTMLAttributes, forwardRef } from 'react'
+
+export type LineBreakRef = HTMLBRElement
+export type LineBreakProps = HTMLAttributes<LineBreakRef>
+
+/**
+ * Render the line break component.
+ * @param children - The children of the line break.
+ * @param rest - The rest of the props of the line break.
+ * @returns The rendered line break component.
+ */
+const LineBreak = forwardRef<LineBreakRef, LineBreakProps>(
+  ({ ...rest }, ref) => {
+    return <br ref={ref} {...rest} id={rest.id ?? customId} />
+  }
+)
+
+LineBreak.displayName = 'LineBreak'
+
+export default LineBreak
