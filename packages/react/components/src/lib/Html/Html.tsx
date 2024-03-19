d3805121640569c815a022084cc2d74fae38e225
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-19 12:34:50 +0800

    feat: 🎸 add `Html` shared React component

---
+++ packages/react/components/src/lib/Html/Html.tsx
@@ -0,0 +1,18 @@
+import { HtmlHTMLAttributes } from 'react'
+
+export type HtmlRef = HTMLHtmlElement
+export type HtmlProps = HtmlHTMLAttributes<HtmlRef>
+
+/**
+ * Render the HTML component.
+ * @param children - The children of the HTML.
+ * @param rest - The rest of the props of the HTML.
+ * @returns The rendered HTML component.
+ */
+const Html = ({ children, ...rest }: HtmlProps) => {
+  return <html {...rest}>{children}</html>
+}
+
+Html.displayName = 'Html'
+
+export default Html
