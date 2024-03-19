4cd0a0eb4cfb4920b7fd8d312f15986baf5301b1
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-19 12:03:23 +0800

    feat: 🎸 add `Button` shared React component

---
+++ packages/react/components/src/lib/button/Button.tsx
@@ -0,0 +1,27 @@
+import { ButtonHTMLAttributes, forwardRef } from 'react'
+
+import { customId } from '@guy-romelle-magayano/react-utils/server'
+
+export type ButtonRef = HTMLButtonElement
+export type ButtonProps = ButtonHTMLAttributes<ButtonRef>
+
+/**
+ * Render the button component.
+ * @param type - The type of the button.
+ * @param children - The children of the button.
+ * @param rest - The rest of the props of the button.
+ * @returns The rendered button component.
+ */
+const Button = forwardRef<ButtonRef, ButtonProps>(
+  ({ type = 'button', children, ...rest }, ref) => {
+    return (
+      <button ref={ref} type={type} {...rest} id={rest.id ?? customId}>
+        {children}
+      </button>
+    )
+  }
+)
+
+Button.displayName = 'Button'
+
+export default Button
--- packages/react/components/src/lib/button/index.tsx
+++ packages/react/components/src/lib/button/index.tsx
@@ -1,1 +1,1 @@
-export { SharedButton } from './SharedButton'
+export { default as Button } from './Button'
