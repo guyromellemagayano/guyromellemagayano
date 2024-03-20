190a2879464ae8927cd43ea627fb158882e8a19b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-20 10:50:23 +0800

    feat: 🎸 add `next` components in shared components

--- packages/react/components/src/lib/Script/Script.tsx
+++ packages/react/components/src/lib/Script/Script.tsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { ScriptHTMLAttributes, forwardRef } from 'react'
+import { ScriptHTMLAttributes } from 'react'
+
+import dynamic from 'next/dynamic'
+
+const NextScript = dynamic(() => import('next/script'))
 
 export type ScriptRef = HTMLScriptElement
-export type ScriptProps = ScriptHTMLAttributes<ScriptRef>
+export type ScriptProps = ScriptHTMLAttributes<ScriptRef> & {
+  isNextScript?: boolean
+}
 
 /**
  * Render the script component
@@ -11,15 +17,13 @@ export type ScriptProps = ScriptHTMLAttributes<ScriptRef>
  * @param rest - The rest of the props of the script.
  * @returns The rendered script component.
  */
-const Script = forwardRef<ScriptRef, ScriptProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <script ref={ref} {...rest}>
-        {children}
-      </script>
-    )
+const Script = ({ isNextScript = false, children, ...rest }: ScriptProps) => {
+  if (isNextScript) {
+    return <NextScript {...rest}>{children}</NextScript>
   }
-)
+
+  return <script {...rest}>{children}</script>
+}
 
 Script.displayName = 'Script'
 
