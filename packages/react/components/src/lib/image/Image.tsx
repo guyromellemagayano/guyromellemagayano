f775f67100879958603f59761d4b8e12a9e7afd7
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-20 10:50:23 +0800

    feat: 🎸 add `next` components in shared components

--- packages/react/components/src/lib/image/Image.tsx
+++ packages/react/components/src/lib/image/Image.tsx
@@ -2,18 +2,34 @@
 
 import { ImgHTMLAttributes, forwardRef } from 'react'
 
+import dynamic from 'next/dynamic'
+
+const NextImage = dynamic(() => import('next/image'))
+
 export type ImageRef = HTMLImageElement
-export type ImageProps = ImgHTMLAttributes<ImageRef>
+export type ImageProps = ImgHTMLAttributes<ImageRef> & {
+  isNextImage?: boolean
+  width?: number
+  height?: number
+}
 
 /**
  * Render the image component.
  * @param src - The source of the image.
  * @param alt - The alternate text of the image.
+ * @param isNextImage - The flag to use the next image.
  * @param rest - The rest of the props of the image.
  * @returns The rendered  image component.
  */
 const Image = forwardRef<ImageRef, ImageProps>(
-  ({ src = '#', alt = '', ...rest }, ref) => {
+  (
+    { src = '#', alt = '', width, height, isNextImage = false, ...rest },
+    ref
+  ) => {
+    if (isNextImage) {
+      return <NextImage ref={ref} src={src} alt={alt} {...rest} />
+    }
+
     return <img ref={ref} src={src} alt={alt} {...rest} />
   }
 )
