27ee0fa1d1799f3117553bd7ccaab396741aee2c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-20 10:50:23 +0800

    feat: 🎸 add `next` components in shared components

--- packages/react/components/src/lib/Hyperlink/Hyperlink.tsx
+++ packages/react/components/src/lib/Hyperlink/Hyperlink.tsx
@@ -2,18 +2,33 @@
 
 import { AnchorHTMLAttributes, forwardRef } from 'react'
 
+import dynamic from 'next/dynamic'
+
+const NextLink = dynamic(() => import('next/link'))
+
 export type HyperlinkRef = HTMLAnchorElement
-export type HyperlinkProps = AnchorHTMLAttributes<HyperlinkRef>
+export type HyperlinkProps = AnchorHTMLAttributes<HyperlinkRef> & {
+  isNextLink?: boolean
+}
 
 /**
  * Render the hyperlink component.
  * @param href - The URL of the hyperlink.
+ * @param isNextLink - The flag to use the next link.
  * @param children - The children of the hyperlink.
  * @param rest - The rest of the props of the hyperlink.
  * @returns The rendered hyperlink component.
  */
 const Hyperlink = forwardRef<HyperlinkRef, HyperlinkProps>(
-  ({ href = '#', children, ...rest }, ref) => {
+  ({ href = '#', isNextLink = false, children, ...rest }, ref) => {
+    if (isNextLink) {
+      return (
+        <NextLink ref={ref} href={href} {...rest}>
+          {children}
+        </NextLink>
+      )
+    }
+
     return (
       <a ref={ref} href={href} {...rest}>
         {children}
