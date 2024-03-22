992af75ca225458d221dbe26972633d220d32179
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-22 10:12:21 +0800

    fix: 🐛 shared React component import among SVG components

--- apps/portfolio/src/components/svg/ChevronDown.tsx
+++ apps/portfolio/src/components/svg/ChevronDown.tsx
@@ -1,4 +1,4 @@
-import { RC } from '@guy-romelle-magayano/react-components'
+import { SharedRC } from '@guy-romelle-magayano/react-components'
 
 import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/common'
 
@@ -9,15 +9,15 @@ import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/comm
  */
 const ChevronDownSvg = (rest: TCommonSvgImageProps) => {
   return (
-    <RC.Svg viewBox="0 0 8 6" aria-hidden="true" {...rest}>
-      <RC.Path
+    <SharedRC.Svg viewBox="0 0 8 6" aria-hidden="true" {...rest}>
+      <SharedRC.Path
         d="M1.75 1.75 4 4.25l2.25-2.5"
         fill="none"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </RC.Svg>
+    </SharedRC.Svg>
   )
 }
 
--- apps/portfolio/src/components/svg/ChevronRight.tsx
+++ apps/portfolio/src/components/svg/ChevronRight.tsx
@@ -1,4 +1,4 @@
-import { RC } from '@guy-romelle-magayano/react-components'
+import { SharedRC } from '@guy-romelle-magayano/react-components'
 
 import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/common'
 
@@ -9,14 +9,14 @@ import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/comm
  */
 const ChevronDownSvg = (rest: TCommonSvgImageProps) => {
   return (
-    <RC.Svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...rest}>
-      <RC.Path
+    <SharedRC.Svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...rest}>
+      <SharedRC.Path
         d="M6.75 5.75 9.25 8l-2.5 2.25"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </RC.Svg>
+    </SharedRC.Svg>
   )
 }
 
