cce2c9a9949e1d543101bc10ed9673b9e0e4d6c4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-22 10:12:21 +0800

    fix: 🐛 shared React component import among SVG components

--- apps/portfolio/src/components/svg/Mail.tsx
+++ apps/portfolio/src/components/svg/Mail.tsx
@@ -1,4 +1,4 @@
-import { RC } from '@guy-romelle-magayano/react-components'
+import { SharedRC } from '@guy-romelle-magayano/react-components'
 
 import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/common'
 
@@ -9,7 +9,7 @@ import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/comm
  */
 const MailSvg = (rest: TCommonSvgImageProps) => {
   return (
-    <RC.Svg
+    <SharedRC.Svg
       viewBox="0 0 24 24"
       fill="none"
       strokeWidth="1.5"
@@ -18,15 +18,15 @@ const MailSvg = (rest: TCommonSvgImageProps) => {
       aria-hidden="true"
       {...rest}
     >
-      <RC.Path
+      <SharedRC.Path
         d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
         className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
       />
-      <RC.Path
+      <SharedRC.Path
         d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
         className="stroke-zinc-400 dark:stroke-zinc-500"
       />
-    </RC.Svg>
+    </SharedRC.Svg>
   )
 }
 
--- apps/portfolio/src/components/svg/Sun.tsx
+++ apps/portfolio/src/components/svg/Sun.tsx
@@ -1,4 +1,4 @@
-import { RC } from '@guy-romelle-magayano/react-components'
+import { SharedRC } from '@guy-romelle-magayano/react-components'
 
 import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/common'
 
@@ -9,7 +9,7 @@ import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/comm
  */
 const SunSvg = (rest: TCommonSvgImageProps) => {
   return (
-    <RC.Svg
+    <SharedRC.Svg
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       strokeLinecap="round"
@@ -17,12 +17,12 @@ const SunSvg = (rest: TCommonSvgImageProps) => {
       aria-hidden="true"
       {...rest}
     >
-      <RC.Path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
-      <RC.Path
+      <SharedRC.Path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
+      <SharedRC.Path
         d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
         fill="none"
       />
-    </RC.Svg>
+    </SharedRC.Svg>
   )
 }
 
