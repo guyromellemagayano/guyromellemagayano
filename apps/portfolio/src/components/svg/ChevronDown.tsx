1c4fa91562a40585f3f3bb095bc7a8f418ccf561
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-27 09:35:12 +0800

    fix: 🐛 update shared component imports among SVG components

--- apps/portfolio/src/components/svg/ChevronDown.tsx
+++ apps/portfolio/src/components/svg/ChevronDown.tsx
@@ -1,23 +1,21 @@
-import { SharedRC } from '@guy-romelle-magayano/react-components'
-
-import { TCommonSvgImageProps } from '@guy-romelle-magayano/portfolio/types/common'
+import { Path, Svg, SvgProps } from '@guy-romelle-magayano/react-components'
 
 /**
  * Renders an SVG image of a chevron pointing down.
  * @param rest - Additional SVG props
  * @returns The rendered SVG image of a chevron pointing down.
  */
-const ChevronDownSvg = (rest: TCommonSvgImageProps) => {
+const ChevronDownSvg = (rest: SvgProps) => {
   return (
-    <SharedRC.Svg viewBox="0 0 8 6" aria-hidden="true" {...rest}>
-      <SharedRC.Path
+    <Svg viewBox="0 0 8 6" aria-hidden="true" {...rest}>
+      <Path
         d="M1.75 1.75 4 4.25l2.25-2.5"
         fill="none"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </SharedRC.Svg>
+    </Svg>
   )
 }
 
