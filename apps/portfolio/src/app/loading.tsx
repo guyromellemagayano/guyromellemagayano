7fd7920b55693f25344755b88f34ca5652de4f2d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-29 15:19:32 +0800

    fix: 🐛 missing `Loading` app features

--- apps/portfolio/src/app/loading.tsx
+++ apps/portfolio/src/app/loading.tsx
@@ -1,21 +1,23 @@
-import { SharedReactComponent } from '@guy-romelle-magayano/react-components/server'
+import { Div, P } from '@guy-romelle-magayano/react-components/server'
 
 /**
  * Render the loading page.
  * @returns The loading page component.
  */
 const Loading = async () => {
-  const loadingMessage = 'Loading...'
+  const strings = {
+    loading: 'Loading...'
+  }
 
   return (
-    <SharedReactComponent.Layout className="flex min-h-full items-end justify-center px-6 py-24 sm:py-32 lg:px-8">
-      <SharedReactComponent.Layout className="text-center">
-        <SharedReactComponent.Paragraph className="text-base font-semibold">
-          {loadingMessage}
-        </SharedReactComponent.Paragraph>
-      </SharedReactComponent.Layout>
-    </SharedReactComponent.Layout>
+    <Div className="flex min-h-full items-end justify-center px-6 py-24 sm:py-32 lg:px-8">
+      <Div className="text-center">
+        <P className="text-base font-semibold">{strings.loading}</P>
+      </Div>
+    </Div>
   )
 }
+
+Loading.displayName = 'Loading'
 
 export default Loading
