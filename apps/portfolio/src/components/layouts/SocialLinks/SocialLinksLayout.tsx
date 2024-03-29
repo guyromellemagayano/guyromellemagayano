8784b193b4d09db2f67d44cdfed64a7379aab347
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-29 17:00:58 +0800

    feat: 🎸 add `SocialLinksLayout` component features

---
+++ apps/portfolio/src/components/layouts/SocialLinks/SocialLinksLayout.tsx
@@ -0,0 +1,54 @@
+import { forwardRef } from 'react'
+
+import dynamic from 'next/dynamic'
+
+import {
+  DivisionProps,
+  DivisionRef
+} from '@guy-romelle-magayano/react-components/server'
+import { CommonComponentsProps } from '@guy-romelle-magayano/react-components/types'
+
+import { isEmpty } from '@guy-romelle-magayano/react-utils'
+
+import { SocialLinkProps } from '@guy-romelle-magayano/portfolio/components/Links/Social'
+
+// Dynamic imports
+const Div = dynamic(() =>
+  import('@guy-romelle-magayano/react-components/server').then(mod => mod.Div)
+)
+const SocialLink = dynamic(() =>
+  import('@guy-romelle-magayano/portfolio/components/Links/Social').then(
+    mod => mod.SocialLink
+  )
+)
+
+export type SocialLinksLayoutRef = DivisionRef
+export type SocialLinksLayoutProps = DivisionProps &
+  CommonComponentsProps & {
+    data: Array<SocialLinkProps>
+  }
+
+/**
+ * Render the social links layout component.
+ * @param data - The data of the social links layout.
+ * @param children - The children of the social links layout.
+ * @returns The rendered social links layout component.
+ */
+const SocialLinksLayout = forwardRef<
+  SocialLinksLayoutRef,
+  SocialLinksLayoutProps
+>(({ data, ...rest }, ref) => {
+  return (
+    !isEmpty(data) && (
+      <Div ref={ref} {...rest}>
+        {data.map((rest, index: number) => (
+          <SocialLink key={index} {...rest} />
+        ))}
+      </Div>
+    )
+  )
+})
+
+SocialLinksLayout.displayName = 'SocialLinksLayout'
+
+export default SocialLinksLayout
