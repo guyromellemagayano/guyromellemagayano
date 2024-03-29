1f59d2e3493d1a7da4616570e635d31b1226a7b3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-03-29 15:22:44 +0800

    feat: 🎸 static JSON-based API features, update deps

---
+++ apps/portfolio/src/types/data.d.ts
@@ -0,0 +1,115 @@
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+
+// Base type for SEO metadata
+export type BaseSeoData = {
+  title: string
+  description?: string
+  keywords?: string
+}
+
+// Base type for hero components
+export type BaseHeroData = {
+  heading?: string
+  description?: string[]
+}
+
+// Base type for page data
+export type BasePageData = {
+  meta?: BaseSeoData
+  hero?: BaseHeroData
+}
+
+// Pages data
+export type PagesData = {
+  title: string
+  link: string
+}
+
+// 404 page data
+export type NotFoundPageData = BasePageData
+
+// About page data
+export type AboutPageData = BasePageData
+
+// Articles page data
+export type ArticlesPageData = BasePageData
+
+// Projects page data
+export type ProjectsPageData = BasePageData
+
+// Thank you page data
+export type ThankYouPageData = BasePageData
+
+// Home page data
+export type HomePageData = BasePageData & {
+  slidePhotos?: Array<SlidePhotosData>
+  cvFile?: string
+  workExperiences?: Array<WorkExperienceData>
+}
+
+// Slide photos data
+export type SlidePhotosData = {
+  alt: string
+  src: string
+}
+
+// Skills page data
+export type SkillsPageData = BasePageData & {
+  skills?: Array<SkillsData>
+}
+
+// Skills data
+export type SkillsData = {
+  name: string
+  items?: Array<SkillsItemData>
+}
+
+// Skills item data
+export type SkillsItemData = {
+  title: string
+  skillLevel: number
+  description: Array<string>
+  technologies: Array<TechnologiesData>
+}
+
+// Technologies data
+export type TechnologiesData = {
+  name: string
+  link?: string
+}
+
+// Uses page data
+export type UsesPageData = BasePageData & {
+  tools: Array<ToolsData>
+}
+
+// Tools data
+export type ToolsData = {
+  name: string
+  items: Array<ToolsItemsData>
+}
+
+// Tools items data
+export type ToolsItemsData = {
+  title: string
+  description: string
+}
+
+// Work page data
+export type WorkPageData = BasePageData & {
+  workExperiences?: Array<WorkExperienceData>
+}
+
+// Work experience data
+export type WorkExperienceData = {
+  company?: string
+  duration?: string
+  country?: string
+  contributions?: string[]
+  skills?: string[]
+  title?: string
+  logo: string | StaticImport
+  alt: string
+  start?: string
+  end?: string
+}
