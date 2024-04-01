bfbd04fc3c77b9133d9524c0de954068863cf8af
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-01 15:52:59 +0800

    fix: 🐛 SVG exports and imports

--- apps/portfolio/src/components/svg/Sun.tsx
+++ apps/portfolio/src/components/svg/Sun.tsx
@@ -1,4 +1,4 @@
-import { Svg, SvgPath, SvgProps } from '@guy-romelle-magayano/react-components'
+import { Path, Svg, SvgProps } from '@guy-romelle-magayano/react-components'
 
 /**
  * Renders an SVG image of a sun.
@@ -15,8 +15,8 @@ const SunSvg = (rest: SvgProps) => {
       aria-hidden="true"
       {...rest}
     >
-      <SvgPath d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
-      <SvgPath
+      <Path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
+      <Path
         d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
         fill="none"
       />
