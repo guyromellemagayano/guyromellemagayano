da86cbb50e85ac795f80a810ee346fec10973a2e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-09 09:44:11 +0800

    fix: 🐛 missing `Card` component features

--- apps/portfolio/src/components/Card/Card.tsx
+++ apps/portfolio/src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import {
   ElementType,
   ForwardRefExoticComponent,
+  Ref,
   RefAttributes,
   forwardRef
 } from 'react'
@@ -9,13 +10,13 @@ import dynamic from 'next/dynamic'
 import Link from 'next/link'
 
 import {
-  DivisionProps,
-  DivisionRef
+  DivisionRef,
+  ParagraphProps,
+  ParagraphRef
 } from '@guy-romelle-magayano/react-components/server'
+import { CommonComponentsProps } from '@guy-romelle-magayano/react-components/types'
 
 import { cn } from '@guy-romelle-magayano/react-utils'
-
-import { ChevronRightSvg } from '@guy-romelle-magayano/portfolio/components/SVG'
 
 // Dynamic imports
 const Div = dynamic(() =>
@@ -32,9 +33,14 @@ const P = dynamic(() =>
 const Span = dynamic(() =>
   import('@guy-romelle-magayano/react-components/server').then(mod => mod.Span)
 )
+const ChevronRightSvg = dynamic(() =>
+  import('@guy-romelle-magayano/portfolio/components/SVG').then(
+    mod => mod.ChevronRightSvg
+  )
+)
 
-export type CardRef = DivisionRef
-export type CardProps = DivisionProps & {
+export type CardRef = Ref<any>
+export type CardProps = CommonComponentsProps & {
   as?: ElementType
 }
 export type CardCommonProps = CardProps & {
@@ -53,6 +59,36 @@ export type CardStaticComponents = {
 }
 
 /**
+ * Renders the card component.
+ * @param as - The HTML tag or React component to render as the card.
+ * @param className - Additional CSS classes to apply to the card.
+ * @param children - The content to render inside the card.
+ * @param rest - The rest of the props of the card.
+ * @returns The rendered card component.
+ */
+const Card = forwardRef<CardRef, CardProps>(
+  ({ as: Component = Div, className, children, ...rest }, ref) => {
+    return (
+      children && (
+        <Component
+          ref={ref}
+          {...rest}
+          className={cn('group relative flex flex-col items-start', className)}
+        >
+          {children}
+        </Component>
+      )
+    )
+  }
+) as ForwardRefExoticComponent<CardProps & RefAttributes<CardRef>> &
+  CardStaticComponents
+
+Card.displayName = 'Card'
+
+export type CardLinkRef = DivisionRef
+export type CardLinkProps = CardCommonProps
+
+/**
  * Renders the card link component.
  * @param href - The URL to link to.
  * @param title - The title of the link.
@@ -61,26 +97,31 @@ export type CardStaticComponents = {
  * @param rest - The rest of the props of the link.
  * @returns The rendered card link component.
  */
-const CardLink = forwardRef<CardRef, CardCommonProps>(
+const CardLink = forwardRef<CardLinkRef, CardLinkProps>(
   ({ href, title, target, children, ...rest }, ref) => {
     return (
-      <Div ref={ref} {...rest}>
-        <Div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
+      children && (
+        <Div ref={ref} {...rest}>
+          <Div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl dark:bg-zinc-800/50" />
 
-        {href && children ? (
-          <Link href={href} title={title} target={target}>
-            <Span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
-            <Span className="relative z-10">{children}</Span>
-          </Link>
-        ) : (
-          children
-        )}
-      </Div>
+          {href && children ? (
+            <Link href={href} title={title} target={target}>
+              <Span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+              <Span className="relative z-10">{children}</Span>
+            </Link>
+          ) : (
+            children
+          )}
+        </Div>
+      )
     )
   }
 )
 
 CardLink.displayName = 'CardLink'
+
+export type CardTitleRef = CardRef
+export type CardTitleProps = CardCommonProps
 
 /**
  * Renders the card title component.
@@ -92,34 +133,39 @@ CardLink.displayName = 'CardLink'
  * @param rest - The rest of the props of the title.
  * @returns The rendered card title component.
  */
-const CardTitle = forwardRef<CardRef, CardCommonProps>(
+const CardTitle = forwardRef<CardTitleRef, CardTitleProps>(
   (
     { as: Component = Heading, href, title, children, className, ...rest },
     ref
   ) => {
     return (
-      <Component
-        ref={ref}
-        as="h2"
-        {...rest}
-        className={cn(
-          'text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100',
-          className
-        )}
-      >
-        {href && children ? (
-          <Card.Link href={href} title={title}>
-            {children}
-          </Card.Link>
-        ) : (
-          children
-        )}
-      </Component>
+      children && (
+        <Component
+          ref={ref}
+          as="h2"
+          {...rest}
+          className={cn(
+            'text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100',
+            className
+          )}
+        >
+          {href && children ? (
+            <CardLink href={href} title={title}>
+              {children}
+            </CardLink>
+          ) : (
+            children
+          )}
+        </Component>
+      )
     )
   }
 )
 
 CardTitle.displayName = 'CardTitle'
+
+export type CardDescriptionRef = ParagraphRef
+export type CardDescriptionProps = ParagraphProps & CardCommonProps
 
 /**
  * Renders the card description component.
@@ -129,130 +175,117 @@ CardTitle.displayName = 'CardTitle'
  * @param rest - The rest of the props of the description.
  * @returns The rendered card description component.
  */
-const CardDescription = forwardRef<CardRef, CardCommonProps>(
+const CardDescription = forwardRef<CardDescriptionRef, CardDescriptionProps>(
   ({ children, className, ...rest }, ref) => {
     return (
-      <P
-        ref={ref}
-        {...rest}
-        className={cn(
-          'relative z-10 my-2 text-sm text-zinc-600 dark:text-zinc-400',
-          className
-        )}
-      >
-        {children}
-      </P>
+      children && (
+        <P
+          ref={ref}
+          {...rest}
+          className={cn(
+            'relative z-10 my-2 text-sm text-zinc-600 dark:text-zinc-400',
+            className
+          )}
+        >
+          {children}
+        </P>
+      )
     )
   }
 )
 
 CardDescription.displayName = 'CardDescription'
 
+export type CardCtaRef = DivisionRef
+export type CardCtaProps = CardCommonProps
+
 /**
  * Renders the card call to action component.
  * @param title - The title of the link.
  * @param children - The content to render inside the CTA.
- * @param id - The ID of the CTA.
  * @param className - Additional CSS classes to apply to the CTA.
  * @param href - The URL to link to.
  * @param rest - The rest of the props of the CTA.
  * @returns The rendered card CTA component.
  */
-const CardCta = forwardRef<CardRef, CardCommonProps>(
+const CardCta = forwardRef<CardCtaRef, CardCtaProps>(
   ({ href, title, children, className, ...rest }, ref) => {
     return (
-      <Div
-        ref={ref}
-        aria-hidden="true"
-        {...rest}
-        className={cn(
-          'relative z-10 mt-2 flex items-start text-sm font-medium text-amber-500',
-          className
-        )}
-      >
-        {href && children ? (
-          <Link
-            href={href}
-            title={title}
-            className="flex items-center transition hover:text-amber-600 dark:hover:text-amber-600"
-          >
-            {children}
-            <ChevronRightSvg className="ml-1 h-4 w-4 stroke-current" />
-          </Link>
-        ) : (
-          children
-        )}
-      </Div>
+      children && (
+        <Div
+          ref={ref}
+          aria-hidden="true"
+          {...rest}
+          className={cn(
+            'relative z-10 mt-2 flex items-start text-sm font-medium text-amber-500',
+            className
+          )}
+        >
+          {href && children ? (
+            <Link
+              href={href}
+              title={title}
+              className="flex items-center transition hover:text-amber-600 dark:hover:text-amber-600"
+            >
+              {children}
+
+              <ChevronRightSvg className="ml-1 h-4 w-4 stroke-current" />
+            </Link>
+          ) : (
+            children
+          )}
+        </Div>
+      )
     )
   }
 )
 
 CardCta.displayName = 'CardCta'
 
+export type CardEyebrowRef = CardRef
+export type CardEyebrowProps = CardCommonProps &
+  Pick<CardCommonProps, 'decorate'>
+
 /**
  * An eyebrow component that can be used inside a Card component.
- * @param as - The HTML tag or React component to render as the eyebrow.
  * @param [decorate=false] - Whether to decorate the eyebrow with a line.
  * @param className - Additional CSS classes to apply to the eyebrow.
  * @param children - The content to render inside the eyebrow.
  * @returns The rendered eyebrow component.
  */
-const CardEyebrow = forwardRef<CardRef, CardCommonProps>(
+const CardEyebrow = forwardRef<CardEyebrowRef, CardEyebrowProps>(
   (
-    { as: Component = P, decorate = false, children, className, ...rest },
+    { as: Component = P, decorate = false, className, children, ...rest },
     ref
   ) => {
     return (
-      <Component
-        ref={ref}
-        {...rest}
-        className={cn(
-          'relative z-10 my-1 flex items-center text-sm font-medium',
-          className,
-          decorate && 'pl-3'
-        )}
-      >
-        {decorate && (
-          <Span
-            className="absolute inset-y-0 left-0 flex items-center"
-            aria-hidden="true"
-          >
-            <Span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-          </Span>
-        )}
+      children && (
+        <Component
+          ref={ref}
+          {...rest}
+          className={cn(
+            'relative z-10 my-1 flex items-center text-sm font-medium',
+            className,
+            decorate && 'pl-3'
+          )}
+        >
+          {decorate && (
+            <Span
+              className="absolute inset-y-0 left-0 flex items-center"
+              aria-hidden="true"
+            >
+              <Span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+            </Span>
+          )}
 
-        {children}
-      </Component>
+          {children}
+        </Component>
+      )
     )
   }
 )
 
 CardEyebrow.displayName = 'CardEyebrow'
-
-/**
- * Renders the card component.
- * @param as - The HTML tag or React component to render as the card.
- * @param className - Additional CSS classes to apply to the card.
- * @param children - The content to render inside the card.
- * @param rest - The rest of the props of the card.
- * @returns The rendered card component.
- */
-const Card = forwardRef<CardRef, CardProps>(
-  ({ as: Component = Div, className, children, ...rest }, ref) => {
-    return (
-      <Component
-        ref={ref}
-        {...rest}
-        className={cn('group relative flex flex-col items-start', className)}
-      >
-        {children}
-      </Component>
-    )
-  }
-) as ForwardRefExoticComponent<CardProps & RefAttributes<CardRef>> &
-  CardStaticComponents
-
-Card.displayName = 'Card'
 
 Card.Link = CardLink
 Card.Title = CardTitle
