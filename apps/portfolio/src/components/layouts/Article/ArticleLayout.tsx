d89bf2dc6f9df257dfe2096e237eef205953a188
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-09 09:25:25 +0800

    fix: 🐛 update missing features in Articles page

--- apps/portfolio/src/components/layouts/Article/ArticleLayout.tsx
+++ apps/portfolio/src/components/layouts/Article/ArticleLayout.tsx
@@ -2,9 +2,9 @@
 
 import { forwardRef, useContext } from 'react'
 
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 
-import { Button } from '@guy-romelle-magayano/react-components'
 import {
   Article,
   Div,
@@ -19,11 +19,32 @@ import {
 import { cn, formatDate } from '@guy-romelle-magayano/react-utils'
 
 import { AppContext } from '@guy-romelle-magayano/portfolio/app/providers'
-import { Article as HomeArticleLayout } from '@guy-romelle-magayano/portfolio/components/Article'
-import { BaseContainer } from '@guy-romelle-magayano/portfolio/components/Containers/Base'
-import { Prose } from '@guy-romelle-magayano/portfolio/components/Prose'
-import { ArrowLeftSvg } from '@guy-romelle-magayano/portfolio/components/SVG'
 import { ArticlesData } from '@guy-romelle-magayano/portfolio/types/data'
+
+// Dynamic imports
+const Button = dynamic(() =>
+  import('@guy-romelle-magayano/react-components').then(mod => mod.Button)
+)
+const HomeArticleLayout = dynamic(() =>
+  import('@guy-romelle-magayano/portfolio/components/Article').then(
+    mod => mod.Article
+  )
+)
+const BaseContainer = dynamic(() =>
+  import('@guy-romelle-magayano/portfolio/components/Containers/Base').then(
+    mod => mod.BaseContainer
+  )
+)
+const Prose = dynamic(() =>
+  import('@guy-romelle-magayano/portfolio/components/Prose').then(
+    mod => mod.Prose
+  )
+)
+const ArrowLeftSvg = dynamic(() =>
+  import('@guy-romelle-magayano/portfolio/components/SVG').then(
+    mod => mod.ArrowLeftSvg
+  )
+)
 
 export type ArticleLayoutRef = DivisionRef
 export type ArticleLayoutProps = DivisionProps & {
@@ -32,6 +53,10 @@ export type ArticleLayoutProps = DivisionProps & {
   isHome?: boolean
 }
 export type ArticleProps = Pick<ArticlesData, 'title' | 'date' | 'description'>
+
+const strings = {
+  goBack: 'Go back to articles'
+}
 
 /**
  * Renders the article layout component.
@@ -51,10 +76,7 @@ const ArticleLayout = forwardRef<ArticleLayoutRef, ArticleLayoutProps>(
     const router = useRouter(),
       { previousPathname } = useContext(AppContext),
       dateNow = new Date(),
-      dateNowToString = dateNow.toISOString(),
-      strings = {
-        goBack: 'Go back to articles'
-      }
+      dateNowToString = dateNow.toISOString()
 
     let title: string = article?.title || '',
       date: string = article?.date || dateNowToString
@@ -76,51 +98,55 @@ const ArticleLayout = forwardRef<ArticleLayoutRef, ArticleLayoutProps>(
     }
 
     return (
-      <BaseContainer
-        ref={ref}
-        {...rest}
-        className={cn('mt-16 lg:mt-32', className)}
-      >
-        <Div className="xl:relative">
-          <Div className="mx-auto max-w-2xl">
-            {previousPathname && (
-              <Button
-                type="button"
-                onClick={() => router.back()}
-                aria-label={strings.goBack}
-                className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-              >
-                <ArrowLeftSvg className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-              </Button>
-            )}
-
-            <Article>
-              {title && date && (
-                <Header className="flex flex-col">
-                  <Heading
-                    as="h1"
-                    className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100"
-                  >
-                    {title}
-                  </Heading>
-                  <Time
-                    dateTime={date}
-                    className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
-                  >
-                    <Span>{formatDate(date)}</Span>
-                  </Time>
-                </Header>
+      title &&
+      date &&
+      children && (
+        <BaseContainer
+          ref={ref}
+          {...rest}
+          className={cn('mt-16 lg:mt-32', className)}
+        >
+          <Div className="xl:relative">
+            <Div className="mx-auto max-w-2xl">
+              {previousPathname && (
+                <Button
+                  type="button"
+                  onClick={() => router.back()}
+                  aria-label={strings.goBack}
+                  className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+                >
+                  <ArrowLeftSvg className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+                </Button>
               )}
 
-              {children && (
-                <Prose className="mt-8" data-mdx-content>
-                  {children}
-                </Prose>
-              )}
-            </Article>
+              <Article>
+                {title && date && (
+                  <Header className="flex flex-col">
+                    <Heading
+                      as="h1"
+                      className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100"
+                    >
+                      {title}
+                    </Heading>
+                    <Time
+                      dateTime={date}
+                      className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+                    >
+                      <Span>{formatDate(date)}</Span>
+                    </Time>
+                  </Header>
+                )}
+
+                {children && (
+                  <Prose className="mt-8" data-mdx-content>
+                    {children}
+                  </Prose>
+                )}
+              </Article>
+            </Div>
           </Div>
-        </Div>
-      </BaseContainer>
+        </BaseContainer>
+      )
     )
   }
 )
