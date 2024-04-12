a3f3a8a96e3d5447616d3a0e52c4c1a3733d3c68
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-12 15:43:56 +0800

    fix: 🐛 dynamic import issues in `ArticleLayout` component

--- apps/portfolio/src/components/layouts/Article/ArticleLayout.tsx
+++ apps/portfolio/src/components/layouts/Article/ArticleLayout.tsx
@@ -1,50 +1,36 @@
+/* eslint-disable sort-imports */
 'use client'
 
-import { forwardRef, useContext } from 'react'
+import { forwardRef, memo, useContext } from 'react'
 
-import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 
+import { Button } from '@guy-romelle-magayano/react-components'
 import {
   Article,
   Div,
-  DivisionProps,
-  DivisionRef,
   Header,
   Heading,
   Span,
-  Time
+  Time,
+  type DivisionProps,
+  type DivisionRef
 } from '@guy-romelle-magayano/react-components/server'
 
-import { cn, formatDate } from '@guy-romelle-magayano/react-utils'
+import {
+  cn,
+  formatDate,
+  isArrayType,
+  isEmpty,
+  isStringType
+} from '@guy-romelle-magayano/react-utils'
 
 import { AppContext } from '@guy-romelle-magayano/portfolio/app/providers'
+import { Article as HomeArticleLayout } from '@guy-romelle-magayano/portfolio/components/Article'
+import { BaseContainer } from '@guy-romelle-magayano/portfolio/components/Containers/Base'
+import { Prose } from '@guy-romelle-magayano/portfolio/components/Prose'
+import ArrowLeftSvg from '@guy-romelle-magayano/portfolio/components/SVG/ArrowLeft'
 import { ArticlesData } from '@guy-romelle-magayano/portfolio/types/data'
-
-// Dynamic imports
-const Button = dynamic(() =>
-  import('@guy-romelle-magayano/react-components').then(mod => mod.Button)
-)
-const HomeArticleLayout = dynamic(() =>
-  import('@guy-romelle-magayano/portfolio/components/Article').then(
-    mod => mod.Article
-  )
-)
-const BaseContainer = dynamic(() =>
-  import('@guy-romelle-magayano/portfolio/components/Containers/Base').then(
-    mod => mod.BaseContainer
-  )
-)
-const Prose = dynamic(() =>
-  import('@guy-romelle-magayano/portfolio/components/Prose').then(
-    mod => mod.Prose
-  )
-)
-const ArrowLeftSvg = dynamic(() =>
-  import('@guy-romelle-magayano/portfolio/components/SVG').then(
-    mod => mod.ArrowLeftSvg
-  )
-)
 
 export type ArticleLayoutRef = DivisionRef
 export type ArticleLayoutProps = DivisionProps & {
@@ -68,59 +54,59 @@ const strings = {
  * @param rest - The rest of the props of the article layout.
  * @returns The rendered article layout component.
  */
-const ArticleLayout = forwardRef<ArticleLayoutRef, ArticleLayoutProps>(
-  (
-    { article, articles, isHome = false, children, className, ...rest },
-    ref
-  ) => {
-    const router = useRouter(),
-      { previousPathname } = useContext(AppContext),
-      dateNow = new Date(),
-      dateNowToString = dateNow.toISOString()
+const ArticleLayout = memo(
+  forwardRef<ArticleLayoutRef, ArticleLayoutProps>(
+    (
+      { article, articles, isHome = false, children, className, ...rest },
+      ref
+    ) => {
+      const router = useRouter(),
+        { previousPathname } = useContext(AppContext),
+        dateNow = new Date(),
+        dateNowToString = dateNow.toISOString()
 
-    let title: string = article?.title || '',
-      date: string = article?.date || dateNowToString
+      const title: string = article?.title || '',
+        date: string = article?.date || dateNowToString
 
-    if (isHome && articles && !article) {
+      if (isHome && !isEmpty(articles) && isArrayType(articles) && !article) {
+        return (
+          <Div className="flex flex-col gap-16">
+            {articles
+              ?.map((article, index: number) => (
+                <HomeArticleLayout
+                  key={index}
+                  className="inset-y-6"
+                  {...article}
+                />
+              ))
+              ?.slice(0, 4)}
+          </Div>
+        )
+      }
+
       return (
-        <Div className="flex flex-col gap-16">
-          {articles
-            ?.map((article, index: number) => (
-              <HomeArticleLayout
-                key={index}
-                className="inset-y-6"
-                {...article}
-              />
-            ))
-            ?.slice(0, 4)}
-        </Div>
-      )
-    }
+        !isEmpty(title) &&
+        isStringType(title) &&
+        !isEmpty(children) && (
+          <BaseContainer
+            ref={ref}
+            {...rest}
+            className={cn('mt-16 lg:mt-32', className)}
+          >
+            <Div className="xl:relative">
+              <Div className="mx-auto max-w-2xl">
+                {previousPathname && (
+                  <Button
+                    type="button"
+                    onClick={() => router.back()}
+                    aria-label={strings.goBack}
+                    className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+                  >
+                    <ArrowLeftSvg className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+                  </Button>
+                )}
 
-    return (
-      title &&
-      date &&
-      children && (
-        <BaseContainer
-          ref={ref}
-          {...rest}
-          className={cn('mt-16 lg:mt-32', className)}
-        >
-          <Div className="xl:relative">
-            <Div className="mx-auto max-w-2xl">
-              {previousPathname && (
-                <Button
-                  type="button"
-                  onClick={() => router.back()}
-                  aria-label={strings.goBack}
-                  className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-                >
-                  <ArrowLeftSvg className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-                </Button>
-              )}
-
-              <Article>
-                {title && date && (
+                <Article>
                   <Header className="flex flex-col">
                     <Heading
                       as="h1"
@@ -128,27 +114,30 @@ const ArticleLayout = forwardRef<ArticleLayoutRef, ArticleLayoutProps>(
                     >
                       {title}
                     </Heading>
-                    <Time
-                      dateTime={date}
-                      className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
-                    >
-                      <Span>{formatDate(date)}</Span>
-                    </Time>
-                  </Header>
-                )}
 
-                {children && (
+                    {!isEmpty(date) && isStringType(date) && (
+                      <Time
+                        dateTime={date}
+                        className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+                      >
+                        <Span>{formatDate(date)}</Span>
+                      </Time>
+                    )}
+                  </Header>
+
                   <Prose className="mt-8" data-mdx-content>
                     {children}
                   </Prose>
-                )}
-              </Article>
+                </Article>
+              </Div>
             </Div>
-          </Div>
-        </BaseContainer>
+          </BaseContainer>
+        )
       )
-    )
-  }
+    }
+  )
 )
+
+ArticleLayout.displayName = 'ArticleLayout'
 
 export default ArticleLayout
