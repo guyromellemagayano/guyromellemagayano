85f6d22e80aad4bf7fd937a1156d33c79543e711
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-12 16:27:14 +0800

    fix: 🐛 dynamic import issues in `ProjectsList` component

--- apps/portfolio/src/components/Lists/Projects/ProjectsList.tsx
+++ apps/portfolio/src/components/Lists/Projects/ProjectsList.tsx
@@ -1,10 +1,10 @@
-import { forwardRef, memo } from 'react'
+import { forwardRef } from 'react'
 
-import type {
-  UnorderedListProps,
-  UnorderedListRef
+import {
+  Ul,
+  type UnorderedListProps,
+  type UnorderedListRef
 } from '@guy-romelle-magayano/react-components/server'
-import { Ul } from '@guy-romelle-magayano/react-components/server'
 
 import { cn, isEmpty } from '@guy-romelle-magayano/react-utils'
 
@@ -17,25 +17,23 @@ export type ProjectsListProps = UnorderedListProps
  * @param rest - The rest of the props.
  * @returns The rendered projects list component.
  */
-const ProjectsList = memo(
-  forwardRef<ProjectsListRef, ProjectsListProps>(
-    ({ className, children, ...rest }, ref) => {
-      return (
-        !isEmpty(children) && (
-          <Ul
-            ref={ref}
-            {...rest}
-            className={cn(
-              'grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3',
-              className
-            )}
-          >
-            {children}
-          </Ul>
-        )
+const ProjectsList = forwardRef<ProjectsListRef, ProjectsListProps>(
+  ({ className, children, ...rest }, ref) => {
+    return (
+      !isEmpty(children) && (
+        <Ul
+          ref={ref}
+          {...rest}
+          className={cn(
+            'grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3',
+            className
+          )}
+        >
+          {children}
+        </Ul>
       )
-    }
-  )
+    )
+  }
 )
 
 ProjectsList.displayName = 'ProjectsList'
