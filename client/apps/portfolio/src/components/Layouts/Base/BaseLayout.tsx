20216b7b3b6a94354bff0dfe27ed591c15fe85fe
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-19 19:36:45 +0800

    feat: :fire: update file and folder structure

---
+++ client/apps/portfolio/src/components/Layouts/Base/BaseLayout.tsx
@@ -0,0 +1,76 @@
+import { ReactNode } from 'react'
+
+import Script from 'next/script'
+
+import {
+  Body,
+  Div,
+  Head,
+  Html,
+  Main
+} from '@guy-romelle-magayano/react-components/server'
+
+import { Providers } from '@guy-romelle-magayano/portfolio/app/providers'
+import { FooterLayout } from '@guy-romelle-magayano/portfolio/components/Layouts/Footer'
+import { HeaderLayout } from '@guy-romelle-magayano/portfolio/components/Layouts/Header'
+import {
+  NEXT_PUBLIC_GA_MEASUREMENT_ID,
+  NEXT_PUBLIC_GA_MEASUREMENT_URL
+} from '@guy-romelle-magayano/portfolio/configs/env'
+import { navigationData } from '@guy-romelle-magayano/portfolio/utils/server'
+
+import '@guy-romelle-magayano/portfolio/styles/tailwind.css'
+
+import 'focus-visible'
+
+export type BaseLayoutProps = {
+  children: ReactNode
+}
+
+/**
+ * Render the base layout component.
+ * @param children - The children of the base layout.
+ * @returns The rendered base layout component.
+ */
+const BaseLayout = async ({ children }: BaseLayoutProps) => {
+  const { headerMenu, footerMenu } = await navigationData()
+
+  return (
+    <Html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src={NEXT_PUBLIC_GA_MEASUREMENT_URL}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', "${NEXT_PUBLIC_GA_MEASUREMENT_ID}");
+            `
+          }}
+        />
+      </Head>
+
+      <Body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <Div className="flex w-full">
+            <Div className="relative flex w-full flex-col">
+              <HeaderLayout pages={headerMenu} />
+              <Main className="flex-auto">{children}</Main>
+              <FooterLayout pages={footerMenu} />
+            </Div>
+          </Div>
+        </Providers>
+      </Body>
+    </Html>
+  )
+}
+
+BaseLayout.displayName = 'BaseLayout'
+
+export default BaseLayout
