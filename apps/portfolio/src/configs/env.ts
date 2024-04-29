adade911129b7f71e89b4ec77d977638a411e602
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-04-30 01:41:00 +0800

    fix: :pencil2: update env values and imports of `BaseLayout` component

--- apps/portfolio/src/configs/env.ts
+++ apps/portfolio/src/configs/env.ts
@@ -13,18 +13,18 @@ export const SENTRY_ENVIRONMENT: string = process.env.SENTRY_ENVIRONMENT || ''
 // Google Analytics
 export const GOOGLE_ANALYTICS_MEASUREMENT_ID: string =
   process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID || ''
-export const GOOGLE_ANALYTICS_MEASUREMENT_URL: string =
-  `https://www.googletagmanager.com/gtag/js` +
-    !isEmpty(GOOGLE_ANALYTICS_MEASUREMENT_ID) &&
+export const GOOGLE_ANALYTICS_MEASUREMENT_URL = `https://www.googletagmanager.com/gtag/js${
+  !isEmpty(GOOGLE_ANALYTICS_MEASUREMENT_ID) &&
   isStringType(GOOGLE_ANALYTICS_MEASUREMENT_ID)
     ? `?id=${GOOGLE_ANALYTICS_MEASUREMENT_ID}`
     : ''
+}`
 
 // Google Adsense
 export const GOOGLE_ADSENSE_CLIENT_ID: string =
   process.env.GOOGLE_ADSENSE_CLIENT_ID || ''
-export const GOOGLE_ADSENSE_MEASUREMENT_URL: string =
-  `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js` +
-    !isEmpty(GOOGLE_ADSENSE_CLIENT_ID) && isStringType(GOOGLE_ADSENSE_CLIENT_ID)
+export const GOOGLE_ADSENSE_MEASUREMENT_URL = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js${
+  !isEmpty(GOOGLE_ADSENSE_CLIENT_ID) && isStringType(GOOGLE_ADSENSE_CLIENT_ID)
     ? `?client=${GOOGLE_ADSENSE_CLIENT_ID}`
     : ''
+}`
