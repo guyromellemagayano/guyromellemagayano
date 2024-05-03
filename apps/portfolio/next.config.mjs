279b1561849e27d2d64cecee584b2ea7c5ec9344
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-03 17:46:49 +0800

    build: :wrench: update `withPWAInit` settings on `next` config

--- apps/portfolio/next.config.mjs
+++ apps/portfolio/next.config.mjs
@@ -119,8 +119,6 @@ const withMDX = createMDX({
 const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === 'development',
   dest: 'public',
-  scope: '/app',
-  sw: 'service-worker.js',
   publicExcludes: ['!favicon/**/*']
 })
 
