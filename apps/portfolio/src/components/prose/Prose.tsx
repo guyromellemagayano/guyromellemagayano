2e45dc8946dae49e5cea4134bef47a444dd99512
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-06 18:47:49 +0800

    refactor: :truck: rename `Prose` component, update imports

---
+++ apps/portfolio/src/components/prose/Prose.tsx
@@ -0,0 +1,39 @@
+import { forwardRef } from 'react'
+
+import {
+  Div,
+  type DivisionProps,
+  type DivisionRef
+} from '@guy-romelle-magayano/react-components/server'
+
+import { cn, isEmpty } from '@guy-romelle-magayano/react-utils'
+
+export type ProseRef = DivisionRef
+export type ProseProps = DivisionProps
+
+/**
+ * Renders the prose component.
+ * @param children - The children of the prose.
+ * @param className - The class name of the prose.
+ * @param rest - The rest of the prose props.
+ * @returns The rendered prose component.
+ */
+const Prose = forwardRef<ProseRef, ProseProps>(
+  ({ children, className, ...rest }, ref) => {
+    return (
+      !isEmpty(children) && (
+        <Div
+          ref={ref}
+          {...rest}
+          className={cn('prose dark:prose-invert', className)}
+        >
+          {children}
+        </Div>
+      )
+    )
+  }
+)
+
+Prose.displayName = 'Prose'
+
+export default Prose
