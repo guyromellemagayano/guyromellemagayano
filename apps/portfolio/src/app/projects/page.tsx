65d9473ffa12b93fe521c44a63bfde05bd627e9d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-06 17:29:32 +0800

    refactor: :truck: rename projects app page, update imports

--- apps/portfolio/src/app/projects/page.tsx
+++ apps/portfolio/src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next'
 
-import { ProjectsApp } from '@guy-romelle-magayano/portfolio/components/Apps/Projects'
+import { ProjectsApp } from '@guy-romelle-magayano/portfolio/components/apps/projects'
 import {
   projectsAppData,
   projectsPageData
