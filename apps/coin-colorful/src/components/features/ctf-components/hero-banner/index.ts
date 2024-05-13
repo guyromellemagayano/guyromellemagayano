5ff50eefd2b2df97236bd0b2c49338e13ad9ec87
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 05:09:28 +0800

    feat: :sparkles: add `HeroBannerCtfComponentFeature` and `HeroBannerGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/hero-banner/index.ts
@@ -0,0 +1,5 @@
+export type * from './HeroBannerCtfComponentFeature'
+export { default as HeroBannerCtfComponentFeature } from './HeroBannerCtfComponentFeature'
+export type * from './HeroBannerGqlCtfComponentFeature'
+export { default as HeroBannerGqlCtfComponentFeature } from './HeroBannerGqlCtfComponentFeature'
+export * from './__generated/hero-banner-ctf-component-feature.generated'
