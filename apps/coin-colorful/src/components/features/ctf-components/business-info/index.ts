3277a758e9759a51c341dc4dbeefc0cd3b122a84
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 03:44:48 +0800

    feat: :sparkles: add `BusinessInfoCtfComponentFeature` and `BusinessInfoGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/business-info/index.ts
@@ -0,0 +1,5 @@
+export type * from './BusinessInfoCtfComponentFeature'
+export { default as BusinessInfoCtfComponentFeature } from './BusinessInfoCtfComponentFeature'
+export type * from './BusinessInfoGqlCtfComponentFeature'
+export { default as BusinessInfoGqlCtfComponentFeature } from './BusinessInfoGqlCtfComponentFeature'
+export * from './__generated/business-info-ctf-component-feature.generated'
