e29ba8d56a9e730512a86df6fee7409c6468d93b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 03:31:16 +0800

    feat: :sparkles: add `AssetCtfComponentFeature` component and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/asset/index.ts
@@ -0,0 +1,3 @@
+export type * from './AssetCtfComponentFeature'
+export { default as AssetCtfComponentFeature } from './AssetCtfComponentFeature'
+export * from './__generated/asset-ctf-component-feature.generated'
