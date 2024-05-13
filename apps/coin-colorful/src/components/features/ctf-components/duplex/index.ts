f94339c0bd0707bf1748d38eeb504066725281b7
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 04:33:47 +0800

    feat: :sparkles: add `DuplexCtfComponentFeature` and `DuplexGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/duplex/index.ts
@@ -0,0 +1,5 @@
+export type * from './DuplexCtfComponentFeature'
+export { default as DuplexCtfComponentFeature } from './DuplexCtfComponentFeature'
+export type * from './DuplexGqlCtfComponentFeature'
+export { default as DuplexGqlCtfComponentFeature } from './DuplexGqlCtfComponentFeature'
+export * from './__generated/duplex-ctf-component-feature.generated'
