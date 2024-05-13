8f1a885db740dd2aeb7cf8b25d20b348fe0e5cd5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 05:56:08 +0800

    feat: :sparkles: add `MobileMenuCtfComponentFeature` and `MobileMenuGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/mobile-menu/index.ts
@@ -0,0 +1,4 @@
+export type * from './MobileMenuCtfComponentFeature'
+export { default as MobileMenuCtfComponentFeature } from './MobileMenuCtfComponentFeature'
+export type * from './MobileMenuGqlCtfComponentFeature'
+export { default as MobileMenuGqlCtfComponentFeature } from './MobileMenuGqlCtfComponentFeature'
