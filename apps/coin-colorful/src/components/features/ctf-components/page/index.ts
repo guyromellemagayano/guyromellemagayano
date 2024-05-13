7eaa3f520d3a6a0fefd2c7fd264155b73dedea4b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 06:18:37 +0800

    feat: :sparkles: add `PageCtfComponentFeature` and `PageGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/page/index.ts
@@ -0,0 +1,5 @@
+export type * from './PageCtfComponentFeature'
+export { default as PageCtfComponentFeature } from './PageCtfComponentFeature'
+export type * from './PageGqlCtfComponentFeature'
+export { default as PageGqlCtfComponentFeature } from './PageGqlCtfComponentFeature'
+export * from './__generated/page-ctf-component-feature.generated'
