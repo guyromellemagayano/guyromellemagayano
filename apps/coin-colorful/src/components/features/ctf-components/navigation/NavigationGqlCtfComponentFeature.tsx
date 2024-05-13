590c92944a24ff7e9e3ad916c204cba0660f60b3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 06:05:40 +0800

    feat: :sparkles: add `NavigationCtfComponentFeature` and `NavigationGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/navigation/NavigationGqlCtfComponentFeature.tsx
@@ -0,0 +1,38 @@
+import { useContentfulLiveUpdates } from '@contentful/live-preview/react'
+
+import {
+  NavigationCtfComponentFeature,
+  useCtfNavigationQuery
+} from '@guy-romelle-magayano/coin-colorful/components'
+import { useContentfulContext } from '@guy-romelle-magayano/coin-colorful/hooks'
+
+/**
+ * Renders the navigation `graphql` feature component for the `contentful` API to consume.
+ * @returns The rendered feature component.
+ */
+const NavigationGqlCtfComponentFeature = () => {
+  const { locale, previewActive } = useContentfulContext(),
+    { data, isLoading } = useCtfNavigationQuery({
+      locale,
+      preview: previewActive
+    }),
+    navigationMenuCollection =
+      data?.navigationMenuCollection &&
+      Object.keys(data?.navigationMenuCollection)?.length > 0
+        ? useContentfulLiveUpdates(data.navigationMenuCollection)
+        : null
+
+  if (!navigationMenuCollection || isLoading) return null
+
+  return (
+    navigationMenuCollection &&
+    Object.keys(navigationMenuCollection)?.length > 0 && (
+      <NavigationCtfComponentFeature {...navigationMenuCollection} />
+    )
+  )
+}
+
+NavigationGqlCtfComponentFeature.displayName =
+  'NavigationGqlCtfComponentFeature'
+
+export default NavigationGqlCtfComponentFeature
