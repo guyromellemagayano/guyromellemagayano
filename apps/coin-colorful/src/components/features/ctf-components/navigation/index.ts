f3e0c1785cb9fd74afc4f22b73c7fd2006713c50
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 06:05:40 +0800

    feat: :sparkles: add `NavigationCtfComponentFeature` and `NavigationGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/navigation/index.ts
@@ -0,0 +1,5 @@
+export type * from './NavigationCtfComponentFeature'
+export { default as NavigationCtfComponentFeature } from './NavigationCtfComponentFeature'
+export type * from './NavigationGqlCtfComponentFeature'
+export { default as NavigationGqlCtfComponentFeature } from './NavigationGqlCtfComponentFeature'
+export * from './__generated/navigation-ctf-component-feature.generated'
