53bf7548d84e1b6fe56bd402829800f89aef930b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 03:06:47 +0800

    feat: :sparkles: add `PageLinkFeature` component and its deps

---
+++ apps/coin-colorful/src/components/features/page-link/index.ts
@@ -0,0 +1,5 @@
+export type * from './PageLinkFeature'
+export { default as PageLinkFeature } from './PageLinkFeature'
+export * from './__generated/category-link-feature.generated'
+export * from './__generated/page-link-feature.generated'
+export * from './__generated/post-link-feature.generated'
