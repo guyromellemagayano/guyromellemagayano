134cd6dff86a03720ad00a1dd1a38e06f948266a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 20:33:45 +0800

    feat: :sparkles: add `RichtextCtfComponentFeature` and `RichtextGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/richtext/__generated/richtext-ctf-component-feature.generated.ts
@@ -0,0 +1,68 @@
+import { UseQueryOptions, useQuery } from '@tanstack/react-query'
+
+import {
+  type PageLinkFieldsFragment,
+  PageLinkFieldsFragmentDoc
+} from '@guy-romelle-magayano/coin-colorful/components'
+import { ctfFetcher } from '@guy-romelle-magayano/coin-colorful/libs'
+import * as Types from '@guy-romelle-magayano/coin-colorful/libs/__generated/graphql.types'
+
+export type RichTextHyperlinkFieldsFragment = {
+  __typename?: 'Query'
+  page?: ({ __typename?: 'Page' } & PageLinkFieldsFragment) | null
+}
+
+export type CtfRichTextHyperlinkQueryVariables = Types.Exact<{
+  id: Types.Scalars['String']
+  locale?: Types.InputMaybe<Types.Scalars['String']>
+  preview?: Types.InputMaybe<Types.Scalars['Boolean']>
+}>
+
+export type CtfRichTextHyperlinkQuery = {
+  __typename?: 'Query'
+} & RichTextHyperlinkFieldsFragment
+
+export const RichTextHyperlinkFieldsFragmentDoc = `
+  fragment RichTextHyperlinkFields on Query {
+    page(id: $id, preview: $preview, locale: $locale) {
+      ...PageLinkFields
+    }
+  }
+`
+
+export const CtfRichTextHyperlinkDocument = `
+  query CtfRichTextHyperlink($id: String!, $locale: String, $preview: Boolean) {
+    ...RichTextHyperlinkFields
+  }
+  ${RichTextHyperlinkFieldsFragmentDoc}
+  ${PageLinkFieldsFragmentDoc}
+`
+
+export const useCtfRichTextHyperlinkQuery = <
+  TData = CtfRichTextHyperlinkQuery,
+  TError = unknown
+>(
+  variables: CtfRichTextHyperlinkQueryVariables,
+  options?: UseQueryOptions<CtfRichTextHyperlinkQuery, TError, TData>
+) =>
+  useQuery<CtfRichTextHyperlinkQuery, TError, TData>({
+    queryKey: ['CtfRichTextHyperlink', variables],
+    queryFn: ctfFetcher<
+      CtfRichTextHyperlinkQuery,
+      CtfRichTextHyperlinkQueryVariables
+    >(CtfRichTextHyperlinkDocument, variables),
+    ...options
+  })
+
+useCtfRichTextHyperlinkQuery.getKey = (
+  variables: CtfRichTextHyperlinkQueryVariables
+) => ['CtfRichTextHyperlink', variables]
+useCtfRichTextHyperlinkQuery.fetcher = (
+  variables: CtfRichTextHyperlinkQueryVariables,
+  options?: RequestInit['headers']
+) =>
+  ctfFetcher<CtfRichTextHyperlinkQuery, CtfRichTextHyperlinkQueryVariables>(
+    CtfRichTextHyperlinkDocument,
+    variables,
+    options
+  )
