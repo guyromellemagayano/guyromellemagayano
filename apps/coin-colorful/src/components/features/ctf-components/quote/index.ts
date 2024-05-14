a631303906e4854a4db1d8f85e3ffc0d7ca0b04c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 19:38:00 +0800

    feat: :sparkles: add `QuoteCtfComponentFeature` and `QuoteGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/quote/index.ts
@@ -0,0 +1,5 @@
+export type * from './QuoteCtfComponentFeature'
+export { default as QuoteCtfComponentFeature } from './QuoteCtfComponentFeature'
+export type * from './QuoteGqlCtfComponentFeature'
+export { default as QuoteGqlCtfComponentFeature } from './QuoteGqlCtfComponentFeature'
+export * from './__generated/quote-ctf-component-feature.generated'
