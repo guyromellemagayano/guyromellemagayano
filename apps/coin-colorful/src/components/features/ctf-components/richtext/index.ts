a74596d769e0c54b86b08c17caf802c93469ee21
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 20:33:45 +0800

    feat: :sparkles: add `RichtextCtfComponentFeature` and `RichtextGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/richtext/index.ts
@@ -0,0 +1,3 @@
+export type * from './RichtextCtfComponentFeature'
+export { default as RichtextCtfComponentFeature } from './RichtextCtfComponentFeature'
+export * from './__generated/richtext-ctf-component-feature.generated'
