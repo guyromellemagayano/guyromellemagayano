16e90ee3ea360d8205606b5cdc2afdcbd042ad02
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 19:12:58 +0800

    feat: :sparkles: add `ProductTableCtfComponentFeature` and `ProductTableGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/product-table/index.ts
@@ -0,0 +1,5 @@
+export type * from './ProductTableCtfComponentFeature'
+export { default as ProductTableCtfComponentFeature } from './ProductTableCtfComponentFeature'
+export type * from './ProductTableGqlCtfComponentFeature'
+export { default as ProductTableGqlCtfComponentFeature } from './ProductTableGqlCtfComponentFeature'
+export * from './__generated/product-table-ctf-component-feature.generated'
