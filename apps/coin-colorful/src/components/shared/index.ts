8282b440ea067bb222fe3b4f4ba128c56aa387f9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 21:40:38 +0800

    fix: :fire: remove main export file for shared components

--- apps/coin-colorful/src/components/shared/index.ts
+++
@@ -1,9 +0,0 @@
-export type * from './component-resolver'
-export type * from './error-box'
-export type * from './graphql-error'
-export type * from './link'
-
-export { default as ComponentResolver } from './component-resolver'
-export { default as ErrorBox } from './error-box'
-export { default as GraphqlError } from './graphql-error'
-export { default as Link } from './link'
