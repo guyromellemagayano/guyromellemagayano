d9aafc0b7ecf44894af6bd607a13bfc7f4ae947c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 21:45:05 +0800

    refactor: :recycle: update main SVG exports

---
+++ apps/coin-colorful/src/components/svg/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as ColorfulCoinLogo } from './svg/icons/colorful-coin-logo.svg'
+export { default as LogoTagline } from './svg/icons/logo-tagline.svg'
+export { default as SettingsIcon } from './svg/icons/settings-icon.svg'
