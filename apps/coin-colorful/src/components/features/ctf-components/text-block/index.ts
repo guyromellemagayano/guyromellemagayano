f25cb55e253652cbdfc740c7666cd02b8537068a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 20:57:28 +0800

    feat: :sparkles: add `TextBlockCtfComponentFeature` and `TextBlockGqlCtfComponentFeature` components and its deps

---
+++ apps/coin-colorful/src/components/features/ctf-components/text-block/index.ts
@@ -0,0 +1,5 @@
+export type * from './TextBlockCtfComponentFeature'
+export { default as TextBlockCtfComponentFeature } from './TextBlockCtfComponentFeature'
+export type * from './TextBlockGqlCtfComponentFeature'
+export { default as TextBlockGqlCtfComponentFeature } from './TextBlockGqlCtfComponentFeature'
+export * from './__generated/text-block-ctf-component-feature.generated'
