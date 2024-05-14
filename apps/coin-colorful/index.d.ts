6c289545a6a7288c3cb1da7d6adee18a59b1c2f5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-14 22:24:38 +0800

    feat: :bug: add `react-kawaii` type declarations

--- apps/coin-colorful/index.d.ts
+++ apps/coin-colorful/index.d.ts
@@ -4,3 +4,10 @@ declare module '*.svg' {
   export const ReactComponent: any
   export default content
 }
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'react-kawaii' {
+  const content: any
+  export const File: any
+  export default content
+}
