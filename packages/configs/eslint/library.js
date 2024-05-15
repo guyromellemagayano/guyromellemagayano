b860489969532ccb397b846ddd681ff4334b494e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-15 23:46:42 +0800

    build: :wrench: update `eslint` configs, remove unused packages

--- packages/configs/eslint/library.js
+++ packages/configs/eslint/library.js
@@ -1,6 +1,7 @@
 const { resolve } = require('node:path')
 
 const project = resolve(process.cwd(), 'tsconfig.json')
+const gitIgnoreConfig = resolve(process.cwd(), '.gitignore')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
@@ -20,15 +21,36 @@ module.exports = {
       }
     }
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    '.*.js',
-    'node_modules/',
-    'dist/'
-  ],
+  ignorePatterns: [gitIgnoreConfig, '.*.js'],
   overrides: [
     {
-      files: ['*.js?(x)', '*.ts?(x)']
+      files: ['*.ts?(x)'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-object-literal-type-assertion': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_|req|res|next|err|ctx|args|context|info'
+          }
+        ],
+        'sort-imports': [
+          'error',
+          {
+            ignoreDeclarationSort: true
+          }
+        ]
+      }
     }
   ]
 }
--- packages/configs/eslint/next.js
+++ packages/configs/eslint/next.js
@@ -1,15 +1,11 @@
 const { resolve } = require('node:path')
 
 const project = resolve(process.cwd(), 'tsconfig.json')
+const gitIgnoreConfig = resolve(process.cwd(), '.gitignore')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'prettier',
-    require.resolve('@vercel/style-guide/eslint/next'),
-    'eslint-config-turbo'
-  ],
+  extends: ['eslint:recommended', 'prettier', 'eslint-config-turbo'],
   globals: {
     React: true,
     JSX: true
@@ -26,10 +22,36 @@ module.exports = {
       }
     }
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    '.*.js',
-    'node_modules/'
-  ],
-  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }]
+  ignorePatterns: [gitIgnoreConfig, '.*.js'],
+  overrides: [
+    {
+      files: ['*.ts?(x)'],
+      rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-object-literal-type-assertion': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_|req|res|next|err|ctx|args|context|info'
+          }
+        ],
+        'sort-imports': [
+          'error',
+          {
+            ignoreDeclarationSort: true
+          }
+        ]
+      }
+    }
+  ]
 }
