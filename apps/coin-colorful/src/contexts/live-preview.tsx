48d9d83ebc503227d939fce469eba386152c84d0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-17 18:04:58 +0800

    feat: :sparkles: add live preview contexts, fix server rendergin issues among contexts

---
+++ apps/coin-colorful/src/contexts/live-preview.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { ReactNode } from 'react'
+
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react'
+
+import { useContentfulContext } from '@guy-romelle-magayano/coin-colorful/hooks'
+
+export type LivePreviewProviderProps = {
+  children: ReactNode
+}
+
+/**
+ * Provides a live preview of content using the Contentful CMS.
+ * @param children - The child components to render within the live preview provider.
+ * @returns The rendered live preview provider component.
+ */
+export const LivePreviewProvider = ({ children }: LivePreviewProviderProps) => {
+  const { previewActive, locale } = useContentfulContext()
+
+  return (
+    <ContentfulLivePreviewProvider
+      locale={locale}
+      enableInspectorMode={previewActive}
+      enableLiveUpdates={previewActive}
+    >
+      {children}
+    </ContentfulLivePreviewProvider>
+  )
+}
