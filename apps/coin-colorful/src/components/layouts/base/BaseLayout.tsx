c238f82c663a94726ec4bf46915a832176c46420
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-17 20:53:49 +0800

    feat: :sparkles: add `BaseLayout` component, update main component exports

---
+++ apps/coin-colorful/src/components/layouts/base/BaseLayout.tsx
@@ -0,0 +1,75 @@
+import { ReactNode } from 'react'
+
+import { DehydratedState } from '@tanstack/react-query'
+import { unstable_setRequestLocale } from 'next-intl/server'
+
+import {
+  Body,
+  Head,
+  Html,
+  Link,
+  Meta
+} from '@guy-romelle-magayano/react-components/server'
+
+import Providers from '@guy-romelle-magayano/coin-colorful/app/[locale]/providers'
+import { colorfulTheme } from '@guy-romelle-magayano/coin-colorful/configs'
+
+export type BaseLayoutProps = {
+  children: ReactNode
+  lang: string
+  dehydratedState?: DehydratedState
+}
+
+/**
+ * Render the base layout component.
+ * @param {BaseLayoutProps} props - The properties of the base layout.
+ * @returns The rendered base layout component.
+ */
+const BaseLayout = async (props: BaseLayoutProps) => {
+  const { dehydratedState, children, lang } = props
+
+  unstable_setRequestLocale(lang)
+
+  return (
+    <Html lang={lang} dir="ltr" suppressHydrationWarning>
+      <Head>
+        <Meta charSet="utf-8" />
+        <Meta name="robots" content="noindex, nofollow" />
+
+        {/* PWA primary color */}
+        <Meta name="theme-color" content={colorfulTheme.palette.primary.main} />
+
+        <Link
+          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <Link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <Link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <Link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <Link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <Body>
+        <Providers dehydratedState={dehydratedState}>{children}</Providers>
+      </Body>
+    </Html>
+  )
+}
+
+BaseLayout.displayName = 'BaseLayout'
+
+export default BaseLayout
