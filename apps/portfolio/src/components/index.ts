b3390c50b998759406931a310eb3b0e109ea7d30
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-21 01:33:23 +0800

    feat: :sparkles: add new `portfolio` component exports

---
+++ apps/portfolio/src/components/index.ts
@@ -0,0 +1,88 @@
+// --------------------
+// APP PAGE COMPONENTS
+// --------------------
+export * from './apps/about'
+export * from './apps/articles'
+export * from './apps/home'
+export * from './apps/projects'
+export * from './apps/skills'
+export * from './apps/uses'
+export * from './apps/work'
+
+// --------------------
+// ARTICLE COMPONENT
+// --------------------
+export * from './article'
+
+// --------------------
+// AVATAR COMPONENT
+// --------------------
+export * from './avatar'
+
+// --------------------
+// BUTTON COMPONENTS
+// --------------------
+export * from './buttons/dark-mode'
+
+// --------------------
+// CARD COMPONENTS
+// --------------------
+export * from './card'
+
+// --------------------
+// CONTAINERS COMPONENTS
+// --------------------
+export * from './containers/avatar'
+export * from './containers/base'
+
+// --------------------
+// LAYOUT COMPONENTS
+// --------------------
+export * from './layouts/article'
+export * from './layouts/base'
+export * from './layouts/content'
+export * from './layouts/footer'
+export * from './layouts/header'
+export * from './layouts/newsletter'
+export * from './layouts/photo'
+export * from './layouts/resume'
+export * from './layouts/section'
+export * from './layouts/social-links'
+
+// --------------------
+// LINK COMPONENTS
+// --------------------
+export * from './links/navigation'
+export * from './links/social'
+
+// --------------------
+// LIST COMPONENTS
+// --------------------
+export * from './lists/article-cards'
+export * from './lists/articles'
+export * from './lists/items/navigation'
+export * from './lists/navigation'
+export * from './lists/projects'
+export * from './lists/projects-cards'
+export * from './lists/skills'
+export * from './lists/skills-cards'
+export * from './lists/tools'
+export * from './lists/tools-cards'
+export * from './lists/work'
+export * from './lists/work-cards'
+
+// --------------------
+// NAVIGATION COMPONENTS
+// --------------------
+export * from './navigation/desktop'
+export * from './navigation/mobile'
+
+// --------------------
+// PROSE COMPONENTS
+// --------------------
+export * from './prose'
+
+// --------------------
+// SVG COMPONENTS
+// --------------------
+export * from './svg'
