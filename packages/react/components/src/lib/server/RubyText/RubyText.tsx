6ca195a6d61e1eb25b0726d121d46809e48917d3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-20 17:52:19 +0800

    refactor: :art: update `RubyText` component code structure, update exports

--- packages/react/components/src/lib/server/RubyText/RubyText.tsx
+++ packages/react/components/src/lib/server/RubyText/RubyText.tsx
@@ -5,19 +5,19 @@ export type RubyTextProps = HTMLAttributes<RubyTextRef>
 
 /**
  * Render the ruby text component.
- * @param children - The children of the ruby text.
- * @param rest - The rest of the props of the ruby text.
+ * @param {RubyTextProps} props - The ruby text component properties.
+ * @param {RubyTextRef} ref - The ruby text component reference.
  * @returns The rendered ruby text component.
  */
-export const RubyText = forwardRef<RubyTextRef, RubyTextProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <rt ref={ref} {...rest}>
-        {children}
-      </rt>
-    )
-  }
-)
+const RubyText = forwardRef<RubyTextRef, RubyTextProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <rt ref={ref} {...rest}>
+      {children}
+    </rt>
+  )
+})
 
 RubyText.displayName = 'RubyText'
 
