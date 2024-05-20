329be54d24100b7cd24622c5929547c3870a4949
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-20 09:48:57 +0800

    refactor: :art: update `Label` component code structure, update exports

--- packages/react/components/src/lib/client/Label/Label.tsx
+++ packages/react/components/src/lib/client/Label/Label.tsx
@@ -7,19 +7,19 @@ export type LabelProps = LabelHTMLAttributes<LabelRef>
 
 /**
  * Render the label component.
- * @param children - The children of the label.
- * @param rest - The rest of the props of the label.
+ * @param {LabelProps} props - The label component properties.
+ * @param {LabelRef} ref - The label component reference.
  * @returns The rendered label component.
  */
-export const Label = forwardRef<LabelRef, LabelProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <label ref={ref} {...rest}>
-        {children}
-      </label>
-    )
-  }
-)
+const Label = forwardRef<LabelRef, LabelProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <label ref={ref} {...rest}>
+      {children}
+    </label>
+  )
+})
 
 Label.displayName = 'Label'
 
