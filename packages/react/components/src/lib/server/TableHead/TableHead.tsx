9089945728101d078b25ae5c2b0375fcc806bb96
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-20 17:58:11 +0800

    refactor: :art: update `TableHead` component code structure, update exports

--- packages/react/components/src/lib/server/TableHead/TableHead.tsx
+++ packages/react/components/src/lib/server/TableHead/TableHead.tsx
@@ -5,19 +5,19 @@ export type TableHeadProps = HTMLAttributes<TableHeadRef>
 
 /**
  * Render the table head component.
- * @param children - The children of the table head.
- * @param rest - The rest of the props of the table head.
+ * @param {TableHeadProps} props - The table head component properties.
+ * @param {TableHeadRef} ref - The table head component reference.
  * @returns The rendered table head component.
  */
-export const TableHead = forwardRef<TableHeadRef, TableHeadProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <thead ref={ref} {...rest}>
-        {children}
-      </thead>
-    )
-  }
-)
+const TableHead = forwardRef<TableHeadRef, TableHeadProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <thead ref={ref} {...rest}>
+      {children}
+    </thead>
+  )
+})
 
 TableHead.displayName = 'TableHead'
 
