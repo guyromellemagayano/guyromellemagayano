3c0c65bf0d394bdcfc9dc653d6d015b6a2a126fe
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-20 17:47:47 +0800

    refactor: :art: update `Italic` component code structure, update exports

--- packages/react/components/src/lib/server/Italic/Italic.tsx
+++ packages/react/components/src/lib/server/Italic/Italic.tsx
@@ -5,19 +5,19 @@ export type ItalicProps = HTMLAttributes<ItalicRef>
 
 /**
  * Render the italic component.
- * @param children - The children of the italic.
- * @param rest - The rest of the props of the italic.
+ * @param {ItalicProps} props - The italic component properties.
+ * @param {ItalicRef} ref - The italic component reference.
  * @returns The rendered italic component.
  */
-export const Italic = forwardRef<ItalicRef, ItalicProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <i ref={ref} {...rest}>
-        {children}
-      </i>
-    )
-  }
-)
+const Italic = forwardRef<ItalicRef, ItalicProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <i ref={ref} {...rest}>
+      {children}
+    </i>
+  )
+})
 
 Italic.displayName = 'Italic'
 
