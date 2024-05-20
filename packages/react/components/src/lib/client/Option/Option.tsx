58783648870c7b70ce29651518abcce0dcd2f547
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-20 15:45:13 +0800

    refactor: :art: update `Option` component code structure, update exports

--- packages/react/components/src/lib/client/Option/Option.tsx
+++ packages/react/components/src/lib/client/Option/Option.tsx
@@ -7,19 +7,19 @@ export type OptionProps = OptionHTMLAttributes<OptionRef>
 
 /**
  * Render the option component.
- * @param children - The children of the option.
- * @param rest - The rest of the props of the option.
+ * @param {OptionProps} props - The option component properties.
+ * @param {OptionRef} ref - The option component reference.
  * @returns The rendered option component.
  */
-export const Option = forwardRef<OptionRef, OptionProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <option ref={ref} {...rest}>
-        {children}
-      </option>
-    )
-  }
-)
+const Option = forwardRef<OptionRef, OptionProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <option ref={ref} {...rest}>
+      {children}
+    </option>
+  )
+})
 
 Option.displayName = 'Option'
 
