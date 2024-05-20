5743b514d0ec68804b1e40ac22bcb301f4a39d2b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-20 17:31:44 +0800

    refactor: :art: update `Column` component code structure, update exports

--- packages/react/components/src/lib/server/Column/Column.tsx
+++ packages/react/components/src/lib/server/Column/Column.tsx
@@ -5,19 +5,19 @@ export type ColumnProps = ColHTMLAttributes<ColumnRef>
 
 /**
  * Render the column component.
- * @param children - The children of the column.
- * @param rest - The rest of the props of the column.
+ * @param {ColumnProps} props - The column component properties.
+ * @param {ColumnRef} ref - The column component reference.
  * @returns The rendered column component.
  */
-export const Column = forwardRef<ColumnRef, ColumnProps>(
-  ({ children, ...rest }, ref) => {
-    return (
-      <col ref={ref} {...rest}>
-        {children}
-      </col>
-    )
-  }
-)
+const Column = forwardRef<ColumnRef, ColumnProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <col ref={ref} {...rest}>
+      {children}
+    </col>
+  )
+})
 
 Column.displayName = 'Column'
 
