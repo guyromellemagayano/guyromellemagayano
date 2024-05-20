9aaa03bcd59e666f7badb4f10b979bb70bea30d5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-21 02:13:19 +0800

    feat: :sparkles: add common `any` types for hook, lib, and util types

---
+++ packages/react/utils/src/utils.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// ====================================================
+// Common Utils Prop Types
+// ====================================================
+export type CommonUtilsProps = {
+  [key: string]: any
+}
