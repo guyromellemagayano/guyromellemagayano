9e4df47b08cbb00b082c58414baf1cf7bff2732d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-07-25 03:38:33 +0800

    fix: :bug: fix conditional rendering logic of `PhotoLayout` component

--- apps/portfolio/src/components/layouts/photo/PhotoLayout.tsx
+++ apps/portfolio/src/components/layouts/photo/PhotoLayout.tsx
@@ -8,7 +8,7 @@ import {
   type DivisionRef
 } from '@guy-romelle-magayano/react-components/server'
 
-import { cn } from '@guy-romelle-magayano/react-utils'
+import { cn, isEmpty } from '@guy-romelle-magayano/react-utils'
 
 import { type SlidePhotosData } from '@guy-romelle-magayano/portfolio/types'
 
@@ -17,9 +17,7 @@ export type PhotoLayoutProps = DivisionProps & {
   data?: Array<SlidePhotosData>
 }
 
-/**
- * The rotations data.
- */
+// The rotations data.
 const RotationsData: Array<string> = [
   'rotate-2',
   '-rotate-2',
@@ -29,43 +27,44 @@ const RotationsData: Array<string> = [
 ]
 
 /**
- * Renders the photo layout component.
- * @param {PhotoLayoutProps} props - The props of the photo layout.
- * @param {PhotoLayoutRef} ref - The reference of the photo layout.
- * @returns The rendered photo layout component.
+ * Renders the photo layout component
+ * @param {PhotoLayoutProps} props - The component props
+ * @param {PhotoLayoutRef} ref - The component reference
+ * @returns The rendered JSX component
  */
 const PhotoLayout = memo(
   forwardRef<PhotoLayoutRef, PhotoLayoutProps>(({ data, ...rest }, ref) => {
+    if (isEmpty(data)) {
+      return null
+    }
+
     return (
-      data &&
-      data?.length > 0 && (
-        <Div {...rest} ref={ref}>
-          <Div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-            {data.map(
-              ({ src, alt }: SlidePhotosData, index: number) =>
-                src &&
-                src?.length > 0 && (
-                  <Div
-                    key={index}
-                    className={cn(
-                      'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-                      RotationsData[index % RotationsData.length]
-                    )}
-                  >
-                    <Image
-                      src={src}
-                      alt={alt}
-                      sizes="(min-width: 640px) 18rem, 11rem"
-                      className="absolute inset-0 h-auto w-full object-cover"
-                      fill
-                      priority
-                    />
-                  </Div>
-                )
-            )}
-          </Div>
+      <Div {...rest} ref={ref}>
+        <Div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+          {data?.map(
+            ({ src, alt }: SlidePhotosData, index: number) =>
+              src &&
+              src?.length > 0 && (
+                <Div
+                  key={index}
+                  className={cn(
+                    'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+                    RotationsData[index % RotationsData.length]
+                  )}
+                >
+                  <Image
+                    src={src}
+                    alt={alt}
+                    sizes="(min-width: 640px) 18rem, 11rem"
+                    className="absolute inset-0 h-auto w-full object-cover"
+                    fill
+                    priority
+                  />
+                </Div>
+              )
+          )}
         </Div>
-      )
+      </Div>
     )
   })
 )
