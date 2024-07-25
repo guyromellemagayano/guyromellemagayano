c1122b636747b95389f58db5cfa785c505da3a63
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-07-25 10:49:56 +0800

    refactor: :recycle: update `PhotoLayout` component

--- apps/portfolio/src/components/layouts/photo/PhotoLayout.tsx
+++ apps/portfolio/src/components/layouts/photo/PhotoLayout.tsx
@@ -8,13 +8,14 @@ import {
   type DivisionRef
 } from '@guy-romelle-magayano/react-components/server'
 
-import { cn, isEmpty } from '@guy-romelle-magayano/react-utils'
+import { cn } from '@guy-romelle-magayano/react-utils'
 
 import { type SlidePhotosData } from '@guy-romelle-magayano/portfolio/types'
 
 export type PhotoLayoutRef = DivisionRef
 export type PhotoLayoutProps = DivisionProps & {
   data?: Array<SlidePhotosData>
+  priority?: boolean
 }
 
 // The rotations data.
@@ -33,40 +34,40 @@ const RotationsData: Array<string> = [
  * @returns The rendered JSX component
  */
 const PhotoLayout = memo(
-  forwardRef<PhotoLayoutRef, PhotoLayoutProps>(({ data, ...rest }, ref) => {
-    if (isEmpty(data)) {
-      return null
-    }
+  forwardRef<PhotoLayoutRef, PhotoLayoutProps>(
+    ({ data, priority, ...rest }, ref) => {
+      const defaultImageAlt = 'Photo Layout Image'
 
-    return (
-      <Div {...rest} ref={ref}>
-        <Div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-          {data?.map(
-            ({ src, alt }: SlidePhotosData, index: number) =>
-              src &&
-              src?.length > 0 && (
-                <Div
-                  key={index}
-                  className={cn(
-                    'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
-                    RotationsData[index % RotationsData.length]
-                  )}
-                >
-                  <Image
-                    src={src}
-                    alt={alt}
-                    sizes="(min-width: 640px) 18rem, 11rem"
-                    className="absolute inset-0 h-auto w-full object-cover"
-                    fill
-                    priority
-                  />
-                </Div>
-              )
-          )}
+      if (!data || data.length === 0) {
+        return null
+      }
+
+      return (
+        <Div {...rest} ref={ref}>
+          <Div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+            {data.map(({ src, alt }, index) => (
+              <Div
+                key={index}
+                className={cn(
+                  'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+                  RotationsData[index % RotationsData.length]
+                )}
+              >
+                <Image
+                  src={src}
+                  alt={alt || defaultImageAlt}
+                  sizes="(min-width: 640px) 18rem, 11rem"
+                  className="absolute inset-0 h-auto w-full object-cover"
+                  fill
+                  priority={priority}
+                />
+              </Div>
+            ))}
+          </Div>
         </Div>
-      </Div>
-    )
-  })
+      )
+    }
+  )
 )
 
 PhotoLayout.displayName = 'PhotoLayout'
