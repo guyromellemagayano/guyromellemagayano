d9be4af744447936a529d4df3d7f4dbc9c4a0f59
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-07-31 15:45:27 +0800

    feat: :sparkles: add skills section in home page

--- apps/portfolio/src/components/apps/home/Home.tsx
+++ apps/portfolio/src/components/apps/home/Home.tsx
@@ -1,4 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
+
+import {
+  CodeBracketIcon,
+  NewspaperIcon,
+  UserIcon
+} from '@heroicons/react/16/solid'
+import { useTheme } from 'next-themes'
 
 import {
   Article,
@@ -13,16 +21,16 @@ import {
   ArticleLayout,
   BaseContainer,
   ContentLayout,
-  NewsletterLayout,
+  JumbotronLayout,
+  NewsletterForm,
   PhotoLayout,
-  ResumeLayout,
-  SocialLinksLayout
+  ResumeLayout
 } from '@guy-romelle-magayano/portfolio/components'
-import {
-  type ArticlesData,
-  type HomePageData,
-  type ProjectsData,
-  type SocialLinksData
+import type {
+  ArticlesData,
+  HomePageData,
+  ProjectsData,
+  SocialLinksData
 } from '@guy-romelle-magayano/portfolio/types'
 
 export type HomeAppProps = HomePageData & {
@@ -37,44 +45,109 @@ const strings = {
 }
 
 /**
- * Render the home application component.
- * @param {HomeAppProps} props - The props of the home application.
- * @returns The rendered home application component.
+ * Render the home application component
+ * @param {HomeAppProps} props - The component props
+ * @returns The rendered JSX component
  */
 const HomeApp = (props: HomeAppProps) => {
   const {
-      hero,
-      slidePhotos,
-      cvFile,
-      workExperiences,
-      links,
-      articles,
-      projects
-    } = props,
-    heading = hero?.heading || undefined,
-    description = hero?.description || undefined
+    hero,
+    aboutMeInfo,
+    skillsInfo,
+    slidePhotos,
+    cvFile,
+    workExperiences,
+    links,
+    articles,
+    projects
+  } = props
+
+  const { resolvedTheme } = useTheme()
 
   return (
     <>
-      {heading &&
-        heading?.length > 0 &&
-        description &&
-        ((typeof description === 'string' && description?.length > 0) ||
-          (Array.isArray(description) && description?.length > 0)) && (
-          <ContentLayout.Simple
-            title={heading}
-            intro={description}
-            className="mt-9 sm:mt-9"
-          >
-            {links && links?.length > 0 && (
-              <SocialLinksLayout className="mt-6 flex gap-6" data={links} />
-            )}
-          </ContentLayout.Simple>
-        )}
+      {/* Intro section */}
+      <JumbotronLayout
+        heading={hero?.heading}
+        description={hero?.description}
+        links={links}
+      />
 
-      {slidePhotos && slidePhotos?.length > 0 && (
-        <PhotoLayout className="mt-16 sm:mt-20" data={slidePhotos} />
-      )}
+      {/* Slide photos sections */}
+      <PhotoLayout className="mt-16 sm:mt-20" data={slidePhotos} />
+
+      {/* About me section */}
+      <ContentLayout.Simple
+        title={aboutMeInfo?.heading}
+        intro={aboutMeInfo?.description}
+        className="mt-20 md:mt-24"
+      />
+
+      {/* Skills section */}
+      <ContentLayout.Simple
+        title={skillsInfo?.heading}
+        intro={skillsInfo?.description}
+        className="mt-20 md:mt-24"
+      >
+        {skillsInfo?.skills && skillsInfo?.skills?.length > 0 && (
+          <div className="mx-auto max-w-7xl">
+            <div className="-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-4 lg:grid-cols-5">
+              {skillsInfo?.skills?.map(skill => {
+                const imageAlt =
+                  resolvedTheme === 'dark' &&
+                  skill.image.dark &&
+                  skill.image.dark?.alt?.length > 0 &&
+                  skill.image.dark?.src !== '#'
+                    ? skill.image.dark.alt
+                    : skill.image.default.alt
+                const imageSrc =
+                  resolvedTheme === 'dark' &&
+                  skill.image.dark &&
+                  skill.image.dark?.src?.length > 0 &&
+                  skill.image.dark?.src !== '#'
+                    ? skill.image.dark.src
+                    : skill.image.default.src
+
+                return (
+                  <div
+                    key={skill.name}
+                    className="h-full w-full cursor-pointer bg-neutral-200/45 px-4 py-8 ring-1 ring-neutral-950/10 transition hover:bg-neutral-200 dark:bg-white/5 dark:ring-neutral-950/5 dark:hover:bg-white/15"
+                  >
+                    <img
+                      alt={imageAlt}
+                      src={imageSrc}
+                      className="md:h-18 mx-auto h-16 w-20 md:h-20 md:w-24"
+                    />
+                    <h3 className="mt-6 text-center text-base font-semibold leading-5 tracking-tighter text-zinc-600 dark:text-zinc-400">
+                      {skill.name}
+                    </h3>
+                    <ul
+                      role="list"
+                      className="mt-3 flex justify-center gap-x-3"
+                    >
+                      <li>
+                        <span className="flex items-center gap-x-1 text-sm font-medium leading-6 text-gray-400">
+                          <NewspaperIcon className="h-4 w-4" />
+                        </span>
+                      </li>
+                      <li>
+                        <span className="flex items-center gap-x-1 text-sm font-medium leading-6 text-gray-400">
+                          <CodeBracketIcon className="h-4 w-4" />
+                        </span>
+                      </li>
+                      <li>
+                        <span className="flex items-center gap-x-1 text-sm font-medium leading-6 text-gray-400">
+                          <UserIcon className="h-4 w-4" />
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
+      </ContentLayout.Simple>
 
       <BaseContainer className="mt-20 md:mt-24">
         <Div
@@ -87,12 +160,13 @@ const HomeApp = (props: HomeAppProps) => {
               'gap-y-20'
           )}
         >
+          {/* Articles section */}
           <Section>
             {articles && articles?.length > 0 && (
               <Article className="mb-6">
                 <Heading
                   as="h3"
-                  className=" text-3xl font-bold tracking-tight text-zinc-800 sm:text-4xl dark:text-zinc-100"
+                  className="text-3xl font-bold tracking-tight text-zinc-800 sm:text-4xl dark:text-zinc-100"
                 >
                   {strings.articles}
                 </Heading>
@@ -103,8 +177,10 @@ const HomeApp = (props: HomeAppProps) => {
           </Section>
 
           <Section className="space-y-10 lg:pl-16 xl:pl-24">
-            <NewsletterLayout />
+            {/* Newsletter form section */}
+            <NewsletterForm />
 
+            {/* Work experiences section */}
             {((workExperiences && workExperiences?.length > 0) ||
               (cvFile && cvFile?.length > 0)) && (
               <ResumeLayout
