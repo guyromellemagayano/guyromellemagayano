51b46c182527719f6715ae9759e16e908b6fbaef
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-13 03:19:06 +0800

    fix: :lipstick: fix UI of `ContentLayout` and `JumbotronLayout` UI components

--- apps/portfolio/src/components/layouts/jumbotron/JumbotronLayout.tsx
+++ apps/portfolio/src/components/layouts/jumbotron/JumbotronLayout.tsx
@@ -1,13 +1,16 @@
 import {
   ContentLayout,
+  PhotoLayout,
   SocialLinksLayout
 } from '@guy-romelle-magayano/portfolio/components'
 import type {
   BaseHeroData,
+  HomePageData,
   SocialLinksData
 } from '@guy-romelle-magayano/portfolio/types'
 
 export type JumbotronLayoutProps = BaseHeroData & {
+  photos: HomePageData['slidePhotos']
   links?: Array<SocialLinksData>
 }
 
@@ -19,22 +22,23 @@ export type JumbotronLayoutProps = BaseHeroData & {
 const JumbotronLayout = ({
   heading = '',
   description = '',
-  links = []
+  links,
+  photos
 }: JumbotronLayoutProps) => {
   if (!heading && !description) {
     return null
   }
 
   return (
-    <ContentLayout.Simple
-      title={heading}
-      intro={description}
-      className="mt-9 sm:mt-9"
-    >
-      {links.length > 0 && (
-        <SocialLinksLayout className="mt-6 flex gap-6" data={links} />
-      )}
-    </ContentLayout.Simple>
+    <>
+      <ContentLayout.Simple title={heading} className="mt-20 md:mt-24" />
+      <PhotoLayout className="mt-8 sm:mt-12" data={photos} />
+      <ContentLayout.Simple intro={description} className="mt-9 sm:mt-9">
+        {links && links?.length > 0 && (
+          <SocialLinksLayout className="mt-6 flex gap-6" data={links} />
+        )}
+      </ContentLayout.Simple>
+    </>
   )
 }
 
