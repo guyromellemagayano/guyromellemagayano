eeda9a171829a361df8736b5630c18ad2f7b6ace
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-19 08:57:33 +0800

    build: :construction_worker: add SCSS support for `postcss`, update deps

--- apps/portfolio/postcss.config.js
+++ apps/portfolio/postcss.config.js
@@ -1,13 +1,14 @@
 module.exports = {
+  parser: 'postcss-scss',
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
     'postcss-focus-visible': {
       replaceWith: '[data-focus-visible-added]'
     },
     'postcss-preset-env': {
       features: { 'nesting-rules': false }
     },
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
