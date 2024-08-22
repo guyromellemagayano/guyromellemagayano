e3807a445b9b91285409ba12c69eadbd1b3e5501
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-22 20:12:34 +0800

    fix: :sparkles: update `ResumeLayout` component with refs, types, and docs

--- apps/portfolio/src/components/layouts/resume/ResumeLayout.tsx
+++ apps/portfolio/src/components/layouts/resume/ResumeLayout.tsx
@@ -4,10 +4,15 @@ import { forwardRef, memo } from 'react'
 
 import { useRouter } from 'next/navigation'
 
-import { Button, ButtonProps } from '@guy-romelle-magayano/react-components'
+import {
+  Button,
+  ButtonProps,
+  ButtonRef
+} from '@guy-romelle-magayano/react-components'
 import {
   Div,
-  DivisionProps
+  DivisionProps,
+  DivisionRef
 } from '@guy-romelle-magayano/react-components/server'
 
 import { cn, isValidData } from '@guy-romelle-magayano/react-utils'
@@ -25,12 +30,6 @@ import type {
   WorkExperiencesData
 } from '@guy-romelle-magayano/portfolio/types'
 
-export type ResumeLayoutRef = ContentLayoutRef
-export type ResumeLayoutProps = ContentLayoutProps & {
-  cvFile?: WorkData['cvFile']
-  workExperiences?: WorkData['workExperiences']
-}
-
 const strings = {
   work: 'Work',
   company: 'Company',
@@ -39,6 +38,7 @@ const strings = {
   downloadCV: 'Download CV'
 }
 
+type WorkExperiencesRef = DivisionRef
 type WorkExperiencesProps = DivisionProps & {
   data?: WorkExperiencesData[]
 }
@@ -46,37 +46,37 @@ type WorkExperiencesProps = DivisionProps & {
 /**
  * Render the work experiences component.
  * @param {WorkExperiencesProps} props - The component props
+ * @param {WorkExperiencesRef} ref - The component reference
  * @returns The rendered JSX component.
  */
-const WorkExperiences = ({
-  data,
-  className,
-  ...rest
-}: WorkExperiencesProps) => {
-  const validData =
-    data?.filter((item): item is WorkExperiencesData =>
-      isValidData(item, 'object')
-    ) || null
+const WorkExperiences = forwardRef<WorkExperiencesRef, WorkExperiencesProps>(
+  ({ data, className, ...rest }, ref) => {
+    const validData =
+      data?.filter((item): item is WorkExperiencesData =>
+        isValidData(item, 'object')
+      ) || null
 
-  if (!validData || validData?.length === 0) {
-    return null
+    if (!validData || validData?.length === 0) {
+      return null
+    }
+
+    return (
+      <Div ref={ref} className={cn('grid gap-y-12', className)} {...rest}>
+        {validData.map(({ id, ...rest }) => {
+          return (
+            <WorkList key={id}>
+              <WorkCardsList {...rest} />
+            </WorkList>
+          )
+        })}
+      </Div>
+    )
   }
-
-  return (
-    <Div className={cn('grid gap-y-12', className)} {...rest}>
-      {validData.map(({ id, ...rest }) => {
-        return (
-          <WorkList key={id}>
-            <WorkCardsList {...rest} />
-          </WorkList>
-        )
-      })}
-    </Div>
-  )
-}
+)
 
 WorkExperiences.displayName = 'WorkExperiences'
 
+type CvFileRef = ButtonRef
 type CvFileProps = ButtonProps & {
   data?: WorkData['cvFile']
 }
@@ -84,38 +84,48 @@ type CvFileProps = ButtonProps & {
 /**
  * Render the cv file component.
  * @param {CvFileProps} props - The component props
- * @returns The rendered cv file component.
+ * @param {CvFileRef} ref - The component reference
+ * @returns The rendered JSX component.
  */
-const CvFile = ({ data, className, ...rest }: CvFileProps) => {
-  const router = useRouter()
-  const validData = isValidData(data, 'string') ? data : null
+const CvFile = forwardRef<CvFileRef, CvFileProps>(
+  ({ data, className, ...rest }, ref) => {
+    const router = useRouter()
+    const validData = isValidData(data, 'string') ? data : null
 
-  if (!validData || validData?.length === 0) {
-    return null
+    if (!validData || validData?.length === 0) {
+      return null
+    }
+
+    return (
+      <Button
+        ref={ref}
+        className={cn(
+          'group mt-6 inline-flex w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 outline-offset-2 transition hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 active:transition-none dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
+          className
+        )}
+        onClick={() => router.push(validData!)}
+        {...rest}
+      >
+        {strings.downloadCV}
+        <ArrowDownSvg className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+      </Button>
+    )
   }
-
-  return (
-    <Button
-      className={cn(
-        'group mt-6 inline-flex w-full items-center justify-center gap-2 rounded-md bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 outline-offset-2 transition hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 active:transition-none dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
-        className
-      )}
-      onClick={() => router.push(validData!)}
-      {...rest}
-    >
-      {strings.downloadCV}
-      <ArrowDownSvg className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-    </Button>
-  )
-}
+)
 
 CvFile.displayName = 'CvFile'
+
+export type ResumeLayoutRef = ContentLayoutRef
+export type ResumeLayoutProps = ContentLayoutProps & {
+  cvFile?: WorkData['cvFile']
+  workExperiences?: WorkData['workExperiences']
+}
 
 /**
  * Renders the resume layout component.
  * @param {ResumeLayoutProps} props - The component props
  * @param {ResumeLayoutRef} ref - The component reference
- * @returns The rendered resume layout component.
+ * @returns The rendered JSX component.
  */
 const ResumeLayout = memo(
   forwardRef<ResumeLayoutRef, ResumeLayoutProps>(
