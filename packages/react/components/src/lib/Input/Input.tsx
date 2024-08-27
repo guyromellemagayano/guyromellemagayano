8e9f89d8a427fdd8c71aa943fa1f34127e6aefea
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:20:57 +0800

    fix: :recycle: update React imports and docs for `Input` package component

--- packages/react/components/src/lib/Input/Input.tsx
+++ packages/react/components/src/lib/Input/Input.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import { InputHTMLAttributes, forwardRef } from 'react'
+import React from 'react'
 
 export type InputRef = HTMLInputElement
-export type InputProps = InputHTMLAttributes<InputRef>
+export type InputProps = React.InputHTMLAttributes<InputRef>
 
 /**
- * Render the input component.
+ * Render the input component
  * @param {InputProps} props - The input component properties
  * @param {InputRef} ref - The input component reference
  * @returns The rendered input component
  */
-export const Input = forwardRef<InputRef, InputProps>((props, ref) => {
+export const Input = React.forwardRef<InputRef, InputProps>((props, ref) => {
   const { type = 'text', ...rest } = props
 
   return <input ref={ref} type={type} {...rest} />
