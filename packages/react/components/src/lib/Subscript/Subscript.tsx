bfe91fe5a6d5428c70a48f568f54517cc0988ea7
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 03:17:36 +0800

    fix: :recycle: update React imports and docs for `Subscript` package component

---
+++ packages/react/components/src/lib/Subscript/Subscript.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export type SubscriptRef = HTMLElement
+export type SubscriptProps = React.HTMLAttributes<SubscriptRef>
+
+/**
+ * Render the subscript component
+ * @param {SubscriptProps} props - The subscript component properties
+ * @param {SubscriptRef} ref - The subscript component reference
+ * @returns The rendered subscript component
+ */
+const Subscript = React.forwardRef<SubscriptRef, SubscriptProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <sub ref={ref} {...rest}>
+        {children}
+      </sub>
+    )
+  }
+)
+
+Subscript.displayName = 'Subscript'
+
+export default Subscript
