adc2468de8f5e0eae0f4575653354dfb6fbca3f9
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:24:26 +0800

    fix: :recycle: update React imports and docs for `Meter` package component

---
+++ packages/react/components/src/lib/Meter/Meter.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+
+export type MeterRef = HTMLMeterElement
+export type MeterProps = React.MeterHTMLAttributes<MeterRef>
+
+/**
+ * Render the meter component
+ * @param {MeterProps} props - The meter component properties
+ * @param {MeterRef} ref - The meter component reference
+ * @returns The rendered meter component
+ */
+const Meter = React.forwardRef<MeterRef, MeterProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <meter ref={ref} {...rest}>
+      {children}
+    </meter>
+  )
+})
+
+Meter.displayName = 'Meter'
+
+export default Meter
