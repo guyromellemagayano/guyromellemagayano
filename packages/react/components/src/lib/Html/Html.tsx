487036a7b23341a6c7d1db24f6d44796287d93f8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:49:18 +0800

    fix: :recycle: update React imports and docs for `Html` package component

---
+++ packages/react/components/src/lib/Html/Html.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export type HtmlRef = HTMLHtmlElement
+export type HtmlProps = React.HtmlHTMLAttributes<HtmlRef>
+
+/**
+ * Render the HTML component
+ * @param {HtmlProps} props - The HTML component properties
+ * @returns The rendered HTML component
+ */
+const Html = (props: HtmlProps) => {
+  const { children, ...rest } = props
+
+  return <html {...rest}>{children}</html>
+}
+
+Html.displayName = 'Html'
+
+export default Html
