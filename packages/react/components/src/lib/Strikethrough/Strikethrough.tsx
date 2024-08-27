57a7d388b1d6971bd6ff51bed279c0795b605d31
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 03:16:23 +0800

    fix: :recycle: update React imports and docs for `Strikethrough` package component

---
+++ packages/react/components/src/lib/Strikethrough/Strikethrough.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export type StrikethroughRef = HTMLElement
+export type StrikethroughProps = React.HTMLAttributes<StrikethroughRef>
+
+/**
+ * Render the strikethrough component
+ * @param {StrikethroughProps} props - The strikethrough component properties
+ * @param {StrikethroughRef} ref - The strikethrough component reference
+ * @returns The rendered strikethrough component
+ */
+const Strikethrough = React.forwardRef<StrikethroughRef, StrikethroughProps>(
+  (props, ref) => {
+    const { children, ...rest } = props
+
+    return (
+      <s ref={ref} {...rest}>
+        {children}
+      </s>
+    )
+  }
+)
+
+Strikethrough.displayName = 'Strikethrough'
+
+export default Strikethrough
