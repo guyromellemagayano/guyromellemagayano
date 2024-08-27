f727adc0054d59b1f28a656819a9efe3a5bafb0d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:26:12 +0800

    fix: :recycle: update React imports and docs for `Output` package component

---
+++ packages/react/components/src/lib/Output/Output.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+
+export type OutputRef = HTMLOutputElement
+export type OutputProps = React.OutputHTMLAttributes<OutputRef>
+
+/**
+ * Render the output component
+ * @param {OutputProps} props - The output component properties
+ * @param {OutputRef} ref - The output component reference
+ * @returns The rendered output component
+ */
+const Output = React.forwardRef<OutputRef, OutputProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <output ref={ref} {...rest}>
+      {children}
+    </output>
+  )
+})
+
+Output.displayName = 'Output'
+
+export default Output
