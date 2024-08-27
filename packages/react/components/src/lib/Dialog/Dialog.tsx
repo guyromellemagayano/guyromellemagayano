afb193f3c9d1b0ae6d7767aa0cee039426bdd997
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:15:55 +0800

    fix: :recycle: update React imports and docs for `Dialog` package component

---
+++ packages/react/components/src/lib/Dialog/Dialog.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+
+export type DialogRef = HTMLDialogElement
+export type DialogProps = React.DialogHTMLAttributes<DialogRef>
+
+/**
+ * Render the dialog component
+ * @param {DialogProps} props - The dialog component properties
+ * @param {DialogRef} ref - The dialog component reference
+ * @returns The rendered dialog component
+ */
+const Dialog = React.forwardRef<DialogRef, DialogProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <dialog ref={ref} {...rest}>
+      {children}
+    </dialog>
+  )
+})
+
+Dialog.displayName = 'Dialog'
+
+export default Dialog
