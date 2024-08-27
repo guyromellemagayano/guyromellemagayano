cd3a9d97768c8bc2fd3d2bceffe5c54d3169a6f2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 03:32:25 +0800

    fix: :recycle: update React imports and docs for `TableHeader` package component

---
+++ packages/react/components/src/lib/TableHeader/TableHeader.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export type TableHeaderRef = HTMLTableCellElement
+export type TableHeaderProps = React.ThHTMLAttributes<TableHeaderRef>
+
+/**
+ * Render the table header component
+ * @param {TableHeaderProps} props - The table header component properties
+ * @param {TableHeaderRef} ref - The table header component reference
+ * @returns The rendered table header component
+ */
+const TableHeader = React.forwardRef<TableHeaderRef, TableHeaderProps>(
+  (props, ref) => {
+    const { children, ...rest } = props
+
+    return (
+      <th ref={ref} {...rest}>
+        {children}
+      </th>
+    )
+  }
+)
+
+TableHeader.displayName = 'TableHeader'
+
+export default TableHeader
