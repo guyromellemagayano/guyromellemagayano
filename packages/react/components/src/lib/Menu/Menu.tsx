ada9a4edd3ce4450dddcb71c80cbfb53a64dc592
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:24:03 +0800

    fix: :recycle: update React imports and docs for `Menu` package component

---
+++ packages/react/components/src/lib/Menu/Menu.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+
+export type MenuRef = HTMLElement
+export type MenuProps = React.HTMLAttributes<MenuRef>
+
+/**
+ * Render the menu component
+ * @param {MenuProps} props - The menu component properties
+ * @param {MenuRef} ref - The menu component reference
+ * @returns The rendered menu component
+ */
+const Menu = React.forwardRef<MenuRef, MenuProps>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <menu ref={ref} {...rest}>
+      {children}
+    </menu>
+  )
+})
+
+Menu.displayName = 'Menu'
+
+export default Menu
