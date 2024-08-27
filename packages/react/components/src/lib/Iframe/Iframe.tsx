ec3efbe5a0b22ab298aaadffdee901d5095f0d52
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:19:15 +0800

    fix: :recycle: update React imports and docs for `Iframe` package component

---
+++ packages/react/components/src/lib/Iframe/Iframe.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+
+export type IframeRef = HTMLIFrameElement
+export type IframeProps = React.IframeHTMLAttributes<IframeRef>
+
+/**
+ * Render the iframe component
+ * @param {IframeProps} props - The iframe component properties
+ * @param {IframeRef} ref - The iframe component reference
+ * @returns The rendered iframe component
+ */
+const Iframe = React.forwardRef<IframeRef, IframeProps>((props, ref) => {
+  const { src = '#', children, ...rest } = props
+
+  return (
+    <iframe ref={ref} src={src} {...rest}>
+      {children}
+    </iframe>
+  )
+})
+
+Iframe.displayName = 'Iframe'
+
+export default Iframe
