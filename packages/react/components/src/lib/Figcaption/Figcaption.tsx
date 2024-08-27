045d29860121312df050d616a04844b4a8d2049d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:47:01 +0800

    fix: :recycle: update React imports and docs for `Figcaption` package component

---
+++ packages/react/components/src/lib/Figcaption/Figcaption.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export type FigcaptionRef = HTMLElement
+export type FigcaptionProps = React.HTMLAttributes<FigcaptionRef>
+
+/**
+ * Render the figcaption component
+ * @param {FigcaptionProps} props - The figcaption component properties
+ * @param {FigcaptionRef} ref - The figcaption component reference
+ * @returns The rendered figcaption component
+ */
+const Figcaption = React.forwardRef<FigcaptionRef, FigcaptionProps>(
+  (props, ref) => {
+    const { children, ...rest } = props
+
+    return (
+      <figcaption ref={ref} {...rest}>
+        {children}
+      </figcaption>
+    )
+  }
+)
+
+Figcaption.displayName = 'Figcaption'
+
+export default Figcaption
