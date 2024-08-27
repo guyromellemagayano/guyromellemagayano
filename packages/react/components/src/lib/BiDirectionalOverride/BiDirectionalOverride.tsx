248e4a9e11f39497f8595ffe95fc76604dba6835
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:11:07 +0800

    fix: :recycle: update React imports and docs for `BiDirectionalOverride` package component

---
+++ packages/react/components/src/lib/BiDirectionalOverride/BiDirectionalOverride.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export type BiDirectionalOverrideRef = HTMLElement
+export type BiDirectionalOverrideProps =
+  React.HTMLAttributes<BiDirectionalOverrideRef>
+
+/**
+ * Render the bi-directional override component
+ * @param {BiDirectionalOverrideProps} props - The bi-directional override component properties
+ * @param {BiDirectionalOverrideRef} ref - The bi-directional override component reference
+ * @returns The rendered bi-directional override component
+ */
+const BiDirectionalOverride = React.forwardRef<
+  BiDirectionalOverrideRef,
+  BiDirectionalOverrideProps
+>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <bdo ref={ref} {...rest}>
+      {children}
+    </bdo>
+  )
+})
+
+BiDirectionalOverride.displayName = 'BiDirectionalOverride'
+
+export default BiDirectionalOverride
