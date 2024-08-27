d2b065d9e88b6419ce1acea26b9fd4c22b01c882
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:10:45 +0800

    fix: :recycle: update React imports and docs for `BiDirectionalIsolation` package component

---
+++ packages/react/components/src/lib/BiDirectionalIsolation/BiDirectionalIsolation.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export type BiDirectionalIsolationRef = HTMLElement
+export type BiDirectionalIsolationProps =
+  React.HTMLAttributes<BiDirectionalIsolationRef>
+
+/**
+ * Render the bi-directional isolation component
+ * @param {BiDirectionalIsolationProps} props - The bi-directional isolation component properties
+ * @param {BiDirectionalIsolationRef} ref - The bi-directional isolation component reference
+ * @returns The rendered bi-directional isolation component
+ */
+const BiDirectionalIsolation = React.forwardRef<
+  BiDirectionalIsolationRef,
+  BiDirectionalIsolationProps
+>((props, ref) => {
+  const { children, ...rest } = props
+
+  return (
+    <bdi ref={ref} {...rest}>
+      {children}
+    </bdi>
+  )
+})
+
+BiDirectionalIsolation.displayName = 'BiDirectionalIsolation'
+
+export default BiDirectionalIsolation
