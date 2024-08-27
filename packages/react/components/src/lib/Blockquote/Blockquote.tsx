aa1b89160e5459cce7d1a83a9b458fb03284fbc6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:11:24 +0800

    fix: :recycle: update React imports and docs for `Blockquote` package component

---
+++ packages/react/components/src/lib/Blockquote/Blockquote.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export type BlockquoteRef = HTMLQuoteElement
+export type BlockquoteProps = React.BlockquoteHTMLAttributes<BlockquoteRef>
+
+/**
+ * Render the blockquote component
+ * @param {BlockquoteProps} props - The blockquote component properties
+ * @param {BlockquoteRef} ref - The blockquote component reference
+ * @returns The rendered blockquote component
+ */
+const Blockquote = React.forwardRef<BlockquoteRef, BlockquoteProps>(
+  (props, ref) => {
+    const { children, ...rest } = props
+
+    return (
+      <blockquote ref={ref} {...rest}>
+        {children}
+      </blockquote>
+    )
+  }
+)
+
+Blockquote.displayName = 'Blockquote'
+
+export default Blockquote
