ab5612ccad16c13f000829184068222e3f0913ce
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-08-28 02:42:35 +0800

    fix: :recycle: update React imports and docs for `ColumnGroup` package component

---
+++ packages/react/components/src/lib/ColumnGroup/ColumnGroup.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export type ColumnGroupRef = HTMLTableColElement
+export type ColumnGroupProps = React.ColgroupHTMLAttributes<ColumnGroupRef>
+
+/**
+ * Render the column group component
+ * @param {ColumnGroupProps} props - The column group component properties
+ * @param {ColumnGroupRef} ref - The column group component reference
+ * @returns The rendered column group component
+ */
+const ColumnGroup = React.forwardRef<ColumnGroupRef, ColumnGroupProps>(
+  (props, ref) => {
+    const { children, ...rest } = props
+
+    return (
+      <colgroup ref={ref} {...rest}>
+        {children}
+      </colgroup>
+    )
+  }
+)
+
+ColumnGroup.displayName = 'ColumnGroup'
+
+export default ColumnGroup
