d6b995a411e1019dc43cf5916d75b11c47d0297d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:04:59 +0800

    refactor: :recycle: refactor props for `TableBody` React component

--- packages/react/components/src/lib/TableBody/TableBody.tsx
+++ packages/react/components/src/lib/TableBody/TableBody.tsx
@@ -4,15 +4,13 @@ export type TableBodyRef = HTMLTableSectionElement
 export type TableBodyProps = React.HTMLAttributes<TableBodyRef>
 
 /**
- * Render the table body component
+ * Render the table body component.
  * @param {TableBodyProps} props - The table body component properties
  * @param {TableBodyRef} ref - The table body component reference
  * @returns The rendered table body component
  */
 const TableBody = React.forwardRef<TableBodyRef, TableBodyProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <tbody ref={ref} {...rest}>
         {children}
