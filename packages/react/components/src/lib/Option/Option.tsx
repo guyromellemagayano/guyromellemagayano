06c3adad93725e2713a0343b8735b331d14b78b4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:22:27 +0800

    refactor: :recycle: refactor props for `Option` React component

--- packages/react/components/src/lib/Option/Option.tsx
+++ packages/react/components/src/lib/Option/Option.tsx
@@ -6,20 +6,20 @@ export type OptionRef = HTMLOptionElement
 export type OptionProps = React.OptionHTMLAttributes<OptionRef>
 
 /**
- * Render the option component
+ * Render the option component.
  * @param {OptionProps} props - The option component properties
  * @param {OptionRef} ref - The option component reference
  * @returns The rendered option component
  */
-const Option = React.forwardRef<OptionRef, OptionProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <option ref={ref} {...rest}>
-      {children}
-    </option>
-  )
-})
+const Option = React.forwardRef<OptionRef, OptionProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <option ref={ref} {...rest}>
+        {children}
+      </option>
+    )
+  }
+)
 
 Option.displayName = 'Option'
 
