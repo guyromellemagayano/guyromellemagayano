4ecb35995fc39c494b99bbcbd14b849772142595
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:07:04 +0800

    refactor: :recycle: refactor props `ColumnGroup` React component

--- packages/react/components/src/lib/ColumnGroup/ColumnGroup.tsx
+++ packages/react/components/src/lib/ColumnGroup/ColumnGroup.tsx
@@ -4,15 +4,13 @@ export type ColumnGroupRef = HTMLTableColElement
 export type ColumnGroupProps = React.ColgroupHTMLAttributes<ColumnGroupRef>
 
 /**
- * Render the column group component
+ * Render the column group component.
  * @param {ColumnGroupProps} props - The column group component properties
  * @param {ColumnGroupRef} ref - The column group component reference
  * @returns The rendered column group component
  */
 const ColumnGroup = React.forwardRef<ColumnGroupRef, ColumnGroupProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <colgroup ref={ref} {...rest}>
         {children}
