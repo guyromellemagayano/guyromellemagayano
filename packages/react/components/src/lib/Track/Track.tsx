1897b5b7b0259c0c82d4c8cef6ee7e237400fd6e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:09:30 +0800

    refactor: :recycle: refactor props for `Track` React component

--- packages/react/components/src/lib/Track/Track.tsx
+++ packages/react/components/src/lib/Track/Track.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import React from 'react'
 
 export type TrackRef = HTMLTrackElement
 export type TrackProps = React.TrackHTMLAttributes<TrackRef>
 
 /**
- * Render the track component
+ * Render the track component.
  * @param {TrackProps} props - The track component properties
  * @param {TrackRef} ref - The track component reference
  * @returns The rendered track component
  */
-const Track = React.forwardRef<TrackRef, TrackProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <track ref={ref} {...rest}>
-      {children}
-    </track>
-  )
-})
+const Track = React.forwardRef<TrackRef, TrackProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <track ref={ref} {...rest}>
+        {children}
+      </track>
+    )
+  }
+)
 
 Track.displayName = 'Track'
 
