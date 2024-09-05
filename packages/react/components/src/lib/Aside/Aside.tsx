471bb64a1be6416f18baf27c3ca4196ba136a909
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:01:30 +0800

    refactor: :recycle: refactor props for `Aside` React component

--- packages/react/components/src/lib/Aside/Aside.tsx
+++ packages/react/components/src/lib/Aside/Aside.tsx
@@ -4,20 +4,20 @@ export type AsideRef = HTMLElement
 export type AsideProps = React.HTMLAttributes<AsideRef>
 
 /**
- * Render the aside component
+ * Render the aside component.
  * @param {AsideProps} props - The aside component properties
  * @param {AsideRef} ref - The aside component reference
  * @returns The rendered aside component
  */
-const Aside = React.forwardRef<AsideRef, AsideProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <aside ref={ref} {...rest}>
-      {children}
-    </aside>
-  )
-})
+const Aside = React.forwardRef<AsideRef, AsideProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <aside ref={ref} {...rest}>
+        {children}
+      </aside>
+    )
+  }
+)
 
 Aside.displayName = 'Aside'
 
