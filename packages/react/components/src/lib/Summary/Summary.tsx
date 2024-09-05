20f5e8f31e79cd2b244f2686dba03d66b1c58ed0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:02:44 +0800

    refactor: :recycle: refactor props for `Summary` React component

--- packages/react/components/src/lib/Summary/Summary.tsx
+++ packages/react/components/src/lib/Summary/Summary.tsx
@@ -4,20 +4,20 @@ export type SummaryRef = HTMLElement
 export type SummaryProps = React.HTMLAttributes<SummaryRef>
 
 /**
- * Render the summary component
+ * Render the summary component.
  * @param {SummaryProps} props - The summary component properties
  * @param {SummaryRef} ref - The summary component reference
  * @returns The rendered summary component
  */
-const Summary = React.forwardRef<SummaryRef, SummaryProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <summary ref={ref} {...rest}>
-      {children}
-    </summary>
-  )
-})
+const Summary = React.forwardRef<SummaryRef, SummaryProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <summary ref={ref} {...rest}>
+        {children}
+      </summary>
+    )
+  }
+)
 
 Summary.displayName = 'Summary'
 
