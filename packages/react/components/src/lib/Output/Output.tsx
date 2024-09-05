222c0010e30b01448fd61ef0fb5d3fb2a61f24d5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:23:00 +0800

    refactor: :recycle: refactor props for `Output` React component

--- packages/react/components/src/lib/Output/Output.tsx
+++ packages/react/components/src/lib/Output/Output.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import React from 'react'
 
 export type OutputRef = HTMLOutputElement
 export type OutputProps = React.OutputHTMLAttributes<OutputRef>
 
 /**
- * Render the output component
+ * Render the output component.
  * @param {OutputProps} props - The output component properties
  * @param {OutputRef} ref - The output component reference
  * @returns The rendered output component
  */
-const Output = React.forwardRef<OutputRef, OutputProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <output ref={ref} {...rest}>
-      {children}
-    </output>
-  )
-})
+const Output = React.forwardRef<OutputRef, OutputProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <output ref={ref} {...rest}>
+        {children}
+      </output>
+    )
+  }
+)
 
 Output.displayName = 'Output'
 
