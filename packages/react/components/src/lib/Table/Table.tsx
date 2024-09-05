ed50929ff6b194cbadd4b7f2c68c78489c1d82a1
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:04:37 +0800

    refactor: :recycle: refactor props for `Table` React component

--- packages/react/components/src/lib/Table/Table.tsx
+++ packages/react/components/src/lib/Table/Table.tsx
@@ -4,20 +4,20 @@ export type TableRef = HTMLTableElement
 export type TableProps = React.TableHTMLAttributes<TableRef>
 
 /**
- * Render the table component
+ * Render the table component.
  * @param {TableProps} props - The table component properties
  * @param {TableRef} ref - The table component reference
  * @returns The rendered table component
  */
-const Table = React.forwardRef<TableRef, TableProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <table ref={ref} {...rest}>
-      {children}
-    </table>
-  )
-})
+const Table = React.forwardRef<TableRef, TableProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <table ref={ref} {...rest}>
+        {children}
+      </table>
+    )
+  }
+)
 
 Table.displayName = 'Table'
 
