d0b7e74f879ee2de0c36cd958c981bd780804f2c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:24:12 +0800

    refactor: :recycle: refactor props for `Progress` React component

--- packages/react/components/src/lib/Progress/Progress.tsx
+++ packages/react/components/src/lib/Progress/Progress.tsx
@@ -6,20 +6,20 @@ export type ProgressRef = HTMLProgressElement
 export type ProgressProps = React.ProgressHTMLAttributes<ProgressRef>
 
 /**
- * Render the progress component
+ * Render the progress component.
  * @param {ProgressProps} props - The progress component properties
  * @param {ProgressRef} ref - The progress component reference
  * @returns The rendered progress component
  */
-const Progress = React.forwardRef<ProgressRef, ProgressProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <progress ref={ref} {...rest}>
-      {children}
-    </progress>
-  )
-})
+const Progress = React.forwardRef<ProgressRef, ProgressProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <progress ref={ref} {...rest}>
+        {children}
+      </progress>
+    )
+  }
+)
 
 Progress.displayName = 'Progress'
 
