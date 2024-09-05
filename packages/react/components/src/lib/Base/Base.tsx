e8dc6c1f5fffb058d4b0aeef2660af9a73bbfa2d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:02:19 +0800

    refactor: :recycle: refactor props for `Base` React component

--- packages/react/components/src/lib/Base/Base.tsx
+++ packages/react/components/src/lib/Base/Base.tsx
@@ -4,20 +4,20 @@ export type BaseRef = HTMLBaseElement
 export type BaseProps = React.BaseHTMLAttributes<BaseRef>
 
 /**
- * Render the base component
+ * Render the base component.
  * @param {BaseProps} props - The base component properties
  * @param {BaseRef} ref - The base component reference
  * @returns The rendered base component
  */
-const Base = React.forwardRef<BaseRef, BaseProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <base ref={ref} {...rest}>
-      {children}
-    </base>
-  )
-})
+const Base = React.forwardRef<BaseRef, BaseProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <base ref={ref} {...rest}>
+        {children}
+      </base>
+    )
+  }
+)
 
 Base.displayName = 'Base'
 
