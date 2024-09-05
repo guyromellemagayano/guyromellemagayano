a4c42b8fa7f42b9dc03924513ece79c2a0dd18bd
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:50:32 +0800

    refactor: :recycle: refactor props for `Label` React component

--- packages/react/components/src/lib/Label/Label.tsx
+++ packages/react/components/src/lib/Label/Label.tsx
@@ -6,20 +6,20 @@ export type LabelRef = HTMLLabelElement
 export type LabelProps = React.LabelHTMLAttributes<LabelRef>
 
 /**
- * Render the label component
+ * Render the label component.
  * @param {LabelProps} props - The label component properties
  * @param {LabelRef} ref - The label component reference
  * @returns The rendered label component
  */
-const Label = React.forwardRef<LabelRef, LabelProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <label ref={ref} {...rest}>
-      {children}
-    </label>
-  )
-})
+const Label = React.forwardRef<LabelRef, LabelProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <label ref={ref} {...rest}>
+        {children}
+      </label>
+    )
+  }
+)
 
 Label.displayName = 'Label'
 
