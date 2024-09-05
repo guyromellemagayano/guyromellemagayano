654c89f8dba985c14e7043cb2fce7b1eaf15194e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:21:30 +0800

    refactor: :recycle: refactor props for `Meta` React component

--- packages/react/components/src/lib/Meter/Meter.tsx
+++ packages/react/components/src/lib/Meter/Meter.tsx
@@ -6,20 +6,20 @@ export type MeterRef = HTMLMeterElement
 export type MeterProps = React.MeterHTMLAttributes<MeterRef>
 
 /**
- * Render the meter component
+ * Render the meter component.
  * @param {MeterProps} props - The meter component properties
  * @param {MeterRef} ref - The meter component reference
  * @returns The rendered meter component
  */
-const Meter = React.forwardRef<MeterRef, MeterProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <meter ref={ref} {...rest}>
-      {children}
-    </meter>
-  )
-})
+const Meter = React.forwardRef<MeterRef, MeterProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <meter ref={ref} {...rest}>
+        {children}
+      </meter>
+    )
+  }
+)
 
 Meter.displayName = 'Meter'
 
