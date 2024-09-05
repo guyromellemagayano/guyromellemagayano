8a93ebe21c8003cb9990db039e4027c2da0d15c4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:21:43 +0800

    refactor: :recycle: refactor props for `Navigation` React component

--- packages/react/components/src/lib/Navigation/Navigation.tsx
+++ packages/react/components/src/lib/Navigation/Navigation.tsx
@@ -4,15 +4,13 @@ export type NavigationRef = HTMLElement
 export type NavigationProps = React.HTMLAttributes<NavigationRef>
 
 /**
- * Render the navigation component
+ * Render the navigation component.
  * @param {NavigationProps} props - The navigation component properties
  * @param {NavigationRef} ref - The navigation component reference
  * @returns The rendered navigation component
  */
 const Navigation = React.forwardRef<NavigationRef, NavigationProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <nav ref={ref} {...rest}>
         {children}
