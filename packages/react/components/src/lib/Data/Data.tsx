5371cefb8e7da1837c35a8bf5b11fb96f7804b6c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:07:37 +0800

    refactor: :recycle: refactor props for `Data` React component

--- packages/react/components/src/lib/Data/Data.tsx
+++ packages/react/components/src/lib/Data/Data.tsx
@@ -4,20 +4,20 @@ export type DataRef = HTMLDataElement
 export type DataProps = React.DataHTMLAttributes<DataRef>
 
 /**
- * Render the data component
+ * Render the data component.
  * @param {DataProps} props - The data component properties
  * @param {DataRef} ref - The data component reference
  * @returns The rendered data component
  */
-const Data = React.forwardRef<DataRef, DataProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <data ref={ref} {...rest}>
-      {children}
-    </data>
-  )
-})
+const Data = React.forwardRef<DataRef, DataProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <data ref={ref} {...rest}>
+        {children}
+      </data>
+    )
+  }
+)
 
 Data.displayName = 'Data'
 
