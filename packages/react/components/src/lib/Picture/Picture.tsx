ff471978b02d63315e99c2946616d6bfed694eba
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:23:37 +0800

    refactor: :recycle: refactor props for `Picture` React component

--- packages/react/components/src/lib/Picture/Picture.tsx
+++ packages/react/components/src/lib/Picture/Picture.tsx
@@ -6,20 +6,20 @@ export type PictureRef = HTMLPictureElement
 export type PictureProps = React.HTMLAttributes<PictureRef>
 
 /**
- * Render the picture component
+ * Render the picture component.
  * @param {PictureProps} props - The picture component properties
  * @param {PictureRef} ref - The picture component reference
  * @returns The rendered picture component
  */
-const Picture = React.forwardRef<PictureRef, PictureProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <picture ref={ref} {...rest}>
-      {children}
-    </picture>
-  )
-})
+const Picture = React.forwardRef<PictureRef, PictureProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <picture ref={ref} {...rest}>
+        {children}
+      </picture>
+    )
+  }
+)
 
 Picture.displayName = 'Picture'
 
