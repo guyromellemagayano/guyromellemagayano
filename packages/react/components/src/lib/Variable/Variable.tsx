5b275424515242f800542d6125069a1fcf85c883
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:11:13 +0800

    refactor: :recycle: refactor props for `Variable` React component

--- packages/react/components/src/lib/Variable/Variable.tsx
+++ packages/react/components/src/lib/Variable/Variable.tsx
@@ -4,20 +4,20 @@ export type VariableRef = HTMLElement
 export type VariableProps = React.HTMLAttributes<VariableRef>
 
 /**
- * Render the variable component
+ * Render the variable component.
  * @param {VariableProps} props - The variable component properties
  * @param {VariableRef} ref - The variable component reference
  * @returns The rendered variable component
  */
-const Variable = React.forwardRef<VariableRef, VariableProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <var ref={ref} {...rest}>
-      {children}
-    </var>
-  )
-})
+const Variable = React.forwardRef<VariableRef, VariableProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <var ref={ref} {...rest}>
+        {children}
+      </var>
+    )
+  }
+)
 
 Variable.displayName = 'Variable'
 
