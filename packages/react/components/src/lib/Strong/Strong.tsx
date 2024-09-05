72214ce556786e749ad1ad884bffa5eb318ec890
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:02:29 +0800

    refactor: :recycle: refactor props for `Strong` React component

--- packages/react/components/src/lib/Strong/Strong.tsx
+++ packages/react/components/src/lib/Strong/Strong.tsx
@@ -4,20 +4,20 @@ export type StrongRef = HTMLElement
 export type StrongProps = React.HTMLAttributes<StrongRef>
 
 /**
- * Render the strong component
+ * Render the strong component.
  * @param {StrongProps} props - The strong component properties
  * @param {StrongRef} ref - The strong component reference
  * @returns The rendered strong component
  */
-const Strong = React.forwardRef<StrongRef, StrongProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <strong ref={ref} {...rest}>
-      {children}
-    </strong>
-  )
-})
+const Strong = React.forwardRef<StrongRef, StrongProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <strong ref={ref} {...rest}>
+        {children}
+      </strong>
+    )
+  }
+)
 
 Strong.displayName = 'Strong'
 
