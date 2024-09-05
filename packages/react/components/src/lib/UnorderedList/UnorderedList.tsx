6c66e7237fa323be5eff1f08760b41813bd9b7ed
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:10:06 +0800

    refactor: :recycle: refactor props for `UnorderedList` React component

--- packages/react/components/src/lib/UnorderedList/UnorderedList.tsx
+++ packages/react/components/src/lib/UnorderedList/UnorderedList.tsx
@@ -4,15 +4,13 @@ export type UnorderedListRef = HTMLUListElement
 export type UnorderedListProps = React.HTMLAttributes<UnorderedListRef>
 
 /**
- * Render the unordered list component
+ * Render the unordered list component.
  * @param {UnorderedListProps} props - The unordered list component properties
  * @param {UnorderedListRef} ref - The unordered list component reference
  * @returns The rendered unordered list component
  */
 const UnorderedList = React.forwardRef<UnorderedListRef, UnorderedListProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <ul ref={ref} {...rest}>
         {children}
