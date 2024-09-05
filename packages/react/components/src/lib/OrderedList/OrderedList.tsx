4f7ca6ea3438ee64a98d6c2666307e823e705dc3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:22:42 +0800

    refactor: :recycle: refactor props for `OrderedList` React component

--- packages/react/components/src/lib/OrderedList/OrderedList.tsx
+++ packages/react/components/src/lib/OrderedList/OrderedList.tsx
@@ -4,15 +4,13 @@ export type OrderedListRef = HTMLOListElement
 export type OrderedListProps = React.OlHTMLAttributes<OrderedListRef>
 
 /**
- * Render the ordered list component
+ * Render the ordered list component.
  * @param {OrderedListProps} props - The ordered list component properties
  * @param {OrderedListRef} ref - The ordered list component reference
  * @returns The rendered ordered list component
  */
 const OrderedList = React.forwardRef<OrderedListRef, OrderedListProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <ol ref={ref} {...rest}>
         {children}
