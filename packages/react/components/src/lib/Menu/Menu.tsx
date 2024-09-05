18d253e501cb87fd29a97f7ec8ee9a58fccecc17
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:20:59 +0800

    refactor: :recycle: refactor props for `Menu` React component

--- packages/react/components/src/lib/Menu/Menu.tsx
+++ packages/react/components/src/lib/Menu/Menu.tsx
@@ -6,20 +6,20 @@ export type MenuRef = HTMLElement
 export type MenuProps = React.HTMLAttributes<MenuRef>
 
 /**
- * Render the menu component
+ * Render the menu component.
  * @param {MenuProps} props - The menu component properties
  * @param {MenuRef} ref - The menu component reference
  * @returns The rendered menu component
  */
-const Menu = React.forwardRef<MenuRef, MenuProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <menu ref={ref} {...rest}>
-      {children}
-    </menu>
-  )
-})
+const Menu = React.forwardRef<MenuRef, MenuProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <menu ref={ref} {...rest}>
+        {children}
+      </menu>
+    )
+  }
+)
 
 Menu.displayName = 'Menu'
 
