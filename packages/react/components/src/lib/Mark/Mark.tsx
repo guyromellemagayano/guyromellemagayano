23e08eedbe6c66fb794c09bd1dbc3604884ca79b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:16:08 +0800

    refactor: :recycle: refactor props for `Mark` React component

--- packages/react/components/src/lib/Mark/Mark.tsx
+++ packages/react/components/src/lib/Mark/Mark.tsx
@@ -4,20 +4,20 @@ export type MarkRef = HTMLElement
 export type MarkProps = React.HTMLAttributes<MarkRef>
 
 /**
- * Render the mark component
+ * Render the mark component.
  * @param {MarkProps} props - The mark component properties
  * @param {MarkRef} ref - The mark component reference
  * @returns The rendered mark component
  */
-const Mark = React.forwardRef<MarkRef, MarkProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <mark ref={ref} {...rest}>
-      {children}
-    </mark>
-  )
-})
+const Mark = React.forwardRef<MarkRef, MarkProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <mark ref={ref} {...rest}>
+        {children}
+      </mark>
+    )
+  }
+)
 
 Mark.displayName = 'Mark'
 
