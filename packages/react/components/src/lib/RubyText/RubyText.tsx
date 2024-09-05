7aec03a41360d2157264b5c5f012d7b2ed927dea
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:27:02 +0800

    refactor: :recycle: refactor props for `RubyText` React component

--- packages/react/components/src/lib/RubyText/RubyText.tsx
+++ packages/react/components/src/lib/RubyText/RubyText.tsx
@@ -4,20 +4,20 @@ export type RubyTextRef = HTMLElement
 export type RubyTextProps = React.HTMLAttributes<RubyTextRef>
 
 /**
- * Render the ruby text component
+ * Render the ruby text component.
  * @param {RubyTextProps} props - The ruby text component properties
  * @param {RubyTextRef} ref - The ruby text component reference
  * @returns The rendered ruby text component
  */
-const RubyText = React.forwardRef<RubyTextRef, RubyTextProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <rt ref={ref} {...rest}>
-      {children}
-    </rt>
-  )
-})
+const RubyText = React.forwardRef<RubyTextRef, RubyTextProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <rt ref={ref} {...rest}>
+        {children}
+      </rt>
+    )
+  }
+)
 
 RubyText.displayName = 'RubyText'
 
