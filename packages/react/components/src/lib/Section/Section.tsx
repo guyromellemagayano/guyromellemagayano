273a98df1257f5508bbb6407473195c29fcb7c9a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:28:48 +0800

    refactor: :recycle: refactor props for `Section` React component

--- packages/react/components/src/lib/Section/Section.tsx
+++ packages/react/components/src/lib/Section/Section.tsx
@@ -4,20 +4,20 @@ export type SectionRef = HTMLElement
 export type SectionProps = React.HTMLAttributes<SectionRef>
 
 /**
- * Render the section component
+ * Render the section component.
  * @param {SectionProps} props - The section component properties
  * @param {SectionRef} ref - The section component reference
  * @returns The rendered section component
  */
-const Section = React.forwardRef<SectionRef, SectionProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <section ref={ref} {...rest}>
-      {children}
-    </section>
-  )
-})
+const Section = React.forwardRef<SectionRef, SectionProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <section ref={ref} {...rest}>
+        {children}
+      </section>
+    )
+  }
+)
 
 Section.displayName = 'Section'
 
