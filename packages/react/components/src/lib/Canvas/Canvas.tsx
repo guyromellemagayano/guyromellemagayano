3ac8c59c4a876386a470d720405c1fd28c656957
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:04:18 +0800

    refactor: :recycle: refactor props for `Canvas` React component

--- packages/react/components/src/lib/Canvas/Canvas.tsx
+++ packages/react/components/src/lib/Canvas/Canvas.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import React from 'react'
 
 export type CanvasRef = HTMLCanvasElement
 export type CanvasProps = React.CanvasHTMLAttributes<CanvasRef>
 
 /**
- * Render the canvas component
+ * Render the canvas component.
  * @param {CanvasProps} props - The canvas component properties
  * @param {CanvasRef} ref - The canvas component reference
  * @returns The rendered canvas component
  */
-const Canvas = React.forwardRef<CanvasRef, CanvasProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <canvas ref={ref} {...rest}>
-      {children}
-    </canvas>
-  )
-})
+const Canvas = React.forwardRef<CanvasRef, CanvasProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <canvas ref={ref} {...rest}>
+        {children}
+      </canvas>
+    )
+  }
+)
 
 Canvas.displayName = 'Canvas'
 
