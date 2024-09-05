7d4d475c682525d9570a903f430fb0ba59599ef4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:04:48 +0800

    refactor: :recycle: refactor props for `Cite` React component

--- packages/react/components/src/lib/Cite/Cite.tsx
+++ packages/react/components/src/lib/Cite/Cite.tsx
@@ -4,20 +4,20 @@ export type CiteRef = HTMLQuoteElement
 export type CiteProps = React.HTMLAttributes<CiteRef>
 
 /**
- * Render the cite component
+ * Render the cite component.
  * @param {CiteProps} props - The cite component properties
  * @param {CiteRef} ref - The cite component reference
  * @returns The rendered cite component
  */
-const Cite = React.forwardRef<CiteRef, CiteProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <cite ref={ref} {...rest}>
-      {children}
-    </cite>
-  )
-})
+const Cite = React.forwardRef<CiteRef, CiteProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <cite ref={ref} {...rest}>
+        {children}
+      </cite>
+    )
+  }
+)
 
 Cite.displayName = 'Cite'
 
