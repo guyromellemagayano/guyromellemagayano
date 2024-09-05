8699e41b3f602974344b04443dae8f5b244cff55
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:04:19 +0800

    refactor: :recycle: refactor props and remove `use client` for `Svg` React component

--- packages/react/components/src/lib/Svg/Svg.tsx
+++ packages/react/components/src/lib/Svg/Svg.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import React from 'react'
 
 export type SvgRef = SVGSVGElement
 export type SvgProps = React.SVGProps<SvgRef>
 
 /**
- * Renders a SVG image
+ * Renders a SVG image.
  * @param {SvgProps} props - The SVG image properties
  * @param {SvgRef} ref - The SVG image reference
  * @returns The rendered SVG image
  */
-export const Svg = React.forwardRef<SvgRef, SvgProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <svg ref={ref} {...rest}>
-      {children}
-    </svg>
-  )
-})
+export const Svg = React.forwardRef<SvgRef, SvgProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <svg ref={ref} {...rest}>
+        {children}
+      </svg>
+    )
+  }
+)
 
 Svg.displayName = 'SVG'
 
@@ -27,13 +25,11 @@ export type SvgPathRef = SVGPathElement
 export type SvgPathProps = React.SVGProps<SvgPathRef>
 
 /**
- * Renders a shared SVG path
+ * Renders a shared SVG path.
  * @param {SvgPathProps} props - The shared SVG path properties
  * @returns The rendered shared SVG path
  */
-export const SvgPath = (props: SvgPathProps) => {
-  const { ...rest } = props
-
+export const SvgPath = ({ ...rest }: SvgPathProps) => {
   return <path {...rest} />
 }
 
