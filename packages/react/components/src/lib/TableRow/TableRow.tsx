91624d07667d27a844953bd3353f0abc348398d6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:06:35 +0800

    refactor: :recycle: refactor props for `TableRow` React component

--- packages/react/components/src/lib/TableRow/TableRow.tsx
+++ packages/react/components/src/lib/TableRow/TableRow.tsx
@@ -4,20 +4,20 @@ export type TableRowRef = HTMLTableRowElement
 export type TableRowProps = React.HTMLAttributes<TableRowRef>
 
 /**
- * Render the table row component
+ * Render the table row component.
  * @param {TableRowProps} props - The table row component properties
  * @param {TableRowRef} ref - The table row component reference
  * @returns The rendered table row component
  */
-const TableRow = React.forwardRef<TableRowRef, TableRowProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <tr ref={ref} {...rest}>
-      {children}
-    </tr>
-  )
-})
+const TableRow = React.forwardRef<TableRowRef, TableRowProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <tr ref={ref} {...rest}>
+        {children}
+      </tr>
+    )
+  }
+)
 
 TableRow.displayName = 'TableRow'
 
