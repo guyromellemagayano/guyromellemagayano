b08bf8507d7c420bc98028e8ee2ee93951ce83a5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:15:36 +0800

    refactor: :recycle: refactor props for `Main` React component

--- packages/react/components/src/lib/Main/Main.tsx
+++ packages/react/components/src/lib/Main/Main.tsx
@@ -4,20 +4,20 @@ export type MainRef = HTMLElement
 export type MainProps = React.HTMLAttributes<MainRef>
 
 /**
- * Render the main component
+ * Render the main component.
  * @param {MainProps} props - The main component properties
  * @param {MainRef} ref - The main component reference
  * @returns The rendered main component
  */
-const Main = React.forwardRef<MainRef, MainProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <main ref={ref} {...rest}>
-      {children}
-    </main>
-  )
-})
+const Main = React.forwardRef<MainRef, MainProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <main ref={ref} {...rest}>
+        {children}
+      </main>
+    )
+  }
+)
 
 Main.displayName = 'Main'
 
