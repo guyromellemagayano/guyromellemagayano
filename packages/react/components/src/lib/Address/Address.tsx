d964e4b7523f3a1154e1abb6bbd4a848b59d10e3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 11:56:04 +0800

    refactor: :recycle: refactor props for `Address` React component

--- packages/react/components/src/lib/Address/Address.tsx
+++ packages/react/components/src/lib/Address/Address.tsx
@@ -4,20 +4,20 @@ export type AddressRef = HTMLElement
 export type AddressProps = React.HTMLAttributes<AddressRef>
 
 /**
- * Render the address component
+ * Render the address component.
  * @param {AddressProps} props - The address component properties
  * @param {AddressRef} ref - The address component reference
  * @returns The rendered address component
  */
-const Address = React.forwardRef<AddressRef, AddressProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <address ref={ref} {...rest}>
-      {children}
-    </address>
-  )
-})
+const Address = React.forwardRef<AddressRef, AddressProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <address ref={ref} {...rest}>
+        {children}
+      </address>
+    )
+  }
+)
 
 Address.displayName = 'Address'
 
