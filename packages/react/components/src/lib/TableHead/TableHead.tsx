8ef47bca5e2ee1dfc0920996203a258757fc8db1
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:06:05 +0800

    refactor: :recycle: refactor props for `TableHead` React component

--- packages/react/components/src/lib/TableHead/TableHead.tsx
+++ packages/react/components/src/lib/TableHead/TableHead.tsx
@@ -4,15 +4,13 @@ export type TableHeadRef = HTMLTableSectionElement
 export type TableHeadProps = React.HTMLAttributes<TableHeadRef>
 
 /**
- * Render the table head component
+ * Render the table head component.
  * @param {TableHeadProps} props - The table head component properties
  * @param {TableHeadRef} ref - The table head component reference
  * @returns The rendered table head component
  */
 const TableHead = React.forwardRef<TableHeadRef, TableHeadProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <thead ref={ref} {...rest}>
         {children}
