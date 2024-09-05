271a24ea4558ec9a09c1c61ba5782ea68c7e9b44
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:03:28 +0800

    refactor: :recycle: refactor props and remove `use client` for `Superscript` React component

--- packages/react/components/src/lib/Superscript/Superscript.tsx
+++ packages/react/components/src/lib/Superscript/Superscript.tsx
@@ -1,20 +1,16 @@
-'use client'
-
 import React from 'react'
 
 export type SuperscriptRef = HTMLElement
 export type SuperscriptProps = React.HTMLAttributes<SuperscriptRef>
 
 /**
- * Render the superscript component
+ * Render the superscript component.
  * @param {SuperscriptProps} props - The superscript component properties
  * @param {SuperscriptRef} ref - The superscript component reference
  * @returns The rendered superscript component
  */
 const Superscript = React.forwardRef<SuperscriptRef, SuperscriptProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <sup ref={ref} {...rest}>
         {children}
