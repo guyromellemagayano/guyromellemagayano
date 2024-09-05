f759c1138cb75d9cb3a257cd1fdcad7076b4beba
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:22:15 +0800

    refactor: :recycle: refactor props for `Optgroup` React component

--- packages/react/components/src/lib/Optgroup/Optgroup.tsx
+++ packages/react/components/src/lib/Optgroup/Optgroup.tsx
@@ -6,20 +6,20 @@ export type OptgroupRef = HTMLOptGroupElement
 export type OptgroupProps = React.OptgroupHTMLAttributes<OptgroupRef>
 
 /**
- * Render the optgroup component
+ * Render the optgroup component.
  * @param {OptgroupProps} props - The optgroup component properties
  * @param {OptgroupRef} ref - The optgroup component reference
  * @returns The rendered optgroup component
  */
-const Optgroup = React.forwardRef<OptgroupRef, OptgroupProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <optgroup ref={ref} {...rest}>
-      {children}
-    </optgroup>
-  )
-})
+const Optgroup = React.forwardRef<OptgroupRef, OptgroupProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <optgroup ref={ref} {...rest}>
+        {children}
+      </optgroup>
+    )
+  }
+)
 
 Optgroup.displayName = 'Optgroup'
 
