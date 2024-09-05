ba504439b3b61bb587c8db0c81aa01eba5668ed7
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:01:16 +0800

    refactor: :recycle: refactor props for `Article` React component

--- packages/react/components/src/lib/Article/Article.tsx
+++ packages/react/components/src/lib/Article/Article.tsx
@@ -4,20 +4,20 @@ export type ArticleRef = HTMLElement
 export type ArticleProps = React.HTMLAttributes<ArticleRef>
 
 /**
- * Render the article component
+ * Render the article component.
  * @param {ArticleProps} props - The article component properties
  * @param {ArticleRef} ref - The article component reference
  * @returns The rendered article component
  */
-const Article = React.forwardRef<ArticleRef, ArticleProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <article ref={ref} {...rest}>
-      {children}
-    </article>
-  )
-})
+const Article = React.forwardRef<ArticleRef, ArticleProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <article ref={ref} {...rest}>
+        {children}
+      </article>
+    )
+  }
+)
 
 Article.displayName = 'Article'
 
