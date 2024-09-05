ca391a649c8a26e265e0aa087bc7aacae8be17d1
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:12:30 +0800

    refactor: :recycle: update `CommonComponentProps` in React component types

--- packages/react/components/src/components.d.ts
+++ packages/react/components/src/components.d.ts
@@ -1,6 +1,10 @@
+import { ReactNode } from 'react'
+
 // ====================================================
 // Common Component Prop Types
 // ====================================================
 export type CommonComponentsProps = {
+  children?: ReactNode
+  className?: string
   [key: string]: any
 }
