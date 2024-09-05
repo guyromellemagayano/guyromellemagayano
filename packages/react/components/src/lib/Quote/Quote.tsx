05f6c1a8ceec81ec82063da5a5b2c2e2fb76a515
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:24:27 +0800

    refactor: :recycle: refactor props for `Quote` React component

--- packages/react/components/src/lib/Quote/Quote.tsx
+++ packages/react/components/src/lib/Quote/Quote.tsx
@@ -4,20 +4,20 @@ export type QuoteRef = HTMLQuoteElement
 export type QuoteProps = React.HTMLAttributes<QuoteRef>
 
 /**
- * Render the quote component
+ * Render the quote component.
  * @param {QuoteProps} props - The quote component properties
  * @param {QuoteRef} ref - The quote component reference
  * @returns The rendered quote component
  */
-const Quote = React.forwardRef<QuoteRef, QuoteProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <q ref={ref} {...rest}>
-      {children}
-    </q>
-  )
-})
+const Quote = React.forwardRef<QuoteRef, QuoteProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <q ref={ref} {...rest}>
+        {children}
+      </q>
+    )
+  }
+)
 
 Quote.displayName = 'Quote'
 
