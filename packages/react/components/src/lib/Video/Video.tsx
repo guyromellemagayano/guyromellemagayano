3d6fe55fe9b49cd5b38eebe7a87077e6ec247590
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:11:26 +0800

    refactor: :recycle: refactor props for `Video` React component

--- packages/react/components/src/lib/Video/Video.tsx
+++ packages/react/components/src/lib/Video/Video.tsx
@@ -6,20 +6,20 @@ export type VideoRef = HTMLVideoElement
 export type VideoProps = React.VideoHTMLAttributes<VideoRef>
 
 /**
- * Render the video component
+ * Render the video component.
  * @param {VideoProps} props - The video component properties
  * @param {VideoRef} ref - The video component reference
  * @returns The rendered video component
  */
-const Video = React.forwardRef<VideoRef, VideoProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <video ref={ref} {...rest}>
-      {children}
-    </video>
-  )
-})
+const Video = React.forwardRef<VideoRef, VideoProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <video ref={ref} {...rest}>
+        {children}
+      </video>
+    )
+  }
+)
 
 Video.displayName = 'Video'
 
