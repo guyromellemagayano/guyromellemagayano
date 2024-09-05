03a68994cea0c65577a6472224f94e88d7350a25
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:49:48 +0800

    refactor: :recycle: refactor props for `InsertedText` React component

--- packages/react/components/src/lib/InsertedText/InsertedText.tsx
+++ packages/react/components/src/lib/InsertedText/InsertedText.tsx
@@ -4,15 +4,13 @@ export type InsertedTextRef = HTMLModElement
 export type InsertedTextProps = React.InsHTMLAttributes<InsertedTextRef>
 
 /**
- * Render the inserted text component
+ * Render the inserted text component.
  * @param {InsertedTextProps} props - The inserted text component properties
  * @param {InsertedTextRef} ref - The inserted text component reference
  * @returns The rendered inserted text component
  */
 const InsertedText = React.forwardRef<InsertedTextRef, InsertedTextProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <ins ref={ref} {...rest}>
         {children}
