918fc2c6d61a1eb91eb3d1c6216107841012184b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:48:12 +0800

    refactor: :recycle: refactor props for `HeadingGroup` React component

--- packages/react/components/src/lib/HeadingGroup/HeadingGroup.tsx
+++ packages/react/components/src/lib/HeadingGroup/HeadingGroup.tsx
@@ -4,15 +4,13 @@ export type HeadingGroupRef = HTMLElement
 export type HeadingGroupProps = React.HTMLAttributes<HeadingGroupRef>
 
 /**
- * Render the heading group component
+ * Render the heading group component.
  * @param {HeadingGroupProps} props - The heading group component properties
  * @param {HeadingGroupRef} ref - The heading group component reference
  * @returns The rendered heading group component
  */
 const HeadingGroup = React.forwardRef<HeadingGroupRef, HeadingGroupProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <hgroup ref={ref} {...rest}>
         {children}
