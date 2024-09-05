47033795699b8fd1160c47ac57e25ccc98b7337e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:01:45 +0800

    refactor: :recycle: refactor props and remove `use client` for `Audio` React component

--- packages/react/components/src/lib/Audio/Audio.tsx
+++ packages/react/components/src/lib/Audio/Audio.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import React from 'react'
 
 export type AudioRef = HTMLAudioElement
 export type AudioProps = React.AudioHTMLAttributes<AudioRef>
 
 /**
- * Render the audio component
+ * Render the audio component.
  * @param {AudioProps} props - The audio component properties
  * @param {AudioRef} ref - The audio component reference
  * @returns The rendered audio component
  */
-const Audio = React.forwardRef<AudioRef, AudioProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <audio ref={ref} {...rest}>
-      {children}
-    </audio>
-  )
-})
+const Audio = React.forwardRef<AudioRef, AudioProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <audio ref={ref} {...rest}>
+        {children}
+      </audio>
+    )
+  }
+)
 
 Audio.displayName = 'Audio'
 
