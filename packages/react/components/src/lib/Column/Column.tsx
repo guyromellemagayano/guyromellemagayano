99fc048bdd4934d0f091ae4e343f763885876f68
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:06:21 +0800

    refactor: :recycle: refactor props for `Column` React component

--- packages/react/components/src/lib/Column/Column.tsx
+++ packages/react/components/src/lib/Column/Column.tsx
@@ -4,20 +4,20 @@ export type ColumnRef = HTMLTableColElement
 export type ColumnProps = React.ColHTMLAttributes<ColumnRef>
 
 /**
- * Render the column component
+ * Render the column component.
  * @param {ColumnProps} props - The column component properties
  * @param {ColumnRef} ref - The column component reference
  * @returns The rendered column component
  */
-const Column = React.forwardRef<ColumnRef, ColumnProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <col ref={ref} {...rest}>
-      {children}
-    </col>
-  )
-})
+const Column = React.forwardRef<ColumnRef, ColumnProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <col ref={ref} {...rest}>
+        {children}
+      </col>
+    )
+  }
+)
 
 Column.displayName = 'Column'
 
