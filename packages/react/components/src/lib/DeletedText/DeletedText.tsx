104fc241c95fdd8cdf213e4a425f79c2d0461b6c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:28:30 +0800

    refactor: :recycle: refactor props for `DeletedText` React component

--- packages/react/components/src/lib/DeletedText/DeletedText.tsx
+++ packages/react/components/src/lib/DeletedText/DeletedText.tsx
@@ -4,15 +4,13 @@ export type DeletedTextRef = HTMLModElement
 export type DeletedTextProps = React.DelHTMLAttributes<DeletedTextRef>
 
 /**
- * Render the deleted text component
+ * Render the deleted text component.
  * @param {DeletedTextProps} props - The deleted text component properties
  * @param {DeletedTextRef} ref - The deleted text component reference
  * @returns The rendered deleted text component
  */
 const DeletedText = React.forwardRef<DeletedTextRef, DeletedTextProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <del ref={ref} {...rest}>
         {children}
