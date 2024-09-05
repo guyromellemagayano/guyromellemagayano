25b50fec41ceadff9c4dbfc0257ebc37a7a39ec1
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:50:04 +0800

    refactor: :recycle: refactor props for `Italic` React component

--- packages/react/components/src/lib/Italic/Italic.tsx
+++ packages/react/components/src/lib/Italic/Italic.tsx
@@ -4,20 +4,20 @@ export type ItalicRef = HTMLElement
 export type ItalicProps = React.HTMLAttributes<ItalicRef>
 
 /**
- * Render the italic component
+ * Render the italic component.
  * @param {ItalicProps} props - The italic component properties
  * @param {ItalicRef} ref - The italic component reference
  * @returns The rendered italic component
  */
-const Italic = React.forwardRef<ItalicRef, ItalicProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <i ref={ref} {...rest}>
-      {children}
-    </i>
-  )
-})
+const Italic = React.forwardRef<ItalicRef, ItalicProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <i ref={ref} {...rest}>
+        {children}
+      </i>
+    )
+  }
+)
 
 Italic.displayName = 'Italic'
 
