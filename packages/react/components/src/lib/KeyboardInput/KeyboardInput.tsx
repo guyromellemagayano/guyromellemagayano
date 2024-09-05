389c6e8c412bf67d70045af778ba1caa37be031f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:50:18 +0800

    refactor: :recycle: refactor props for `KeyboardInput` React component

--- packages/react/components/src/lib/KeyboardInput/KeyboardInput.tsx
+++ packages/react/components/src/lib/KeyboardInput/KeyboardInput.tsx
@@ -4,15 +4,13 @@ export type KeyboardInputRef = HTMLElement
 export type KeyboardInputProps = React.HTMLAttributes<KeyboardInputRef>
 
 /**
- * Render the keyboard input component
+ * Render the keyboard input component.
  * @param {KeyboardInputProps} props - The keyboard input component properties
  * @param {KeyboardInputRef} ref - The keyboard input component reference
  * @returns The rendered keyboard input component
  */
 const KeyboardInput = React.forwardRef<KeyboardInputRef, KeyboardInputProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <kbd ref={ref} {...rest}>
         {children}
