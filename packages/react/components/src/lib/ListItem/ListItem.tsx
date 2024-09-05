9a1a6cfa7f98b6e35936e667e37f487e03432987
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:51:25 +0800

    refactor: :recycle: refactor props for `ListItem` React component

--- packages/react/components/src/lib/ListItem/ListItem.tsx
+++ packages/react/components/src/lib/ListItem/ListItem.tsx
@@ -4,20 +4,20 @@ export type ListItemRef = HTMLLIElement
 export type ListItemProps = React.LiHTMLAttributes<ListItemRef>
 
 /**
- * Render the list item component
+ * Render the list item component.
  * @param {ListItemProps} props - The list item component properties
  * @param {ListItemRef} ref - The list item component reference
  * @returns The rendered list item component
  */
-const ListItem = React.forwardRef<ListItemRef, ListItemProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <li ref={ref} {...rest}>
-      {children}
-    </li>
-  )
-})
+const ListItem = React.forwardRef<ListItemRef, ListItemProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <li ref={ref} {...rest}>
+        {children}
+      </li>
+    )
+  }
+)
 
 ListItem.displayName = 'ListItem'
 
