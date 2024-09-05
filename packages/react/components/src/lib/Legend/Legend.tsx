b64890af7f622443aa98ab3c372e144a32431a14
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:50:52 +0800

    refactor: :recycle: refactor props for `Legend` React component

--- packages/react/components/src/lib/Legend/Legend.tsx
+++ packages/react/components/src/lib/Legend/Legend.tsx
@@ -6,20 +6,20 @@ export type LegendRef = HTMLLegendElement
 export type LegendProps = React.HTMLAttributes<LegendRef>
 
 /**
- * Render the legend component
+ * Render the legend component.
  * @param {LegendProps} props - The legend component properties
  * @param {LegendRef} ref - The legend component reference
  * @returns The rendered legend component
  */
-const Legend = React.forwardRef<LegendRef, LegendProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <legend ref={ref} {...rest}>
-      {children}
-    </legend>
-  )
-})
+const Legend = React.forwardRef<LegendRef, LegendProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <legend ref={ref} {...rest}>
+        {children}
+      </legend>
+    )
+  }
+)
 
 Legend.displayName = 'Legend'
 
