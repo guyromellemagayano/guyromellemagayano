7aa050c80cd020d12ff372e31a521fab752acd9e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:03:44 +0800

    refactor: :recycle: refactor props for `Body` React component

--- packages/react/components/src/lib/Body/Body.tsx
+++ packages/react/components/src/lib/Body/Body.tsx
@@ -4,20 +4,20 @@ export type BodyRef = HTMLBodyElement
 export type BodyProps = React.HTMLAttributes<BodyRef>
 
 /**
- * Render the body component
+ * Render the body component.
  * @param {BodyProps} props - The body component properties
  * @param {BodyRef} ref - The body component reference
  * @returns The rendered body component
  */
-const Body = React.forwardRef<BodyRef, BodyProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <body ref={ref} {...rest}>
-      {children}
-    </body>
-  )
-})
+const Body = React.forwardRef<BodyRef, BodyProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <body ref={ref} {...rest}>
+        {children}
+      </body>
+    )
+  }
+)
 
 Body.displayName = 'Body'
 
