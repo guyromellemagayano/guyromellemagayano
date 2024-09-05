c3750b8eed446188bbc3d90cf844f41c6d0ac701
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:05:45 +0800

    refactor: :recycle: refactor props for `TableFooter` React component

--- packages/react/components/src/lib/TableFooter/TableFooter.tsx
+++ packages/react/components/src/lib/TableFooter/TableFooter.tsx
@@ -4,15 +4,13 @@ export type TableFooterRef = HTMLTableSectionElement
 export type TableFooterProps = React.HTMLAttributes<TableFooterRef>
 
 /**
- * Render the table footer component
+ * Render the table footer component.
  * @param {TableFooterProps} props - The table footer component properties
  * @param {TableFooterRef} ref - The table footer component reference
  * @returns The rendered table footer component
  */
 const TableFooter = React.forwardRef<TableFooterRef, TableFooterProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <tfoot ref={ref} {...rest}>
         {children}
