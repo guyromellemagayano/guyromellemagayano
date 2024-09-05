34be55a1a8068174e9c2c16d262ebfbc729e6540
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:28:13 +0800

    refactor: :recycle: refactor props for `DefinitionTerm` React component

--- packages/react/components/src/lib/DefinitionTerm/DefinitionTerm.tsx
+++ packages/react/components/src/lib/DefinitionTerm/DefinitionTerm.tsx
@@ -4,15 +4,13 @@ export type DefinitionTermRef = HTMLElement
 export type DefinitionTermProps = React.HTMLAttributes<DefinitionTermRef>
 
 /**
- * Render the definition term component
+ * Render the definition term component.
  * @param {DefinitionTermProps} props - The definition term component properties
  * @param {DefinitionTermRef} ref - The definition term component reference
  * @returns The rendered definition term component
  */
 const DefinitionTerm = React.forwardRef<DefinitionTermRef, DefinitionTermProps>(
-  (props, ref) => {
-    const { children, ...rest } = props
-
+  ({ children, ...rest }, ref) => {
     return (
       <dt ref={ref} {...rest}>
         {children}
