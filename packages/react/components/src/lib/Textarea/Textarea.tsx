08feecd3b53f55951d17df906a4d27f4b5ef2811
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 15:07:07 +0800

    refactor: :recycle: refactor props for `Textarea` React component

--- packages/react/components/src/lib/Textarea/Textarea.tsx
+++ packages/react/components/src/lib/Textarea/Textarea.tsx
@@ -1,25 +1,23 @@
-'use client'
-
 import React from 'react'
 
 export type TextareaRef = HTMLTextAreaElement
 export type TextareaProps = React.TextareaHTMLAttributes<TextareaRef>
 
 /**
- * Render the textarea component
+ * Render the textarea component.
  * @param {TextareaProps} props - The textarea component properties
  * @param {TextareaRef} ref - The textarea component reference
  * @returns The rendered textarea component
  */
-const Textarea = React.forwardRef<TextareaRef, TextareaProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <textarea ref={ref} {...rest}>
-      {children}
-    </textarea>
-  )
-})
+const Textarea = React.forwardRef<TextareaRef, TextareaProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <textarea ref={ref} {...rest}>
+        {children}
+      </textarea>
+    )
+  }
+)
 
 Textarea.displayName = 'Textarea'
 
