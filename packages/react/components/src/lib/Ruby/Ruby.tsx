327b51309353a3e219274188a69140630025305b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 14:24:43 +0800

    refactor: :recycle: refactor props for `Ruby` React component

--- packages/react/components/src/lib/Ruby/Ruby.tsx
+++ packages/react/components/src/lib/Ruby/Ruby.tsx
@@ -4,20 +4,20 @@ export type RubyRef = HTMLElement
 export type RubyProps = React.HTMLAttributes<RubyRef>
 
 /**
- * Render the ruby annotation component
+ * Render the ruby annotation component.
  * @param {RubyProps} props - The ruby annotation component properties
  * @param {RubyRef} ref - The ruby annotation component reference
  * @returns The rendered ruby annotation component
  */
-const Ruby = React.forwardRef<RubyRef, RubyProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <ruby ref={ref} {...rest}>
-      {children}
-    </ruby>
-  )
-})
+const Ruby = React.forwardRef<RubyRef, RubyProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <ruby ref={ref} {...rest}>
+        {children}
+      </ruby>
+    )
+  }
+)
 
 Ruby.displayName = 'Ruby'
 
