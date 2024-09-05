8620793613b2bbae8ead35131695bbc2593b861a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:29:37 +0800

    refactor: :recycle: refactor props for `Division` React component

--- packages/react/components/src/lib/Division/Division.tsx
+++ packages/react/components/src/lib/Division/Division.tsx
@@ -4,20 +4,20 @@ export type DivisionRef = HTMLDivElement
 export type DivisionProps = React.HTMLAttributes<DivisionRef>
 
 /**
- * Render the division component
+ * Render the division component.
  * @param {DivisionProps} props - The division component properties
  * @param {DivisionRef} ref - The division component reference
  * @returns The rendered division component
  */
-const Division = React.forwardRef<DivisionRef, DivisionProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <div ref={ref} {...rest}>
-      {children}
-    </div>
-  )
-})
+const Division = React.forwardRef<DivisionRef, DivisionProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <div ref={ref} {...rest}>
+        {children}
+      </div>
+    )
+  }
+)
 
 Division.displayName = 'Division'
 
