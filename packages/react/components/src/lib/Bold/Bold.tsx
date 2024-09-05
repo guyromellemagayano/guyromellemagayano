154080ee417981e923bd0571da08f9b45b6bb535
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-05 13:04:02 +0800

    refactor: :recycle: refactor props for `Bold` React component

--- packages/react/components/src/lib/Bold/Bold.tsx
+++ packages/react/components/src/lib/Bold/Bold.tsx
@@ -4,20 +4,20 @@ export type BoldRef = HTMLElement
 export type BoldProps = React.HTMLAttributes<BoldRef>
 
 /**
- * Render the bold component
+ * Render the bold component.
  * @param {BoldProps} props - The bold component properties
  * @param {BoldRef} ref - The bold component reference
  * @returns The rendered bold component
  */
-const Bold = React.forwardRef<BoldRef, BoldProps>((props, ref) => {
-  const { children, ...rest } = props
-
-  return (
-    <b ref={ref} {...rest}>
-      {children}
-    </b>
-  )
-})
+const Bold = React.forwardRef<BoldRef, BoldProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <b ref={ref} {...rest}>
+        {children}
+      </b>
+    )
+  }
+)
 
 Bold.displayName = 'Bold'
 
