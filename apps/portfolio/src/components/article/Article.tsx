58ead890db8733bfe38bc50362c1f1f29586a3f6
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-06 13:15:43 +0800

    fix: :fire: remove old code for both app and components in `portfolio`

--- apps/portfolio/src/components/article/Article.tsx
+++ apps/portfolio/src/components/article/Article.tsx
@@ -2,17 +2,13 @@
 
 import { forwardRef } from 'react'
 
-import { formatDate } from '@guy-romelle-magayano/react-utils'
+import { formatDate } from '@react-utils'
 
-import {
-  Card,
-  type CardProps,
-  type CardRef
-} from '@guy-romelle-magayano/portfolio/components'
-import { type ArticlesData } from '@guy-romelle-magayano/portfolio/types'
+import { Card, type CardRef } from '@portfolio/components'
+import type { ArticlesData } from '@portfolio/types'
 
 export type ArticleRef = CardRef
-export type ArticleProps = CardProps & ArticlesData
+export type ArticleProps = ArticlesData
 
 const strings = {
   read: 'Read article'
@@ -20,39 +16,40 @@ const strings = {
 
 /**
  * Renders the article component.
- * @param {ArticleProps} props - The props of the article.
- * @param {ArticleRef} ref - The reference of the article.
- * @returns The rendered article component.
+ * @param {ArticleProps} props - The component props
+ * @param {ArticleRef} ref - The component reference
+ * @returns The rendered article component
  */
-const Article = forwardRef<ArticleRef, ArticleProps>((props, ref) => {
-  const { slug, title, date, description, ...rest } = props,
-    href = `/articles/${slug}`
+const Article = forwardRef<ArticleRef, ArticleProps>(
+  ({ slug, title, date, description, ...rest }, ref) => {
+    const href = `/articles/${slug}`
 
-  return (
-    <Card ref={ref} {...rest} as="article">
-      {date && date?.length > 0 && (
-        <Card.Eyebrow
-          as="time"
-          className="mb-2 text-zinc-400 dark:text-zinc-500"
-          dateTime={date}
-          decorate
-        >
-          {formatDate(date)}
-        </Card.Eyebrow>
-      )}
+    return (
+      <Card ref={ref} {...rest} as="article">
+        {date?.length > 0 && (
+          <Card.Eyebrow
+            as="time"
+            className="mb-2 text-zinc-400 dark:text-zinc-500"
+            dateTime={date}
+            decorate
+          >
+            {formatDate(date)}
+          </Card.Eyebrow>
+        )}
 
-      {title && title?.length > 0 && href && href?.length > 0 && (
-        <Card.Title href={href}>{title}</Card.Title>
-      )}
+        {title?.length > 0 && slug?.length > 0 && (
+          <Card.Title href={href}>{title}</Card.Title>
+        )}
 
-      {description && description?.length > 0 && (
-        <Card.Description>{description}</Card.Description>
-      )}
+        {description?.length > 0 && (
+          <Card.Description>{description}</Card.Description>
+        )}
 
-      {href && href?.length > 0 && <Card.Cta>{strings.read}</Card.Cta>}
-    </Card>
-  )
-})
+        {slug?.length > 0 && <Card.Cta>{strings.read}</Card.Cta>}
+      </Card>
+    )
+  }
+)
 
 Article.displayName = 'Article'
 
