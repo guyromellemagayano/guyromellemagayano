343a1d8821496b12ddbea6a344e2dfb7bb2cc4cb
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-06 13:38:45 +0800

    refactor: :recycle: update `Avatar` component

--- apps/portfolio/src/components/avatar/Avatar.tsx
+++ apps/portfolio/src/components/avatar/Avatar.tsx
@@ -5,12 +5,9 @@ import { forwardRef } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import {
-  type HyperlinkProps,
-  type HyperlinkRef
-} from '@guy-romelle-magayano/react-components/server'
+import { HyperlinkProps, HyperlinkRef } from '@react-components'
 
-import { cn } from '@guy-romelle-magayano/react-utils'
+import { cn } from '@react-utils'
 
 export type AvatarRef = HyperlinkRef
 export type AvatarProps = HyperlinkProps & {
@@ -18,41 +15,44 @@ export type AvatarProps = HyperlinkProps & {
   alt?: string
 }
 
-const link: { href: string; label: string } = {
-    href: '/',
-    label: 'Go to Home Page'
-  },
-  image: { file: string; width: number; height: number } = {
-    file: '/images/avatar.jpg',
-    width: 64,
-    height: 64
-  }
+const link = {
+  href: '/',
+  label: 'Go to Home Page'
+}
+const image = {
+  file: '/images/avatar.jpg',
+  width: 64,
+  height: 64
+}
 
 /**
  * Renders the avatar component.
- * @param {AvatarProps} props - The props of the avatar.
- * @param {AvatarRef} ref - The reference of the avatar.
- * @returns The rendered avatar component.
+ * @param {AvatarProps} props - The component props
+ * @param {AvatarRef} ref - The component reference
+ * @returns The rendered avatar component
  */
 const Avatar = forwardRef<AvatarRef, AvatarProps>(
   ({ large = false, className, alt = '', ...rest }, ref) => {
+    const sizeClass = large ? 'h-16 w-16' : 'h-9 w-9'
+    const imageSize = large ? '4rem' : '2.25rem'
+
     return (
       <Link
         ref={ref}
         href={link.href}
+        className={cn('pointer-events-auto', className)}
         aria-label={link.label}
-        className={cn(className, 'pointer-events-auto')}
         {...rest}
       >
         <Image
           src={image.file}
           alt={alt}
-          sizes={large ? '4rem' : '2.25rem'}
-          width={image.width}
           height={image.height}
+          sizes={imageSize}
+          width={image.width}
           className={cn(
             'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
-            large ? 'h-16 w-16' : 'h-9 w-9'
+            sizeClass
           )}
           priority
         />
