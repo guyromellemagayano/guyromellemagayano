8bea9594cfe549564c4a67b1d75cf99afef2ee8e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-08 22:39:46 +0800

    refactor: :recycle: update `HeaderLayout` component

--- apps/portfolio/src/components/layouts/header/HeaderLayout.tsx
+++ apps/portfolio/src/components/layouts/header/HeaderLayout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
 import { CSSProperties, forwardRef, useEffect, useRef } from 'react'
@@ -10,9 +11,9 @@ import {
   type DivisionRef,
   type HeaderProps,
   type HeaderRef
-} from '@guy-romelle-magayano/react-components/server'
+} from '@react-components'
 
-import { clamp, cn } from '@guy-romelle-magayano/react-utils'
+import { clamp, cn } from '@react-utils'
 
 import {
   Avatar,
@@ -21,8 +22,8 @@ import {
   DarkModeButton,
   DesktopNavigation,
   MobileNavigation
-} from '@guy-romelle-magayano/portfolio/components'
-import { PagesData } from '@guy-romelle-magayano/portfolio/types'
+} from '@portfolio/components'
+import { PagesData } from '@portfolio/types'
 
 export type HeaderLayoutRef = HeaderRef
 export type HeaderLayoutProps = HeaderProps & {
@@ -33,7 +34,7 @@ export type HeaderLayoutProps = HeaderProps & {
  * Render the header layout component.
  * @param {HeaderLayoutProps} props - The component props
  * @param {HeaderLayoutRef} ref - The component reference
- * @returns The rendered JSX component.
+ * @returns The rendered header layout component
  */
 const HeaderLayout = forwardRef<HeaderLayoutRef, HeaderLayoutProps>(
   ({ data, className, ...rest }, ref) => {
@@ -43,103 +44,93 @@ const HeaderLayout = forwardRef<HeaderLayoutRef, HeaderLayoutProps>(
     const isInitial = useRef<boolean>(true)
     const isHomePage = pathname === '/'
 
+    const setProperty = (property: string, value: string): void => {
+      document.documentElement.style.setProperty(property, value)
+    }
+
+    const removeProperty = (property: string): void => {
+      document.documentElement.style.removeProperty(property)
+    }
+
+    const updateHeaderStyles = (downDelay: number, upDelay: number) => {
+      const header = headerRef.current
+      if (!header) return
+
+      const { top, height } = header.getBoundingClientRect()
+      const scrollY = clamp({
+        number: window.scrollY,
+        min: 0,
+        max: document.body.scrollHeight - window.innerHeight
+      })
+
+      if (isInitial.current) setProperty('--header-position', 'sticky')
+
+      setProperty('--content-offset', `${downDelay}px`)
+
+      if (scrollY < downDelay) {
+        setProperty('--header-height', `${downDelay + height}px`)
+        setProperty('--header-mb', `${-downDelay}px`)
+      } else if (top + height < -upDelay) {
+        const offset = Math.max(height, scrollY - upDelay)
+        setProperty('--header-height', `${offset}px`)
+        setProperty('--header-mb', `${height - offset}px`)
+      } else if (top === 0) {
+        setProperty('--header-height', `${scrollY + height}px`)
+        setProperty('--header-mb', `${-scrollY}px`)
+      }
+
+      if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
+        setProperty('--header-inner-position', 'fixed')
+        removeProperty('--header-top')
+        removeProperty('--avatar-top')
+      } else {
+        removeProperty('--header-inner-position')
+        setProperty('--header-top', '0px')
+        setProperty('--avatar-top', '0px')
+      }
+    }
+
+    const updateAvatarStyles = (downDelay: number) => {
+      if (!isHomePage || !avatarRef.current) return
+
+      const fromScale = 1
+      const toScale = 36 / 64
+      const scrollY = downDelay - window.scrollY
+      let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale
+      scale = clamp({ number: scale, min: fromScale, max: toScale })
+
+      let x = (scrollY * (0 - 2 / 16)) / downDelay + 2 / 16
+      x = clamp({ number: x, min: 0, max: 2 / 16 })
+
+      setProperty(
+        '--avatar-image-transform',
+        `translate3d(${x}rem, 0, 0) scale(${scale})`
+      )
+      const borderScale = 1 / (toScale / scale)
+      const borderX = (-2 / 16 + x) * borderScale
+      setProperty(
+        '--avatar-border-transform',
+        `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`
+      )
+      setProperty('--avatar-border-opacity', scale === toScale ? '1' : '0')
+    }
+
     useEffect(() => {
       const downDelay = avatarRef.current?.offsetTop ?? 0
       const upDelay = 64
 
-      const setProperty = (property: string, value: string): void => {
-        document.documentElement.style.setProperty(property, value)
-      }
-      const removeProperty = (property: string): void => {
-        document.documentElement.style.removeProperty(property)
-      }
-
-      const updateHeaderStyles = (): void => {
-        if (!headerRef.current) return
-
-        const { top, height } = headerRef.current.getBoundingClientRect()
-        const scrollY = clamp({
-          number: window.scrollY,
-          min: 0,
-          max: document.body.scrollHeight - window.innerHeight
-        })
-
-        if (isInitial.current) {
-          setProperty('--header-position', 'sticky')
-        }
-
-        setProperty('--content-offset', `${downDelay}px`)
-
-        if (isInitial.current || scrollY < downDelay) {
-          setProperty('--header-height', `${downDelay + height}px`)
-          setProperty('--header-mb', `${-downDelay}px`)
-        } else if (top + height < -upDelay) {
-          const offset = Math.max(height, scrollY - upDelay)
-          setProperty('--header-height', `${offset}px`)
-          setProperty('--header-mb', `${height - offset}px`)
-        } else if (top === 0) {
-          setProperty('--header-height', `${scrollY + height}px`)
-          setProperty('--header-mb', `${-scrollY}px`)
-        }
-
-        if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
-          setProperty('--header-inner-position', 'fixed')
-          removeProperty('--header-top')
-          removeProperty('--avatar-top')
-        } else {
-          removeProperty('--header-inner-position')
-          setProperty('--header-top', '0px')
-          setProperty('--avatar-top', '0px')
-        }
-      }
-
-      const updateAvatarStyles = (): void => {
-        if (!isHomePage) return
-
-        const fromScale = 1
-        const toScale = 36 / 64
-        const fromX = 0
-        const toX = 2 / 16
-
-        const scrollY = downDelay - window.scrollY
-
-        let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale
-        scale = clamp({ number: scale, min: fromScale, max: toScale })
-
-        let x = (scrollY * (fromX - toX)) / downDelay + toX
-        x = clamp({ number: x, min: fromX, max: toX })
-
-        setProperty(
-          '--avatar-image-transform',
-          `translate3d(${x}rem, 0, 0) scale(${scale})`
-        )
-
-        const borderScale = 1 / (toScale / scale)
-        const borderX = (-toX + x) * borderScale
-        const borderTransform = `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`
-
-        setProperty('--avatar-border-transform', borderTransform)
-        setProperty('--avatar-border-opacity', scale === toScale ? '1' : '0')
-      }
-
-      const updateStyles = (): void => {
-        updateHeaderStyles()
-        updateAvatarStyles()
+      const handleScroll = () => {
+        updateHeaderStyles(downDelay, upDelay)
+        updateAvatarStyles(downDelay)
         isInitial.current = false
       }
 
-      const handleScroll = () => {
-        requestAnimationFrame(updateStyles)
-      }
-
-      updateStyles()
-
       window.addEventListener('scroll', handleScroll, { passive: true })
-      window.addEventListener('resize', updateStyles)
+      window.addEventListener('resize', handleScroll)
 
       return () => {
         window.removeEventListener('scroll', handleScroll)
-        window.removeEventListener('resize', updateStyles)
+        window.removeEventListener('resize', handleScroll)
       }
     }, [isHomePage])
 
@@ -153,11 +144,11 @@ const HeaderLayout = forwardRef<HeaderLayoutRef, HeaderLayoutProps>(
     return (
       <>
         <Header
-          ref={ref}
           className={cn(
             'pointer-events-none relative z-50 flex flex-none flex-col',
             className
           )}
+          ref={ref}
           style={{
             height: 'var(--header-height)',
             marginBottom: 'var(--header-mb)'
