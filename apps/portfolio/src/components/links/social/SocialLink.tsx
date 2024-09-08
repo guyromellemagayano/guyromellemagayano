fb3c8c0d8ee2a399b70c3d55a4ae09479f268ee3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-08 23:47:07 +0800

    refactor: :recycle: update `SocialLink` component

--- apps/portfolio/src/components/links/social/SocialLink.tsx
+++ apps/portfolio/src/components/links/social/SocialLink.tsx
@@ -1,14 +1,14 @@
-import { FC, forwardRef } from 'react'
+import { FC, forwardRef, useMemo } from 'react'
 
-import { SvgProps } from '@guy-romelle-magayano/react-components'
 import {
   A,
   Span,
+  SvgProps,
   type HyperlinkProps,
   type HyperlinkRef
-} from '@guy-romelle-magayano/react-components/server'
+} from '@react-components'
 
-import { cn, convertStringToLowercase } from '@guy-romelle-magayano/react-utils'
+import { cn, convertStringToLowercase } from '@react-utils'
 
 import {
   BehanceSvg,
@@ -21,8 +21,8 @@ import {
   MastodonSvg,
   MediumSvg,
   TwitterSvg
-} from '@guy-romelle-magayano/portfolio/components'
-import { type SocialLinksData } from '@guy-romelle-magayano/portfolio/types'
+} from '@portfolio/components'
+import type { SocialLinksData } from '@portfolio/types'
 
 export type SocialLinkRef = HyperlinkRef
 export type SocialLinkProps = HyperlinkProps &
@@ -34,45 +34,49 @@ export type SocialLinkProps = HyperlinkProps &
  * Renders the social link component.
  * @param {SocialLinkProps} props - The component props
  * @param {SocialLinkRef} ref - The component reference
- * @returns The rendered JSX component
+ * @returns The rendered social link component
  */
 const SocialLink = forwardRef<SocialLinkRef, SocialLinkProps>(
   ({ icon, href, label, showLabel = false, className, ...rest }, ref) => {
     // Define the Icon component based on the icon prop.
-    const iconMap: Record<string, FC<SvgProps>> = {
-      facebook: FacebookSvg,
-      instagram: InstagramSvg,
-      twitter: TwitterSvg,
-      github: GithubSvg,
-      linkedin: LinkedInSvg,
-      mail: MailSvg,
-      medium: MediumSvg,
-      behance: BehanceSvg,
-      discord: DiscordSvg,
-      mastodon: MastodonSvg
-    }
+    const iconMap: Record<string, FC<SvgProps>> = useMemo(
+      () => ({
+        facebook: FacebookSvg,
+        instagram: InstagramSvg,
+        twitter: TwitterSvg,
+        github: GithubSvg,
+        linkedin: LinkedInSvg,
+        mail: MailSvg,
+        medium: MediumSvg,
+        behance: BehanceSvg,
+        discord: DiscordSvg,
+        mastodon: MastodonSvg
+      }),
+      []
+    )
 
-    const Icon = icon ? iconMap[convertStringToLowercase(icon)] : undefined
+    const Icon = useMemo(() => {
+      return icon ? iconMap[convertStringToLowercase(icon)] : undefined
+    }, [icon, iconMap])
 
     if (!href || !Icon) return null
 
     return (
       <A
         ref={ref}
+        href={href}
+        target="_blank"
+        rel="noreferrer"
         className={cn(
           className,
           'group -m-1 p-1',
           showLabel &&
             'flex text-sm font-medium text-zinc-800 transition hover:text-zinc-500 dark:text-zinc-200 dark:hover:text-zinc-500'
         )}
-        href={href}
         aria-label={label}
-        target="_blank"
-        rel="noreferrer"
         {...rest}
       >
         <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
-
         {showLabel && label && <Span className="ml-4">{label}</Span>}
       </A>
     )
