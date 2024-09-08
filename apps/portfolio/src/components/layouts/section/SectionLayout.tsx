f451fe78905c64869f09c88239b4218ab0e9675e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-08 23:25:58 +0800

    fix: :bug: fix faulty logic of `SectionLayout` component

--- apps/portfolio/src/components/layouts/section/SectionLayout.tsx
+++ apps/portfolio/src/components/layouts/section/SectionLayout.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react'
+import { forwardRef, memo } from 'react'
 
 import {
   Div,
@@ -6,9 +6,9 @@ import {
   Section,
   SectionProps,
   SectionRef
-} from '@guy-romelle-magayano/react-components/server'
+} from '@react-components'
 
-import { cn } from '@guy-romelle-magayano/react-utils'
+import { cn } from '@react-utils'
 
 export type SectionLayoutRef = SectionRef
 export type SectionLayoutProps = SectionProps & {
@@ -18,41 +18,45 @@ export type SectionLayoutProps = SectionProps & {
 
 /**
  * Renders the section layout component.
- * @param {SectionLayoutProps} props - The props of the section layout.
- * @param {SectionLayoutRef} ref - The reference of the section layout.
- * @returns The rendered section layout component.
+ * @param {SectionLayoutProps} props - The component props
+ * @param {SectionLayoutRef} ref - The component reference
+ * @returns The rendered section layout component
  */
-const SectionLayout = forwardRef<SectionLayoutRef, SectionLayoutProps>(
-  ({ title, decorate, className, children, ...rest }, ref) => {
-    return (
-      <Section
-        {...rest}
-        ref={ref}
-        className={cn(
-          decorate &&
-            'md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40',
-          className
-        )}
-      >
-        <Div className="md:grid md:grid-cols-4 md:items-baseline">
-          {title && title?.length > 0 && (
-            <Heading
-              as="h2"
-              className="text-sm font-semibold text-zinc-400 dark:text-zinc-500"
-            >
-              {title}
-            </Heading>
-          )}
+const SectionLayout = memo(
+  forwardRef<SectionLayoutRef, SectionLayoutProps>(
+    ({ title, decorate, className, children, ...rest }, ref) => {
+      if (!title && !children) return null
 
-          {children && (
-            <Div className="group relative flex flex-col items-start md:col-span-3">
-              {children}
-            </Div>
+      return (
+        <Section
+          ref={ref}
+          className={cn(
+            decorate &&
+              'md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40',
+            className
           )}
-        </Div>
-      </Section>
-    )
-  }
+          {...rest}
+        >
+          <Div className="md:grid md:grid-cols-4 md:items-baseline">
+            {title && (
+              <Heading
+                as="h2"
+                className="text-sm font-semibold text-zinc-400 dark:text-zinc-500"
+              >
+                {title}
+              </Heading>
+            )}
+
+            {children && (
+              <Div className="group relative flex flex-col items-start md:col-span-3">
+                {children}
+              </Div>
+            )}
+          </Div>
+        </Section>
+      )
+    }
+  )
 )
 
 SectionLayout.displayName = 'SectionLayout'
