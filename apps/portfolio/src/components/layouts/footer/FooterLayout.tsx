ce5fad4f3d01a058faba8b1432a6e4e0ef183d65
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-09-08 22:18:58 +0800

    refactor: :recycle: udpate `FooterLayout` component

--- apps/portfolio/src/components/layouts/footer/FooterLayout.tsx
+++ apps/portfolio/src/components/layouts/footer/FooterLayout.tsx
@@ -8,15 +8,12 @@ import {
   P,
   type FooterProps,
   type FooterRef
-} from '@guy-romelle-magayano/react-components/server'
+} from '@react-components'
 
-import { cn, isValidData } from '@guy-romelle-magayano/react-utils'
+import { cn } from '@react-utils'
 
-import {
-  BaseContainer,
-  NavigationLink
-} from '@guy-romelle-magayano/portfolio/components'
-import { type PagesData } from '@guy-romelle-magayano/portfolio/types'
+import { BaseContainer, NavigationLink } from '@portfolio/components'
+import { type PagesData } from '@portfolio/types'
 
 export type FooterLayoutRef = FooterRef
 export type FooterLayoutProps = FooterProps & {
@@ -27,18 +24,13 @@ export type FooterLayoutProps = FooterProps & {
  * Render the footer layout component.
  * @param {FooterLayoutProps} props - The component props
  * @param {FooterLayoutRef} ref - The component reference
- * @returns The rendered JSX element
+ * @returns The rendered footer layout component
  */
 const FooterLayout = memo(
   forwardRef<FooterLayoutRef, FooterLayoutProps>(
     ({ data, className, ...rest }, ref) => {
       const yearNow = new Date().getFullYear()
       const copyrightText = `${yearNow} Guy Romelle Magayano`
-
-      const validData =
-        data?.filter((item): item is PagesData =>
-          isValidData(item, 'object')
-        ) || null
 
       return (
         <Footer ref={ref} className={cn('mt-32', className)} {...rest}>
@@ -47,12 +39,11 @@ const FooterLayout = memo(
               <BaseContainer.Inner className="lg:px-4">
                 <Div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
                   <Div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                    {validData &&
-                      data.map(({ id, title, link, ...rest }) => (
-                        <NavigationLink key={id} href={link} {...rest}>
-                          {title}
-                        </NavigationLink>
-                      ))}
+                    {data?.map(({ id, title, link, ...rest }) => (
+                      <NavigationLink key={id} href={link} {...rest}>
+                        {title}
+                      </NavigationLink>
+                    ))}
                   </Div>
                   <P className="text-sm text-zinc-600 dark:text-zinc-300">
                     &copy; {copyrightText}
