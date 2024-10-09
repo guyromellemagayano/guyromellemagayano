8225dbe997a0474492e70160dfae559fed6c1345
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-10-09 08:52:00 +0800

    fix: :pencil2: fix class name typo in `DesktopNavigation` component

--- apps/portfolio/src/components/navigation/desktop/DesktopNavigation.tsx
+++ apps/portfolio/src/components/navigation/desktop/DesktopNavigation.tsx
@@ -27,7 +27,7 @@ const DesktopNavigation = memo(
 
       return (
         <Nav ref={ref} {...rest}>
-          <NavigationList className="dark:ring-white/10' flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+          <NavigationList className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
             {data.map(({ id, link, title }) => {
               return (
                 <NavigationListItem key={id} href={link}>
