49e97fc64b14fc8ef715d46afd6917d5aa3721ef
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-10-16 21:01:13 +0800

    fix: :sparkles: update `ContentLayout` component features, optimize performance

--- apps/portfolio/src/components/layouts/content/ContentLayout.tsx
+++ apps/portfolio/src/components/layouts/content/ContentLayout.tsx
@@ -8,75 +8,89 @@ import {
   memo
 } from 'react'
 
+import dynamic from 'next/dynamic'
 import { usePathname } from 'next/navigation'
 
 import {
-  Aside,
   Div,
-  type DivisionProps,
-  type DivisionRef,
   Heading,
   P,
-  Span
+  Span,
+  type TDivisionProps,
+  type TDivisionRef
 } from '@react-components'
 
 import { cn } from '@react-utils'
 
-import { BaseContainer } from '@portfolio/components'
+// Dynamic imports
+const BaseContainer = dynamic(() =>
+  import('@portfolio/components').then(mod => mod.BaseContainer)
+)
 
-export type ContentLayoutRef = DivisionRef
-export type ContentLayoutProps = DivisionProps & {
+export type TContentLayoutRef = TDivisionRef
+export type TContentLayoutProps = TDivisionProps & {
   as?: ElementType
-  title?: string
-  intro?: string[] | string
+  heading?: string | null
+  description?: (string | null)[] | null
 }
-export type ContentLayoutStaticComponents = {
+export type TContentLayoutStaticComponents = {
   Simple: typeof ContentSimpleLayout
   Aside: typeof ContentAsideLayout
 }
 
 /**
  * Render the content aside layout component.
- * @param {ContentLayoutProps} props - The component props
- * @param {ContentLayoutRef} ref - The component reference
+ * @param {TContentLayoutProps} props - The component props
+ * @param {TContentLayoutRef} ref - The component reference
  * @returns The rendered content aside layout component
  */
-const ContentAsideLayout = memo(
-  forwardRef<ContentLayoutRef, ContentLayoutProps>(
-    ({ title, intro, children, className, ...rest }, ref) => {
-      if (!title && !intro && !children) return null
+export const ContentAsideLayout = memo(
+  forwardRef<TContentLayoutRef, TContentLayoutProps>(
+    ({ heading, description, children, className, ...rest }, ref) => {
+      if (!heading && !description) return null
 
       return (
-        <Aside ref={ref}>
-          {title && (
-            <Heading
-              as="h1"
-              className={cn(
-                'text-4xl font-bold tracking-tighter text-zinc-800 dark:text-zinc-100'
-              )}
-            >
-              {title}
-            </Heading>
-          )}
-          {intro && (
-            <P
-              className={cn('mt-6 text-base text-zinc-600 dark:text-zinc-400')}
-            >
-              {Array.isArray(intro)
-                ? intro.map((paragraph, index) => (
-                    <Span key={index} className={cn('space-y-7')}>
-                      {paragraph}
-                    </Span>
-                  ))
-                : intro}
-            </P>
-          )}
-          {children && (
-            <Div className={cn('mt-16 sm:mt-20', className)} {...rest}>
-              {children}
+        <BaseContainer
+          ref={ref}
+          className={cn('mt-16 sm:mt-32', className)}
+          {...rest}
+        >
+          <Div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+            <Div className="lg:pl-20">
+              <Div className="max-w-xs px-2.5 lg:max-w-none">
+                <Div className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800" />
+              </Div>
             </Div>
-          )}
-        </Aside>
+            <Div className="lg:order-first lg:row-span-2">
+              {heading && (
+                <Heading
+                  as="h1"
+                  className="mb-8 text-3xl font-bold tracking-tighter text-zinc-800 sm:text-5xl dark:text-zinc-100"
+                >
+                  {heading}
+                </Heading>
+              )}
+
+              {description && (
+                <Div className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                  <P className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                    {Array.isArray(description)
+                      ? description?.map((paragraph, index) => (
+                          <Span key={index}>{paragraph}</Span>
+                        ))
+                      : description}
+                  </P>
+                </Div>
+              )}
+
+              {children && (
+                <Div className={cn('mt-9 sm:mt-9', className)} {...rest}>
+                  {children}
+                </Div>
+              )}
+            </Div>
+          </Div>
+        </BaseContainer>
       )
     }
   )
@@ -86,16 +100,16 @@ ContentAsideLayout.displayName = 'ContentAsideLayout'
 
 /**
  * Render the content simple layout component.
- * @param {ContentLayoutProps} props - The component props
- * @param {ContentLayoutRef} ref - The component reference
+ * @param {TContentLayoutProps} props - The component props
+ * @param {TContentLayoutRef} ref - The component reference
  * @returns The rendered content simple layout component
  */
-const ContentSimpleLayout = memo(
-  forwardRef<ContentLayoutRef, ContentLayoutProps>(
-    ({ title, intro, children, className, ...rest }, ref) => {
+export const ContentSimpleLayout = memo(
+  forwardRef<TContentLayoutRef, TContentLayoutProps>(
+    ({ heading, description, children, className, ...rest }, ref) => {
       const pathname = usePathname()
 
-      if (!title && !intro && !children) return null
+      if (!heading && !description) return null
 
       return (
         <BaseContainer
@@ -103,31 +117,23 @@ const ContentSimpleLayout = memo(
           className={cn('mt-16 sm:mt-32', className)}
           {...rest}
         >
-          <Div className={cn('w-full max-w-3xl')}>
-            {title && (
+          <Div className="w-full max-w-3xl">
+            {heading && (
               <Heading
                 as="h1"
-                className={cn(
-                  'text-4xl font-bold tracking-tighter text-zinc-800 sm:text-5xl dark:text-zinc-100'
-                )}
+                className="mb-8 text-3xl font-bold tracking-tighter text-zinc-800 sm:text-5xl dark:text-zinc-100"
               >
-                {title}
+                {heading}
               </Heading>
             )}
 
-            {intro && (
-              <P
-                className={cn(
-                  'mt-6 flex flex-col space-y-7 text-base text-zinc-600 dark:text-zinc-400'
-                )}
-              >
-                {Array.isArray(intro)
-                  ? intro.map((paragraph, index) => (
-                      <Span key={index} className={cn('space-y-7')}>
-                        {paragraph}
-                      </Span>
+            {description && (
+              <P className="mt-6 flex flex-col space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+                {Array.isArray(description)
+                  ? description?.map((paragraph, index) => (
+                      <Span key={index}>{paragraph}</Span>
                     ))
-                  : intro}
+                  : description}
               </P>
             )}
           </Div>
@@ -151,11 +157,11 @@ ContentSimpleLayout.displayName = 'ContentSimpleLayout'
 
 /**
  * Render the content layout component.
- * @param {ContentLayoutProps} props - The component props
- * @param {ContentLayoutRef} ref - The component reference
+ * @param {TContentLayoutProps} props - The component props
+ * @param {TContentLayoutRef} ref - The component reference
  * @returns The rendered content layout component
  */
-const ContentLayout = forwardRef<ContentLayoutRef, ContentLayoutProps>(
+const ContentLayout = forwardRef<TContentLayoutRef, TContentLayoutProps>(
   ({ as: Component = Div, children, ...rest }, ref) => {
     if (!children) {
       return null
@@ -168,9 +174,9 @@ const ContentLayout = forwardRef<ContentLayoutRef, ContentLayoutProps>(
     )
   }
 ) as ForwardRefExoticComponent<
-  ContentLayoutProps & RefAttributes<ContentLayoutRef>
+  TContentLayoutProps & RefAttributes<TContentLayoutRef>
 > &
-  ContentLayoutStaticComponents
+  TContentLayoutStaticComponents
 
 ContentLayout.displayName = 'ContentLayout'
 
