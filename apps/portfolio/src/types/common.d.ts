e1d9df0aabe09807011409316e6f8c741b3f58b2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-10-17 03:03:06 +0800

    fix: :fire: update `portfolio` types

--- apps/portfolio/src/types/common.d.ts
+++ apps/portfolio/src/types/common.d.ts
@@ -1,18 +1,5 @@
-import { MDXContent } from 'mdx/types'
-import { StaticImport } from 'next/dist/shared/lib/get-img-props'
-
-// Common posts data
-export type TCommonPostsData = {
-  slug: string
-  title: string
-  description: string
-  category: string | string[]
-  component?: MDXContent
-}
-
-// Common photos data
-export type TCommonPhotosData = {
-  id?: string
-  alt?: string
-  src: string | StaticImport
+export type TCommonProps = {
+  common: {
+    [key: string]: string
+  }
 }
