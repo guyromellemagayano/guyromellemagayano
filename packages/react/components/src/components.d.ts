e73afc3b91633656be4431cbe252562351e732f8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-10-17 03:03:52 +0800

    fix: :label: update custom React components `TCommonComponentsProps` types

--- packages/react/components/src/components.d.ts
+++ packages/react/components/src/components.d.ts
@@ -3,8 +3,11 @@ import { ReactNode } from 'react'
 // ====================================================
 // Common Component Prop Types
 // ====================================================
-export type TCommonComponentsProps = {
+export type TCommonComponentsProps = TCommonComponentExtensionProps & {
   children?: ReactNode
   className?: string
+}
+
+export type TCommonComponentExtensionProps = {
   [key: string]: any
 }
