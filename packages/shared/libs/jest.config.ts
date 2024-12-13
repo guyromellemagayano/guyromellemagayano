af93ca4790ed93665c8468c9512f03993387352d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-12-13 15:46:20 +0800

    fix: :truck: update naming of shared components, update deps

--- packages/shared/libs/jest.config.ts
+++ packages/shared/libs/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'react-libs',
+  displayName: 'shared-libs',
   preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest'
