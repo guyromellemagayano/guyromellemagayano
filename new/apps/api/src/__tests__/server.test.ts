71fa5e7385a4af6671e621f8e26eaad4aca0fc05
Turbobot <turbobot@vercel.com>
2025-02-11 14:58:28 +0800

    feat(create-turbo): apply official-starter transform

---
+++ new/apps/api/src/__tests__/server.test.ts
@@ -0,0 +1,23 @@
+import supertest from "supertest";
+import { describe, it, expect } from "@jest/globals";
+import { createServer } from "../server";
+
+describe("Server", () => {
+  it("health check returns 200", async () => {
+    await supertest(createServer())
+      .get("/status")
+      .expect(200)
+      .then((res) => {
+        expect(res.ok).toBe(true);
+      });
+  });
+
+  it("message endpoint says hello", async () => {
+    await supertest(createServer())
+      .get("/message/jared")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual({ message: "hello jared" });
+      });
+  });
+});
