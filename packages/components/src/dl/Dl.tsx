76665587afa1dd61e12f40661a544c5314eb6cc1
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-02-26 13:45:53 +0800

    feat: :sparkles: description list component addition

---
+++ packages/components/src/dl/Dl.tsx
@@ -0,0 +1,33 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const DlClient = lazy(async () => {
+  const module = await import("./Dl.client");
+  return { default: module.DlClient };
+});
+
+export type DlRef = HTMLDListElement;
+export type DlProps = HTMLAttributes<DlRef> & CommonComponentProps;
+
+/**
+ * Render the default description list server component.
+ * @param {DlProps} props - The default description list server component properties
+ * @returns The rendered default description list server component
+ */
+export const Dl = ({ isClient = false, children, ...rest }: DlProps) => {
+  const element = <dl {...rest}>{children}</dl>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <DlClient {...rest}>{children}</DlClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Dl.displayName = "Dl";
