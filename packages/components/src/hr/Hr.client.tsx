d2f837c585d95011308bb6f8ad3d56ff4c37b10e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-02-26 14:48:46 +0800

    feat: :sparkles: thematic break (horizontal rule) component addition

---
+++ packages/components/src/hr/Hr.client.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { forwardRef } from "react";
+
+import type { HrProps, HrRef } from "./Hr";
+
+/**
+ * Render the thematic break (horizontal rule) client component.
+ * @param {HrProps} props - The thematic break (horizontal rule) client component properties
+ * @param {HrRef} ref - The thematic break (horizontal rule) client component reference
+ * @returns The rendered thematic break (horizontal rule) client component
+ */
+export const HrClient = forwardRef<HrRef, HrProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <hr ref={ref} {...rest}>
+        {children}
+      </hr>
+    );
+  }
+);
+
+HrClient.displayName = "HrClient";
