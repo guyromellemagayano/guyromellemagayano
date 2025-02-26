8d7010cff5bd99573ba698a5f7c4f5d0ce1f0dde
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-02-26 14:47:59 +0800

    feat: :sparkles: heading group component addition

---
+++ packages/components/src/hgroup/Hgroup.tsx
@@ -0,0 +1,37 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const HgroupClient = lazy(async () => {
+  const module = await import("./Hgroup.client");
+  return { default: module.HgroupClient };
+});
+
+export type HgroupRef = HTMLElement;
+export type HgroupProps = HTMLAttributes<HgroupRef> & CommonComponentProps;
+
+/**
+ * Render the default heading group component.
+ * @param {HgroupProps} props - The default heading group component properties
+ * @returns The rendered default heading group component
+ */
+export const Hgroup = ({
+  isClient = false,
+  children,
+  ...rest
+}: HgroupProps) => {
+  const element = <hgroup {...rest}>{children}</hgroup>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <HgroupClient {...rest}>{children}</HgroupClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Hgroup.displayName = "Hgroup";
