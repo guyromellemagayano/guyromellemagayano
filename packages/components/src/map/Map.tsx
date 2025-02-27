c091601d5ea46a1792d347e97f515779fec6c72e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-02-27 14:17:57 +0800

    feat: :sparkles: image map component addition

---
+++ packages/components/src/map/Map.tsx
@@ -0,0 +1,33 @@
+import { type MapHTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const MapClient = lazy(async () => {
+  const module = await import("./Map.client");
+  return { default: module.MapClient };
+});
+
+export type MapRef = HTMLMapElement;
+export type MapProps = MapHTMLAttributes<MapRef> & CommonComponentProps;
+
+/**
+ * Render the default image map server component.
+ * @param {MapProps} props - The default image map server component properties
+ * @returns The rendered default image map server component
+ */
+export const Map = ({ isClient = false, children, ...rest }: MapProps) => {
+  const element = <map {...rest}>{children}</map>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <MapClient {...rest}>{children}</MapClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Map.displayName = "Map";
