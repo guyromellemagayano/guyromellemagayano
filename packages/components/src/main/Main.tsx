277db456a770dc629a015120c0f3c7fb1bdfad1b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-02-27 14:13:34 +0800

    feat: :sparkles: main component addition

---
+++ packages/components/src/main/Main.tsx
@@ -0,0 +1,33 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const MainClient = lazy(async () => {
+  const module = await import("./Main.client");
+  return { default: module.MainClient };
+});
+
+export type MainRef = HTMLElement;
+export type MainProps = HTMLAttributes<MainRef> & CommonComponentProps;
+
+/**
+ * Render the default main server component.
+ * @param {MainProps} props - The default main server component properties
+ * @returns The rendered default main server component
+ */
+export const Main = ({ isClient = false, children, ...rest }: MainProps) => {
+  const element = <main {...rest}>{children}</main>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <MainClient {...rest}>{children}</MainClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Main.displayName = "Main";
