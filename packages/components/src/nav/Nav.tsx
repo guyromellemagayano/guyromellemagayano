a8b48a3f9cdf92ddf62cff8ccedd2f7270c2f03c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-03 12:19:01 +0800

    feat: :sparkles: navigation section component addition

---
+++ packages/components/src/nav/Nav.tsx
@@ -0,0 +1,33 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const NavClient = lazy(async () => {
+  const module = await import("./Nav.client");
+  return { default: module.NavClient };
+});
+
+export type NavRef = HTMLElement;
+export type NavProps = HTMLAttributes<NavRef> & CommonComponentProps;
+
+/**
+ * Render the default navigation section server component.
+ * @param {NavProps} props - The default navigation section server component properties
+ * @returns The rendered default navigation section server component
+ */
+export const Nav = ({ isClient = false, children, ...rest }: NavProps) => {
+  const element = <nav {...rest}>{children}</nav>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <NavClient {...rest}>{children}</NavClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Nav.displayName = "Nav";
