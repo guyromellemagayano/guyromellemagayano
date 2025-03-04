232700b40cab067430c360f20f102f6105008fa0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-04 15:03:13 +0800

    feat: :sparkles: sample output component addition

---
+++ packages/components/src/samp/Samp.tsx
@@ -0,0 +1,33 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const SampClient = lazy(async () => {
+  const module = await import("./Samp.client");
+  return { default: module.SampClient };
+});
+
+export type SampRef = HTMLElement;
+export type SampProps = HTMLAttributes<SampRef> & CommonComponentProps;
+
+/**
+ * Render the default sample output server component.
+ * @param {SampProps} props - The default sample output server component properties
+ * @returns The rendered default sample output server component
+ */
+export const Samp = ({ isClient = false, children, ...rest }: SampProps) => {
+  const element = <samp {...rest}>{children}</samp>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <SampClient {...rest}>{children}</SampClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Samp.displayName = "Samp";
