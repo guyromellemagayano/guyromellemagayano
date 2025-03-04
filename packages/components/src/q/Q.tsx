e6a28e843be61aa651e6edd6b1e30decb4fc8f32
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-04 15:00:55 +0800

    feat: :sparkles: inline quotation component addition

---
+++ packages/components/src/q/Q.tsx
@@ -0,0 +1,33 @@
+import { type QuoteHTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const QClient = lazy(async () => {
+  const module = await import("./Q.client");
+  return { default: module.QClient };
+});
+
+export type QRef = HTMLQuoteElement;
+export type QProps = QuoteHTMLAttributes<QRef> & CommonComponentProps;
+
+/**
+ * Render the default inline quotation server component.
+ * @param {QProps} props - The default inline quotation server component properties
+ * @returns The rendered default inline quotation server component
+ */
+export const Q = ({ isClient = false, children, ...rest }: QProps) => {
+  const element = <q {...rest}>{children}</q>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <QClient {...rest}>{children}</QClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Q.displayName = "Q";
