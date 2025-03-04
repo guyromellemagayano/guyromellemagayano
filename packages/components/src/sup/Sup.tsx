393eb961b0f2fe9b00300c906d78b2188d5ecf81
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-04 15:07:58 +0800

    feat: :sparkles: superscript component addition

---
+++ packages/components/src/sup/Sup.tsx
@@ -0,0 +1,37 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const SummaryClient = lazy(async () => {
+  const module = await import("./Sup.client");
+  return { default: module.SummaryClient };
+});
+
+export type SummaryRef = HTMLElement;
+export type SummaryProps = HTMLAttributes<SummaryRef> & CommonComponentProps;
+
+/**
+ * Render the default superscript server component.
+ * @param {SummaryProps} props - The default superscript server component properties
+ * @returns The rendered default superscript server component
+ */
+export const Summary = ({
+  isClient = false,
+  children,
+  ...rest
+}: SummaryProps) => {
+  const element = <sup {...rest}>{children}</sup>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <SummaryClient {...rest}>{children}</SummaryClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Summary.displayName = "Summary";
