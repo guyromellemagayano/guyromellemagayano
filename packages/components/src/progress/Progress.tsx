67c044f4018403372ddb60c6c6e8ab8192bd4373
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-04 15:00:36 +0800

    feat: :sparkles: progress indicator component addition

---
+++ packages/components/src/progress/Progress.tsx
@@ -0,0 +1,38 @@
+import { type ProgressHTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const ProgressClient = lazy(async () => {
+  const module = await import("./Progress.client");
+  return { default: module.ProgressClient };
+});
+
+export type ProgressRef = HTMLProgressElement;
+export type ProgressProps = ProgressHTMLAttributes<ProgressRef> &
+  CommonComponentProps;
+
+/**
+ * Render the default progress indicator server component.
+ * @param {ProgressProps} props - The default progress indicator server component properties
+ * @returns The rendered default progress indicator server component
+ */
+export const Progress = ({
+  isClient = false,
+  children,
+  ...rest
+}: ProgressProps) => {
+  const element = <progress {...rest}>{children}</progress>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <ProgressClient {...rest}>{children}</ProgressClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Progress.displayName = "Progress";
