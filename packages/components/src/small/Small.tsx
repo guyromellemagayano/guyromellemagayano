4608b6ce847aff4baea1a98513c0340702690bfc
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-04 15:05:46 +0800

    feat: :sparkles: side comment component addition

---
+++ packages/components/src/small/Small.tsx
@@ -0,0 +1,33 @@
+import { type HTMLAttributes, lazy, Suspense } from "react";
+
+import type { CommonComponentProps } from "../components";
+
+// Dynamically import the client component
+const SmallClient = lazy(async () => {
+  const module = await import("./Small.client");
+  return { default: module.SmallClient };
+});
+
+export type SmallRef = HTMLElement;
+export type SmallProps = HTMLAttributes<SmallRef> & CommonComponentProps;
+
+/**
+ * Render the default side comment server component.
+ * @param {SmallProps} props - The default side comment server component properties
+ * @returns The rendered default side comment server component
+ */
+export const Small = ({ isClient = false, children, ...rest }: SmallProps) => {
+  const element = <small {...rest}>{children}</small>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <SmallClient {...rest}>{children}</SmallClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Small.displayName = "Small";
