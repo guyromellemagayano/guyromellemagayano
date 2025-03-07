cc8825ab952188270b0abc870926b2e0dddc489a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-07 16:28:28 +0800

    refactor: :recycle: fix main component type exports

--- packages/components/src/main/Main.tsx
+++ packages/components/src/main/Main.tsx
@@ -1,4 +1,4 @@
-import { type HTMLAttributes, lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 
 import type { CommonComponentProps } from "../components";
 
@@ -8,8 +8,9 @@ const MainClient = lazy(async () => {
   return { default: module.MainClient };
 });
 
-export type MainRef = HTMLElement;
-export type MainProps = HTMLAttributes<MainRef> & CommonComponentProps;
+export type MainRef = React.ElementRef<"main">;
+export type MainProps = React.ComponentPropsWithoutRef<"main"> &
+  CommonComponentProps;
 
 /**
  * Render the default main server component.
