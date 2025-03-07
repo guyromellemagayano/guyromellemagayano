62084099a4d80af9db8ae0b70594d5d60ecb957a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-07 15:44:59 +0800

    refactor: :recycle: fix code component type exports

--- packages/components/src/code/Code.tsx
+++ packages/components/src/code/Code.tsx
@@ -1,4 +1,4 @@
-import { type HTMLAttributes, lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 
 import type { CommonComponentProps } from "../components";
 
@@ -8,8 +8,9 @@ const CodeClient = lazy(async () => {
   return { default: module.CodeClient };
 });
 
-export type CodeRef = HTMLElement;
-export type CodeProps = HTMLAttributes<CodeRef> & CommonComponentProps;
+export type CodeRef = React.ElementRef<"code">;
+export type CodeProps = React.ComponentPropsWithoutRef<"code"> &
+  CommonComponentProps;
 
 /**
  * Render the default code server component.
