489162ea79e5098cdccd72c058048bdc52b3c262
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-07 15:59:13 +0800

    refactor: :recycle: fix embed external content component type exports

--- packages/components/src/embed/Embed.tsx
+++ packages/components/src/embed/Embed.tsx
@@ -1,4 +1,4 @@
-import { type EmbedHTMLAttributes, lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 
 import type { CommonComponentProps } from "../components";
 
@@ -8,8 +8,9 @@ const EmbedClient = lazy(async () => {
   return { default: module.EmbedClient };
 });
 
-export type EmbedRef = HTMLEmbedElement;
-export type EmbedProps = EmbedHTMLAttributes<EmbedRef> & CommonComponentProps;
+export type EmbedRef = React.ElementRef<"embed">;
+export type EmbedProps = React.ComponentPropsWithoutRef<"embed"> &
+  CommonComponentProps;
 
 /**
  * Render the default embed external content server component.
