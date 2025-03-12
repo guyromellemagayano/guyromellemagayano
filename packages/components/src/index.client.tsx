4336c722729a5f01de801b4e1b5166692eff41fe
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 14:14:44 +0800

    refactor: :recycle: move `blockquote` exports to main index file

--- packages/components/src/index.client.tsx
+++ packages/components/src/index.client.tsx
@@ -23,6 +23,8 @@ import type {
   BdiRef,
   BdoProps,
   BdoRef,
+  BlockquoteProps,
+  BlockquoteRef,
   BProps,
   BRef,
 } from ".";
@@ -224,3 +226,21 @@ export const BdoClient = forwardRef<BdoRef, BdoProps>(
 );
 
 BdoClient.displayName = "BdoClient";
+
+/**
+ * Render the blockquote client component.
+ * @param {BlockquoteProps} props - The blockquote client component properties
+ * @param {BlockquoteRef} ref - The blockquote client component reference
+ * @returns The rendered blockquote client component
+ */
+export const BlockquoteClient = forwardRef<BlockquoteRef, BlockquoteProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <blockquote ref={ref} {...rest}>
+        {children}
+      </blockquote>
+    );
+  }
+);
+
+BlockquoteClient.displayName = "BlockquoteClient";
