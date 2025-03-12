7f7f1a717599c51797bfae3ee376ea6127b24761
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 15:05:25 +0800

    refactor: :recycle: move `dfn` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -870,35 +870,35 @@ export const Details = ({
 Details.displayName = "Details";
 
 // Dynamically import the client component
-// const DfnClient = lazy(async () => {
-//   const module = await import("./index.client");
-//   return { default: module.DfnClient };
-// });
+const DfnClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.DfnClient };
+});
 
-// export type DfnRef = React.ElementRef<"dfn">;
-// export type DfnProps = React.ComponentPropsWithoutRef<"dfn"> &
-//   CommonComponentProps;
+export type DfnRef = React.ElementRef<"dfn">;
+export type DfnProps = React.ComponentPropsWithoutRef<"dfn"> &
+  CommonComponentProps;
 
 /**
  * Render the default definition element server component.
  * @param {DfnProps} props - The default definition element server component properties
  * @returns The rendered default definition element server component
  */
-// export const Dfn = ({ isClient = false, children, ...rest }: DfnProps) => {
-//   const element = <dfn {...rest}>{children}</dfn>;
+export const Dfn = ({ isClient = false, children, ...rest }: DfnProps) => {
+  const element = <dfn {...rest}>{children}</dfn>;
 
-//   if (isClient) {
-//     return (
-//       <Suspense fallback={element}>
-//         <DfnClient {...rest}>{children}</DfnClient>
-//       </Suspense>
-//     );
-//   }
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <DfnClient {...rest}>{children}</DfnClient>
+      </Suspense>
+    );
+  }
 
-//   return element;
-// };
+  return element;
+};
 
-// Dfn.displayName = "Dfn";
+Dfn.displayName = "Dfn";
 
 // Dynamically import the client component
 // const DialogClient = lazy(async () => {
