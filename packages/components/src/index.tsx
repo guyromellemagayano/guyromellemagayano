27efd7a960784a475059cb7a6de55993729fe6bf
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 12:48:14 +0800

    refactor: :recycle: move `article` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -148,3 +148,38 @@ export const Area = ({
 };
 
 Area.displayName = "Area";
+
+// Dynamically import the client component
+const ArticleClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.ArticleClient };
+});
+
+export type ArticleRef = React.ElementRef<"article">;
+export type ArticleProps = React.ComponentPropsWithoutRef<"article"> &
+  CommonComponentProps;
+
+/**
+ * Render the default article server component.
+ * @param {ArticleProps} props - The default article server component properties
+ * @returns The rendered default article server component
+ */
+export const Article = ({
+  isClient = false,
+  children,
+  ...rest
+}: ArticleProps) => {
+  const element = <article {...rest}>{children}</article>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <ArticleClient {...rest}>{children} </ArticleClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Article.displayName = "Article";
