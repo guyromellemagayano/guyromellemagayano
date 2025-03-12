fe7c5cf0545882152391cdf8d7ddfa2ab5e6b711
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 13:18:59 +0800

    refactor: :recycle: move `audio` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -244,3 +244,34 @@ export const B = ({ isClient = false, children, ...rest }: BProps) => {
 };
 
 B.displayName = "B";
+
+// Dynamically import the client component
+const AudioClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.AudioClient };
+});
+
+export type AudioRef = React.ElementRef<"audio">;
+export type AudioProps = React.ComponentPropsWithoutRef<"audio"> &
+  CommonComponentProps;
+
+/**
+ * Render the default audio server component.
+ * @param {AudioProps} props - The default audio server component properties
+ * @returns The rendered default audio server component
+ */
+export const Audio = ({ isClient = false, children, ...rest }: AudioProps) => {
+  const element = <audio {...rest}>{children}</audio>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <AudioClient {...rest}>{children} </AudioClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Audio.displayName = "Audio";
