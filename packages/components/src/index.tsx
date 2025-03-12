d8113fe798912bc43976f7daa6ef882b6d1df062
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 12:42:19 +0800

    refactor: :recycle: move `address` exports to main index file,

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -71,3 +71,38 @@ export const Abbr = ({ isClient = false, children, ...rest }: AbbrProps) => {
 };
 
 Abbr.displayName = "Abbr";
+
+// Dynamically import the client component
+const AddressClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.AddressClient };
+});
+
+export type AddressRef = React.ElementRef<"address">;
+export type AddressProps = React.ComponentPropsWithoutRef<"address"> &
+  CommonComponentProps;
+
+/**
+ * Render the default address server component.
+ * @param {AddressProps} props - The default address server component properties
+ * @returns The rendered default address server component
+ */
+export const Address = ({
+  isClient = false,
+  children,
+  ...rest
+}: AddressProps) => {
+  const element = <address {...rest}>{children}</address>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <AddressClient {...rest}>{children}</AddressClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Address.displayName = "Address";
