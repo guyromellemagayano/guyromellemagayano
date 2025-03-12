c7b54ce7cd40b9fc453a57111a1c2eaab7e934f8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 13:00:18 +0800

    refactor: :recycle: move `aside` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -183,3 +183,34 @@ export const Article = ({
 };
 
 Article.displayName = "Article";
+
+// Dynamically import the client component
+const AsideClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.AsideClient };
+});
+
+export type AsideRef = React.ElementRef<"aside">;
+export type AsideProps = React.ComponentPropsWithoutRef<"aside"> &
+  CommonComponentProps;
+
+/**
+ * Render the default aside server component.
+ * @param {AsideProps} props - The default aside server component properties
+ * @returns The rendered default aside server component
+ */
+export const Aside = ({ isClient = false, children, ...rest }: AsideProps) => {
+  const element = <aside {...rest}>{children}</aside>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <AsideClient {...rest}>{children} </AsideClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Aside.displayName = "Aside";
