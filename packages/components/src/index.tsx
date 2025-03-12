8dfad72b1c48d2fec0545729093db34d7e3584ba
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 14:14:44 +0800

    refactor: :recycle: move `blockquote` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -368,3 +368,38 @@ export const Bdo = ({ isClient = false, children, ...rest }: BdoProps) => {
 };
 
 Bdo.displayName = "Bdo";
+
+// Dynamically import the client component
+const BlockquoteClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.BlockquoteClient };
+});
+
+export type BlockquoteRef = React.ElementRef<"blockquote">;
+export type BlockquoteProps = React.ComponentPropsWithoutRef<"blockquote"> &
+  CommonComponentProps;
+
+/**
+ * Render the default blockquote server component.
+ * @param {BlockquoteProps} props - The default blockquote server component properties
+ * @returns The rendered default blockquote server component
+ */
+export const Blockquote = ({
+  isClient = false,
+  children,
+  ...rest
+}: BlockquoteProps) => {
+  const element = <blockquote {...rest}>{children}</blockquote>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <BlockquoteClient {...rest}>{children} </BlockquoteClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Blockquote.displayName = "Blockquote";
