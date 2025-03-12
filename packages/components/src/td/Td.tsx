5c963dea8428847bcf88b2633f226defcfe709cc
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 12:19:55 +0800

    refactor: :recycle: fix table data cell component type exports

--- packages/components/src/td/Td.tsx
+++ packages/components/src/td/Td.tsx
@@ -1,4 +1,4 @@
-import { type TdHTMLAttributes, lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 
 import type { CommonComponentProps } from "../components";
 
@@ -8,8 +8,9 @@ const TdClient = lazy(async () => {
   return { default: module.TdClient };
 });
 
-export type TdRef = HTMLTableDataCellElement;
-export type TdProps = TdHTMLAttributes<TdRef> & CommonComponentProps;
+export type TdRef = React.ElementRef<"td">;
+export type TdProps = React.ComponentPropsWithoutRef<"td"> &
+  CommonComponentProps;
 
 /**
  * Render the default table data cell server component.
