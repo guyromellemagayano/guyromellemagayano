99ffbe2d18b0d91ef6c93deb4c9d5d7db6d4204a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 17:36:33 +0800

    refactor: :recycle: move `form` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1231,35 +1231,35 @@ export const Footer = ({
 Footer.displayName = "Footer";
 
 // Dynamically import the client component
-// const FormClient = lazy(async () => {
-//   const module = await import("./index.client");
-//   return { default: module.FormClient };
-// });
+const FormClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.FormClient };
+});
 
-// export type FormRef = React.ElementRef<"form">;
-// export type FormProps = React.ComponentPropsWithoutRef<"form"> &
-//   CommonComponentProps;
+export type FormRef = React.ElementRef<"form">;
+export type FormProps = React.ComponentPropsWithoutRef<"form"> &
+  CommonComponentProps;
 
 /**
  * Render the default form server component.
  * @param {FormProps} props - The default form server component properties
  * @returns The rendered default form server component
  */
-// export const Form = ({ isClient = false, children, ...rest }: FormProps) => {
-//   const element = <form {...rest}>{children}</form>;
+export const Form = ({ isClient = false, children, ...rest }: FormProps) => {
+  const element = <form {...rest}>{children}</form>;
 
-//   if (isClient) {
-//     return (
-//       <Suspense fallback={element}>
-//         <FormClient {...rest}>{children}</FormClient>
-//       </Suspense>
-//     );
-//   }
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <FormClient {...rest}>{children}</FormClient>
+      </Suspense>
+    );
+  }
 
-//   return element;
-// };
+  return element;
+};
 
-// Form.displayName = "Form";
+Form.displayName = "Form";
 
 // Dynamically import the client component
 // const HeadClient = lazy(async () => {
