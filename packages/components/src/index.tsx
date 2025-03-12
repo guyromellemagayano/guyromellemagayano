caaf3d512db618cf6de2cc5d731613e35f44e3ac
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 18:22:27 +0800

    refactor: :recycle: move `i` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1463,34 +1463,34 @@ export const Html = ({ isClient = false, children, ...rest }: HtmlProps) => {
 Html.displayName = "Html";
 
 // Dynamically import the client component
-// const IClient = lazy(async () => {
-//   const module = await import("./index.client");
-//   return { default: module.IClient };
-// });
+const IClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.IClient };
+});
 
-// export type IRef = React.ElementRef<"i">;
-// export type IProps = React.ComponentPropsWithoutRef<"i"> & CommonComponentProps;
+export type IRef = React.ElementRef<"i">;
+export type IProps = React.ComponentPropsWithoutRef<"i"> & CommonComponentProps;
 
 /**
  * Render the default idiomatic text server component.
  * @param {IProps} props - The default idiomatic text server component properties
  * @returns The rendered default idiomatic text server component
  */
-// export const I = ({ isClient = false, children, ...rest }: IProps) => {
-//   const element = <i {...rest}>{children}</i>;
+export const I = ({ isClient = false, children, ...rest }: IProps) => {
+  const element = <i {...rest}>{children}</i>;
 
-//   if (isClient) {
-//     return (
-//       <Suspense fallback={element}>
-//         <IClient {...rest}>{children}</IClient>
-//       </Suspense>
-//     );
-//   }
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <IClient {...rest}>{children}</IClient>
+      </Suspense>
+    );
+  }
 
-//   return element;
-// };
+  return element;
+};
 
-// I.displayName = "I";
+I.displayName = "I";
 
 // Dynamically import the client component
 const IframeClient = lazy(async () => {
