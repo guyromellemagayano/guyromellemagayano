9488b71238dad418fb2da0296bd95c00417f010f
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 13:00:18 +0800

    refactor: :recycle: move `aside` exports to main index file

--- packages/components/src/index.client.tsx
+++ packages/components/src/index.client.tsx
@@ -13,6 +13,8 @@ import type {
   ARef,
   ArticleProps,
   ArticleRef,
+  AsideProps,
+  AsideRef,
 } from ".";
 
 /**
@@ -104,3 +106,21 @@ export const ArticleClient = forwardRef<ArticleRef, ArticleProps>(
 );
 
 ArticleClient.displayName = "ArticleClient";
+
+/**
+ * Render the aside client component.
+ * @param {AsideProps} props - The aside client component properties
+ * @param {AsideRef} ref - The aside client component reference
+ * @returns The rendered aside client component
+ */
+export const AsideClient = forwardRef<AsideRef, AsideProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <aside ref={ref} {...rest}>
+        {children}
+      </aside>
+    );
+  }
+);
+
+AsideClient.displayName = "AsideClient";
