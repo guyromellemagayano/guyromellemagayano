d545b656cc0b1245232668d1b65eca1ee4b2be5c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 13:18:59 +0800

    refactor: :recycle: move `audio` exports to main index file

--- packages/components/src/index.client.tsx
+++ packages/components/src/index.client.tsx
@@ -15,6 +15,8 @@ import type {
   ArticleRef,
   AsideProps,
   AsideRef,
+  AudioProps,
+  AudioRef,
   BProps,
   BRef,
 } from ".";
@@ -144,3 +146,21 @@ export const BClient = forwardRef<BRef, BProps>(
 );
 
 BClient.displayName = "BClient";
+
+/**
+ * Render the audio client component.
+ * @param {AudioProps} props - The audio client component properties
+ * @param {AudioRef} ref - The audio client component reference
+ * @returns The rendered audio client component
+ */
+export const AudioClient = forwardRef<AudioRef, AudioProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <audio ref={ref} {...rest}>
+        {children}
+      </audio>
+    );
+  }
+);
+
+AudioClient.displayName = "AudioClient";
