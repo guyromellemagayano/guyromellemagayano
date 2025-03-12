2a3da2e0e34004588b7ed68078412bc0f9b32147
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 14:06:40 +0800

    refactor: :recycle: move `bdi` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -275,3 +275,34 @@ export const Audio = ({ isClient = false, children, ...rest }: AudioProps) => {
 };
 
 Audio.displayName = "Audio";
+
+// Dynamically import the client component
+const BdiClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.BdiClient };
+});
+
+export type BdiRef = React.ElementRef<"bdi">;
+export type BdiProps = React.ComponentPropsWithoutRef<"bdi"> &
+  CommonComponentProps;
+
+/**
+ * Render the default bidirectional isolate server component.
+ * @param {BdiProps} props - The default bidirectional isolate server component properties
+ * @returns The rendered default bidirectional isolate server component
+ */
+export const Bdi = ({ isClient = false, children, ...rest }: BdiProps) => {
+  const element = <bdi {...rest}>{children}</bdi>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <BdiClient {...rest}>{children}</BdiClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Bdi.displayName = "Bdi";
