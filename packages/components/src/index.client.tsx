bcd5cdd90994c24883086c55a7706ff75155944c
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 12:39:06 +0800

    refactor: :recycle: move `a` and `abbr` exports to main index file, update exports

--- packages/components/src/index.client.tsx
+++ packages/components/src/index.client.tsx
@@ -2,7 +2,25 @@
 
 import { forwardRef } from "react";
 
-import type { AbbrProps, AbbrRef } from "./Abbr";
+import type { AbbrProps, AbbrRef, AProps, ARef } from ".";
+
+/**
+ * Render the anchor client component.
+ * @param {AProps} props - The anchor client component properties
+ * @param {ARef} ref - The anchor client component reference
+ * @returns The rendered anchor client component
+ */
+export const AClient = forwardRef<ARef, AProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <a ref={ref} {...rest}>
+        {children}
+      </a>
+    );
+  }
+);
+
+AClient.displayName = "AClient";
 
 /**
  * Render the abbreviation client component.
