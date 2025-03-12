89c961fb3c0bb79acae5de1636f35f6579c16323
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-12 20:27:10 +0800

    refactor: :recycle: move `map` exports to main index file

--- packages/components/src/index.client.tsx
+++ packages/components/src/index.client.tsx
@@ -1073,14 +1073,14 @@ MainClient.displayName = "MainClient";
  * @param {MapRef} ref - The image map client component reference
  * @returns The rendered image map client component
  */
-// export const MapClient = forwardRef<MapRef, MapProps>(
-//   ({ children, ...rest }, ref) => {
-//     return (
-//       <map ref={ref} {...rest}>
-//         {children}
-//       </map>
-//     );
-//   }
-// );
+export const MapClient = forwardRef<MapRef, MapProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <map ref={ref} {...rest}>
+        {children}
+      </map>
+    );
+  }
+);
 
-// MapClient.displayName = "MapClient";
+MapClient.displayName = "MapClient";
