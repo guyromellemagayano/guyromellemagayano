c91048ccda6072b0ba0959da5c1fe9c736ff0819
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-19 07:44:45 +0800

    refactor: :recycle: move `output` exports to main index file

--- packages/components/src/index.client.tsx
+++ packages/components/src/index.client.tsx
@@ -135,6 +135,8 @@ import type {
   OptgroupRef,
   OptionProps,
   OptionRef,
+  OutputProps,
+  OutputRef,
 } from ".";
 
 /**
@@ -1278,3 +1280,21 @@ export const OptionClient = forwardRef<OptionRef, OptionProps>(
 );
 
 OptionClient.displayName = "OptionClient";
+
+/**
+ * Render the output client component.
+ * @param {OutputProps} props - The output client component properties
+ * @param {OutputRef} ref - The output client component reference
+ * @returns The rendered output client component
+ */
+export const OutputClient = forwardRef<OutputRef, OutputProps>(
+  ({ children, ...rest }, ref) => {
+    return (
+      <output ref={ref} {...rest}>
+        {children}
+      </output>
+    );
+  }
+);
+
+OutputClient.displayName = "OutputClient";
