6de1c6a9c2877a8773d2e81bf7626668248c4997
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-19 07:44:45 +0800

    refactor: :recycle: move `output` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -2073,7 +2073,7 @@ Object.displayName = "Object";
 
 // Dynamically import the client component
 const OlClient = lazy(async () => {
-  const module = await import("./Ol.client");
+  const module = await import("./index.client");
   return { default: module.OlClient };
 });
 
@@ -2171,3 +2171,38 @@ export const Option = ({
 };
 
 Option.displayName = "Option";
+
+// Dynamically import the client component
+const OutputClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.OutputClient };
+});
+
+export type OutputRef = React.ElementRef<"output">;
+export type OutputProps = React.ComponentPropsWithoutRef<"output"> &
+  CommonComponentProps;
+
+/**
+ * Render the default output server component.
+ * @param {OutputProps} props - The default output server component properties
+ * @returns The rendered default output server component
+ */
+export const Output = ({
+  isClient = false,
+  children,
+  ...rest
+}: OutputProps) => {
+  const element = <output {...rest}>{children}</output>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <OutputClient {...rest}>{children}</OutputClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Output.displayName = "Output";
