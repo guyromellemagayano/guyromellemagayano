cddbde92de130db1ec5f3df8aa424e6175174fc5
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-19 09:48:27 +0800

    refactor: :recycle: move `p` exports to main index file

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -2206,3 +2206,38 @@ export const Output = ({
 };
 
 Output.displayName = "Output";
+
+// Dynamically import the client component
+const ParagraphClient = lazy(async () => {
+  const module = await import("./index.client");
+  return { default: module.ParagraphClient };
+});
+
+export type ParagraphRef = React.ElementRef<"p">;
+export type ParagraphProps = React.ComponentPropsWithoutRef<"p"> &
+  CommonComponentProps;
+
+/**
+ * Render the default paragraph server component.
+ * @param {ParagraphProps} props - The default paragraph server component properties
+ * @returns The rendered default paragraph server component
+ */
+export const Paragraph = ({
+  isClient = false,
+  children,
+  ...rest
+}: ParagraphProps) => {
+  const element = <p {...rest}>{children}</p>;
+
+  if (isClient) {
+    return (
+      <Suspense fallback={element}>
+        <ParagraphClient {...rest}>{children}</ParagraphClient>
+      </Suspense>
+    );
+  }
+
+  return element;
+};
+
+Paragraph.displayName = "Paragraph";
