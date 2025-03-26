fefcd20b27e898b36a4daf2295b7438ec2ead717
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-03-26 14:12:37 +0800

    refactor: :recycle: create `Home` component, update deps

---
+++ apps/blog/app/components/Home.tsx
@@ -0,0 +1,22 @@
+import { Link } from "@remix-run/react";
+
+import { CounterButton } from "@guyromellemagayano/ui/counter-button";
+
+export const Home = () => {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Blog <br />
+        <span>Kitchen Sink</span>
+      </h1>
+      <CounterButton />
+      <p className="description">
+        Built With <Link to="https://turbo.build/repo">Turborepo</Link>
+        {" & "}
+        <Link to="https://remix.run/">Remix</Link>
+      </p>
+    </div>
+  );
+};
+
+Home.displayName = "Home";
