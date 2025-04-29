b00fcd5805f75cd227978d7db013260c9dd0a17e
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-04-29 11:02:26 +0800

    style: :lipstick: fix tsconfig for admin app, fix lint issues

--- apps/admin/src/app/index.tsx
+++ apps/admin/src/app/index.tsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+
 import { CounterButton } from "@guyromellemagayano/ui/counter-button";
 import { Link } from "@guyromellemagayano/ui/link";
 
--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,7 +1,9 @@
+import "./index.css";
+
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 import App from "./app";
-import "./index.css";
 
 const el = document.getElementById("root");
 if (el) {
