db486a7ad6d0c55b1152bd6412bb82732e2031f8
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-06-24 07:46:33 +0800

    style: :lipstick: code formatting updates for `tailwind` config, bump package versions

--- packages/config-tailwind/src/index.js
+++ packages/config-tailwind/src/index.js
@@ -1,14 +1,14 @@
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import containerQueries from '@tailwindcss/container-queries';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import containerQueries from "@tailwindcss/container-queries";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /**
  * A shared TailwindCSS configuration for the repository.
  * @type {import("tailwindcss").Config}
  */
 export const baseTailwindConfig = {
-  content: ['**/*.{ts,tsx}'],
-  darkMode: 'class',
+  content: ["**/*.{ts,tsx}"],
+  darkMode: "class",
   plugins: [typography, forms, aspectRatio, containerQueries],
 };
