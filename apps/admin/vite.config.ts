c62fe09a358938935bfb702e0b30d3d8d9fbbf30
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-06-24 14:28:00 +0800

    feat: :sparkles: integrate `vite-plugin-devtools-json` package to `admin` app

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { join } from "node:path";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   envDir: join(__dirname, "../../"),
-  plugins: [react()],
+  plugins: [react(), devtoolsJson()],
 });
