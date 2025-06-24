fe276648c0680cc775cc42f51474116aa1e992ab
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-06-24 14:28:48 +0800

    build: :package: update `defineConfig` settings for custom `ui` package

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/**/*.tsx"],
-  format: ["esm"],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   outDir: "dist",
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
   target: "esnext",
+  outExtension({ format }) {
+    return {
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    };
+  },
 });
