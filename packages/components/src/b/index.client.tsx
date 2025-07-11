714073ed21be54ace1f1272f8f54991c4672b0bc
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2025-07-11 12:15:44 +0800

    fix: :bug: revert back to original implementation of `B` component

--- packages/components/src/b/index.client.tsx
+++ packages/components/src/b/index.client.tsx
@@ -5,20 +5,15 @@ import React, { memo } from "react";
 import { B, type BProps, type BRef } from ".";
 
 /**
- * Client-side bring attention to component.
- * For most use cases, the server component should be sufficient.
- * This exists for cases where client-side interactivity is specifically needed.
+ * Render the bring attention client component.
  */
-export const BClient = React.forwardRef<BRef, BProps>((props, ref) => {
-  // Simply delegate to the main component
-  // The main component already handles all optimizations
-  return <B ref={ref} {...props} />;
-});
+export const BClient = React.forwardRef<BRef, BProps>((props, ref) => (
+  <B ref={ref} {...props} />
+));
 
 BClient.displayName = "BClient";
 
-// Memoized version for cases where props change frequently
+/**
+ * Memoized version of `BClient` for performance optimization.
+ */
 export const MemoizedBClient = memo(BClient);
-
-// Export default for convenience
-export default BClient;
